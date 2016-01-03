a78c2eb86807b097a1c3a2d09a5ea63184c4ea1d
Dongmin Lee <ldm2468@gmail.com>
2016-01-04 00:31:46 +0900

    디버깅2 (위험!!)

--- src/content/board/view.php
+++ src/content/board/view.php
@@ -117,6 +117,7 @@ function putCommentTree($parent,$root){
 				else if(!$m['s_pic'] && !$b_comment_anonymous)
 					echo '<img src="/images/no-image.png" style="float:left;width:'.$pic_sz.'px;height:'.$pic_sz.'px;margin-right:7px;" />';
 				else{
+                    echo hexdec($hash_val);
                     $hue = ((hexdec($hash_val) / 1234) % 361 + 361) % 361; //positive int from 0 to 360
                     echo $hue;
                     echo '<div class="circle anonymous-bubble" style="float:left; width:'.$pic_sz.'px; height:'.$pic_sz.'px; background:rgb('.hsvToRgb($hue,50,70).');">'.substr(base_convert($hash_val, 16, 36),2,1).'</div>';
