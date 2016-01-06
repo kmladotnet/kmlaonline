d47d3978bada78231646dcdf22525b3574afffc8
Dongmin Lee <ldm2468@gmail.com>
2016-01-06 22:34:11 +0900

    grid 스크롤바 다시 수정

--- src/content/main.php
+++ src/content/main.php
@@ -25,7 +25,7 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="0" data-gs-y="0"
                 data-gs-width="10" data-gs-height="6">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block">
                             <div class="main-block-title">
                                 꼭 보세요
@@ -40,10 +40,13 @@ function printContentPc(){
                                     </a>
                                 </div>
                             </div>
-                            <?php
-                                require_once("modules/article-list.php");
-                                articleList($mysqli->query("SELECT * FROM kmlaonline_important_notices_table WHERE n_state=1 ORDER BY n_id DESC"), true,true,true,true);
-                            ?>
+
+                            <div class="main-block-content">
+                                <?php
+                                    require_once("modules/article-list.php");
+                                    articleList($mysqli->query("SELECT * FROM kmlaonline_important_notices_table WHERE n_state=1 ORDER BY n_id DESC"), true,true,true,true);
+                                ?>
+                            </div>
                         </div>
                     </div>
             </div>
@@ -51,12 +54,14 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="10" data-gs-y="0"
                 data-gs-width="2" data-gs-height="2">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block">
-						<div class="main-block-title">
-							<img src="/theme/dev/birthday.png" style="width:32px;" /> 생일!
-						</div>
-						<?php include("modules/birthday.php"); ?>
+                            <div class="main-block-title">
+                                <img src="/theme/dev/birthday.png" style="width:32px;" /> 생일!
+                            </div>
+                            <div class="main-block-content">
+                                <?php include("modules/birthday.php"); ?>
+                            </div>
 						</div>
 					</div>
             </div>
@@ -64,7 +69,7 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="10" data-gs-y="2"
                 data-gs-width="2" data-gs-height="4">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block gradient">
                             <div class="main-block-title">
                                 <img src="/theme/dev/food.png" style="width:32px;" /> 식단!
@@ -74,7 +79,10 @@ function printContentPc(){
                                     <a <?php if($is_night) echo 'style="color:black"'; ?> onclick="main_changeFood(this, 'food-dinner');">저녁</a>
                                 </div>
                             </div>
-                        <?php include("modules/menu.php"); ?>
+
+                            <div class="main-block-content">
+                                <?php include("modules/menu.php"); ?>
+                            </div>
 					   </div>
 					</div>
             </div>
@@ -82,7 +90,7 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="0" data-gs-y="6"
                 data-gs-width="12" data-gs-height="6">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block">
                             <div class="main-block-title">
                                 큼라보드
@@ -93,13 +101,13 @@ function printContentPc(){
                                     </div>
                                 <?php } ?>
                             </div>
-                            <div style="padding:5px;">
+                            <div class="main-block-content">
                                 <?php
-                                $dat="";
-                                if(file_exists("data/kmlaboard.txt") && filesize("data/kmlaboard.txt")>0){
-                                    $dat=file_get_contents("data/kmlaboard.txt");
-                                }
-                                filterContent(nl2br(strip_tags($dat,"<b><big><small><i><u><strong><strike><a><font><img><q><s><sub><sup>")));
+                                    $dat="";
+                                    if(file_exists("data/kmlaboard.txt") && filesize("data/kmlaboard.txt")>0){
+                                        $dat=file_get_contents("data/kmlaboard.txt");
+                                    }
+                                    filterContent(nl2br(strip_tags($dat,"<b><big><small><i><u><strong><strike><a><font><img><q><s><sub><sup>")));
                                 ?>
                             </div>
                         </div>
@@ -109,7 +117,7 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="0" data-gs-y="12"
                 data-gs-width="12" data-gs-height="4">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block">
                             <div class="main-block-title">
                                 자유게시판
@@ -117,10 +125,12 @@ function printContentPc(){
                                         <a href="/board/commu">더보기</a>
                                     </div>
                             </div>
-                            <?php
-                                require_once("modules/article-list.php");
-                                articleList($mysqli->query("SELECT * FROM kmlaonline_board_data WHERE n_cat=139 and n_parent is null ORDER BY n_id DESC LIMIT 7"), true, true, true, true);
-                            ?>
+                            <div class="main-block-content">
+                                <?php
+                                    require_once("modules/article-list.php");
+                                    articleList($mysqli->query("SELECT * FROM kmlaonline_board_data WHERE n_cat=139 and n_parent is null ORDER BY n_id DESC LIMIT 7"), true, true, true, true);
+                                ?>
+                            </div>
                         </div>
 					</div>
             </div>
@@ -128,17 +138,19 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="0" data-gs-y="16"
                 data-gs-width="7" data-gs-height="6">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block gradient" style="overflow:auto">
                             <div class="main-block-title">
                                 내 게시판
                                 <div style="font-size:9pt;float:right;height:15pt;padding-top:3pt;"><a href="/board/special:list-mine">모두 보기</a> | <a href="/user/settings?display=1">항목 바꾸기</a></div>
                             </div>
-                            <?php
-                                require_once("modules/article-list.php");
-                                $accessible_categories=getUserMainBoards($me);
-                                articleList($board->getArticleList($accessible_categories, false, 0, 0, 16), true, true, true, false);
-                            ?>
+                            <div class="main-block-content">
+                                <?php
+                                    require_once("modules/article-list.php");
+                                    $accessible_categories=getUserMainBoards($me);
+                                    articleList($board->getArticleList($accessible_categories, false, 0, 0, 16), true, true, true, false);
+                                ?>
+                            </div>
 					   </div>
 					</div>
             </div>
@@ -146,13 +158,13 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="7" data-gs-y="16"
                 data-gs-width="5" data-gs-height="6">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block gradient" style="overflow:auto">
                             <div class="main-block-title">
                                 <img src="/theme/dev/kmlacafe.png" style="width:24px;vertical-align:bottom;margin-bottom:2px;" /> 큼라 카페
                                 <div style="font-size:9pt;float:right;height:15pt;padding-top:3pt;"><a href="/board/site_kmlacafe">모두 보기</a></div>
                             </div>
-                            <div style="padding:5px;">
+                            <div class="main-block-content">
                             <?php
                                 require_once("modules/article-list.php");
                                 $articles=$board->getArticleList(array($board->getCategory(false,"site_kmlacafe")['n_id']), false, 0, 0, 16);
@@ -166,13 +178,15 @@ function printContentPc(){
             <div class="grid-stack-item"
                 data-gs-x="0" data-gs-y="22"
                 data-gs-width="12" data-gs-height="4">
-                    <div class="grid-stack-item-content main-block-content">
+                    <div class="grid-stack-item-content">
                         <div class="main-block">
                             <div class="main-block-title">
                                 갤러리
                                 <div style="font-size:9pt;float:right;height:15pt;padding-top:3pt;"><a href="/board/all_gallery">모두 보기</a></div>
                             </div>
-                            <?php printGallery(); ?>
+                            <div class="main-block-content">
+                                <?php printGallery(); ?>
+                            </div>
                         </div>
 					</div>
             </div>
