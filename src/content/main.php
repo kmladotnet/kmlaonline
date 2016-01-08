a1f09e40e9d80906ae96288b396c98d146daa7e3
Dongmin Lee <ldm2468@gmail.com>
2016-01-08 23:51:59 +0900

    php?

--- src/content/main.php
+++ src/content/main.php
@@ -170,7 +170,7 @@ JSON
         };
         $('.grid-stack').gridstack(options);
         $('.grid-stack').on('change', function (e, items) {
-            $.post("ajax/user/updatelayout.php", {"json": mainGridToJSON(), "ajax": "1"});
+            $.post("ajax/user/updatelayout", {"json": mainGridToJSON(), "ajax": "1"});
         });
     });
     </script>
