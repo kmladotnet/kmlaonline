23ea3212676e1f4608489825be18575d0bbd9216
Dongmin Lee <ldm2468@gmail.com>
2016-01-08 20:49:20 +0900

    익스트림 디버깅

--- src/content/board.php
+++ src/content/board.php
@@ -15,7 +15,7 @@ if($board_id=="special:list-all"){
 	include "src/content/board/list-mine.php";
 	$board_act="list";
 	$title="내 게시판 - $title";
-}else if(is_array($board_id)) {
+}else if(true) {
     print_r($board_id);
 	$title=getOrDefault($board_id["title"], "여러가지");
     $board_id = arrayToCategories($board_id["cat"]);
