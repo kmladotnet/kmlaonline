98140ff8b4085c550f555b0502131cb11cc375de
Dongmin Lee <ldm2468@gmail.com>
2016-01-09 11:54:09 +0900

    디렉터리 이름

--- src/process/user/getmodule.php
+++ src/process/user/getmodule.php
@@ -1,5 +1,5 @@
 <?php
-require_once("src/modules/module.php");
+require_once("src/content/modules/module.php");
 $post_data = json_decode($_POST['json'], true);
 moduleContents($post_data['name'], $post_data['options']['options']);
 ?>
