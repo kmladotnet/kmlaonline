3a4c4034dfa36f846def47242126d6c0fc3283a4
Dongmin Lee <ldm2468@gmail.com>
2016-01-09 11:58:25 +0900

    POST에서 REQUEST로

--- src/process/user/getmodule.php
+++ src/process/user/getmodule.php
@@ -1,5 +1,5 @@
 <?php
 require_once("src/content/modules/module.php");
-$post_data = json_decode($_POST['json'], true);
+$post_data = json_decode($_REQUEST['json'], true);
 moduleContents($post_data['name'], $post_data['options']['options']);
 ?>
