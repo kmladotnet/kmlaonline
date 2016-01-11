18c2936798fa6ef865d316781f4dcddb5f0445fb
Dongmin Lee <ldm2468@gmail.com>
2016-01-11 10:17:02 +0900

    kmlaboard에 ckeditor

--- src/content/util/kmlaboard.php
+++ src/content/util/kmlaboard.php
@@ -14,9 +14,15 @@ function printContent(){
 		if(isUserPermitted($me['n_id'], "kmlaboard_changer")){
 			?>
 			<form action="/proc/util/kmlaboard" method="post" onsubmit="return saveAjax(this,'처리 중...');">
-				<textarea name="data" style="width:98%;box-sizing:border-box"><?php echo htmlspecialchars($dat); ?></textarea>
+				<textarea name="data" id="s_data_ckeditor" style="width:98%;box-sizing:border-box"><?php echo htmlspecialchars($dat); ?></textarea>
 				<input type="submit" value="저장" style="width:80px;height:32px;" />
 			</form>
+            <script>
+                CKEDITOR.replace('s_data_ckeditor', {
+                    language: 'ko',
+                    font_names : '맑은 고딕;나눔고딕;나눔명조;나눔펜;굴림;바탕;돋움;궁서;Register;Sensation;Arial;Times New Roman;Verdana;Trebuchet MS;'
+                });
+            </script>
 			<?php
 		}else{
 			$dat=nl2br(strip_tags($dat,"<b><big><small><i><u><strong><strike><a><font><img><q><s><sub><sup>"));
