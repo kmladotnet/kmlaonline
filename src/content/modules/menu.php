13b0849cb29430c61d897f3f12d32fce73241f66
Dongmin Lee <ldm2468@gmail.com>
2016-01-11 10:54:43 +0900

    식단 모양

--- src/content/modules/menu.php
+++ src/content/modules/menu.php
@@ -1,6 +1,5 @@
-<div style="clear:right;display:block;height:7px;"></div>
 <div style="text-align:center">
-    <div style="font-size:9pt;float:right;height:15pt;padding-top:3pt;">
+    <div style="font-size:9pt;height:15pt;">
         <a <?php if($is_morning) echo 'style="color:black"'; ?> onclick="main_changeFood(this, 'food-breakfast');">아침</a> |
         <a <?php if($is_afternoon) echo 'style="color:black"'; ?> onclick="main_changeFood(this, 'food-lunch');">점심</a> |
         <a <?php if($is_night) echo 'style="color:black"'; ?> onclick="main_changeFood(this, 'food-dinner');">저녁</a> |
