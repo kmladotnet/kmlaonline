f8f0a4869f284edd8a37dfedb1c7a79251c82291
Dongmin Lee <ldm2468@gmail.com>
2016-01-11 10:52:53 +0900

    식단 모두보기 위치 변경

--- src/content/modules/menu.php
+++ src/content/modules/menu.php
@@ -1,5 +1,11 @@
 <div style="clear:right;display:block;height:7px;"></div>
 <div style="text-align:center">
+    <div style="font-size:9pt;float:right;height:15pt;padding-top:3pt;">
+        <a <?php if($is_morning) echo 'style="color:black"'; ?> onclick="main_changeFood(this, 'food-breakfast');">아침</a> |
+        <a <?php if($is_afternoon) echo 'style="color:black"'; ?> onclick="main_changeFood(this, 'food-lunch');">점심</a> |
+        <a <?php if($is_night) echo 'style="color:black"'; ?> onclick="main_changeFood(this, 'food-dinner');">저녁</a> |
+        <a href="/util/schedule?<?php echo "year=$curYear&amp;month=$curMonth&amp;mode=food:0"?>">모두 보기</a>
+    </div>
     <?php
     $curYear=date("Y"); $curMonth=date("n"); $curDay=date("j");
     if($is_morning && date("H")>=22){
@@ -20,7 +26,4 @@
     <div id="food-breakfast" class="morning"><?php echo isset($scheduleData['food:0'])?nl2br($scheduleData['food:0']):"<span style='color:#DDD'>(입력되지 않음)</span>"; ?></div>
     <div id="food-lunch" class="afternoon"><?php echo isset($scheduleData['food:1'])?nl2br($scheduleData['food:1']):"<span style='color:#DDD'>(입력되지 않음)</span>"; ?></div>
     <div id="food-dinner" class="night"><?php echo isset($scheduleData['food:2'])?nl2br($scheduleData['food:2']):"<span style='color:#DDD'>(입력되지 않음)</span>"; ?></div>
-</div>
-<div style="position:absolute;right:0;bottom:0;text-align:right;padding:3pt;">
-    <a href="/util/schedule?<?php echo "year=$curYear&amp;month=$curMonth&amp;mode=food:0"?>">모두 보기</a>
 </div>
