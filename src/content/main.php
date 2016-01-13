276eaef4c6be0e5ea4c7997dfcc787222ebd262d
Dongmin Lee <ldm2468@gmail.com>
2016-01-13 11:58:37 +0900

    모듈 추가 창 준비

--- src/content/main.php
+++ src/content/main.php
@@ -28,6 +28,11 @@ function printContentPc(){
 			</tr>
 		</table>
         <button type="button" class="btn btn-danger" onclick="resetMainLayout()">초기화</button>
+        <button type="button" class="btn btn-success" data-toggle="collapse" data-target="#add-module">패널 추가</button>
+
+        <div id="add-module">
+            준비중입니다... ㅠㅠ
+        </div>
         <div class="grid-stack">
             <?php
                     $default_options = <<<JSON
