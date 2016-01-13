5e56507e203e5e5d99a784cfd969e552f9ee16ff
Dongmin Lee <ldm2468@gmail.com>
2016-01-13 22:20:02 +0900

    크기조정 핸들

--- src/content/main.php
+++ src/content/main.php
@@ -194,12 +194,11 @@ JSON
             animate: true,
             cell_height: 64,
             vertical_margin: 4,
-            always_show_resize_handle: true,
             draggable: {
                 handle: '.main-block-title',
             },
             resizable: {
-                handles: 'e, se, s, sw, w'
+                handles: 'se, sw'
             }
         };
         $('.grid-stack').gridstack(options);
