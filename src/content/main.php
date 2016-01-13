d831d2d0feec110bb831b464b23fda391953362f
Dongmin Lee <ldm2468@gmail.com>
2016-01-13 13:34:12 +0900

    패널 추가 버튼 텍스트

--- src/content/main.php
+++ src/content/main.php
@@ -28,7 +28,7 @@ function printContentPc(){
 			</tr>
 		</table>
         <button type="button" class="btn btn-danger" onclick="resetMainLayout()">초기화</button>
-        <select id="add-module" class="selectpicker" data-style="btn-success" data-size="10">
+        <select id="add-module" class="selectpicker" data-style="btn-success" data-size="10" title="패널 추가">
             <option value="important">중요 공지</option>
             <option value="birthday">생일</option>
             <option value="menu">식단</option>
