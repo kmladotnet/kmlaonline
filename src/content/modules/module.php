7c8b8988dafc6ed19c75076cb056ae200f2da0f3
Dongmin Lee <ldm2468@gmail.com>
2016-01-15 20:12:30 +0900

    변수 고치기

--- src/content/modules/module.php
+++ src/content/modules/module.php
@@ -1,7 +1,7 @@
 <?php
 require_once("article-list.php");
 function moduleTitle($module_name, $options) {
-	global $member, $me, $is_morning, $is_afternoon, $is_night, $mysqli, $board;
+	global $member, $me, $is_morning, $is_afternoon, $is_night, $mysqli, $board, $curYear, $curMonth, $curDay;
     switch($module_name) {
         case 'important':
             ?>
@@ -52,7 +52,7 @@ function moduleTitle($module_name, $options) {
 }
 
 function moduleContent($module_name, $options) {
-	global $member, $me, $is_morning, $is_afternoon, $is_night, $mysqli, $board;
+	global $member, $me, $is_morning, $is_afternoon, $is_night, $mysqli, $board, $curMonth, $curDay;
     switch($module_name) {
         case 'important':
             articleList($mysqli->query("SELECT * FROM kmlaonline_important_notices_table WHERE n_state=1 ORDER BY n_id DESC"), true,true,true,true);
