5b69abb7b23bc38b4827af5ccc475abd57bef5b8
Dongmin Lee <ldm2468@gmail.com>
2016-01-16 00:12:12 +0900

    printr

--- src/process/user/getmodule.php
+++ src/process/user/getmodule.php
@@ -1,5 +1,6 @@
 <?php
 require_once("src/content/modules/module.php");
 $post_data = json_decode($_REQUEST['json'], true);
+print_r($post_data['options']['options']);
 moduleContents($post_data['name'], $post_data['options']['options']);
 ?>
