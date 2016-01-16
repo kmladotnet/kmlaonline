8a6a2c7fa6a8250b5ba993c7c1716e90d056b256
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 00:36:43 +0900

    날씨 모양

--- src/content/modules/weather.php
+++ src/content/modules/weather.php
@@ -5,19 +5,19 @@ $ko_time = new DateTimeZone('Asia/Seoul');
 <div class="weather">
     <div class="weather-last-update">
         <?php
-        echo $weather->lastUpdate->setTimeZone($ko_time)->format('m월 d일 H시 i분');
+        echo $weather->lastUpdate->setTimeZone($ko_time)->format('n월 j일 H시 i분 기준');
         ?>
     </div>
     <link href="css/owfont-regular.css" rel="stylesheet" type="text/css">
-    <i class="owf owf-<?php echo $weather->weather->id;?> owf-5x"></i>
     <div class="weather-temp">
+        <i class="owf owf-<?php echo $weather->weather->id;?> owf-2x" style="vertical-align:middle"></i>
         <?php
         echo $weather->temperature;
         ?>
     </div>
     <div class="weather-name">
         <?php
-        echo lang('weather', $weather->weather->id);
+        echo '(',lang('weather', $weather->weather->id),')';
         ?>
     </div>
     <div class="weather-copyright">
