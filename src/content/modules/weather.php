6d2172365e9872b27e9758499e9aff39882ab8c4
Dongmin Lee <ldm2468@gmail.com>
2016-01-16 21:24:58 +0900

    충격과 공포의 핵

--- src/content/modules/weather.php
+++ src/content/modules/weather.php
@@ -1,16 +1,22 @@
 <?php
 use Cmfcmf\OpenWeatherMap;
 use Cmfcmf\OpenWeatherMap\Exception as OWMException;
+function __autoload($className) {
+      if (file_exists('src/lib/OpenWeatherMap/'.$className.'.php')) {
+          require_once('src/lib/OpenWeatherMap/'.$className.'.php');
+          return true;
+      }
+      if (file_exists('src/lib/OpenWeatherMap/Fetcher/'.$className.'.php')) {
+          require_once('src/lib/OpenWeatherMap/Fetcher/'.$className.'.php');
+          return true;
+      }
+      if (file_exists('src/lib/OpenWeatherMap/Util/'.$className.'.php')) {
+          require_once('src/lib/OpenWeatherMap/Util/'.$className.'.php');
+          return true;
+      }
+      return false;
+}
 require_once("src/lib/OpenWeatherMap.php");
-foreach (glob("src/lib/OpenWeatherMap/*.php") as $filename){
-    require_once($filename);
-}
-foreach (glob("src/lib/OpenWeatherMap/Fetcher/*.php") as $filename){
-    require_once($filename);
-}
-foreach (glob("src/lib/OpenWeatherMap/Util/*.php") as $filename){
-    require_once($filename);
-}
 
 $lang = 'ko';
 
