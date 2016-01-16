2e326efc2b062ed27c1c708f5960b6bda110ddf7
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 00:08:47 +0900

    날씨 업데이트 시기

--- src/content/modules/weather.php
+++ src/content/modules/weather.php
@@ -2,6 +2,11 @@
 $weather = getWeather();
 ?>
 <div class="weather">
+    <div class="weather-last-update">
+        <?php
+        echo $weather->lastUpdate->format('m월 d일 H시 i분');
+        ?>
+    </div>
     <link href="css/owfont-regular.css" rel="stylesheet" type="text/css">
     <i class="owf owf-<?php echo $weather->weather->id;?> owf-5x"></i>
     <div class="weather-temp">
@@ -14,5 +19,7 @@ $weather = getWeather();
         echo lang('weather', $weather->weather->id);
         ?>
     </div>
-    ?>
+    <div class="weather-copyright">
+        정보제공: <a href="http://openweathermap.org/">openweathermap.org</a>
+    </div>
 </div>
