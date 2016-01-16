0777a66a0d3d90a32373facd2b4a5d3ffd1fb05d
Dongmin Lee <ldm2468@gmail.com>
2016-01-16 21:27:12 +0900

    극.혐.

--- src/content/modules/weather.php
+++ src/content/modules/weather.php
@@ -2,19 +2,20 @@
 use Cmfcmf\OpenWeatherMap;
 use Cmfcmf\OpenWeatherMap\Exception as OWMException;
 function __autoload($className) {
-      if (file_exists('src/lib/OpenWeatherMap/'.$className.'.php')) {
-          require_once('src/lib/OpenWeatherMap/'.$className.'.php');
-          return true;
-      }
-      if (file_exists('src/lib/OpenWeatherMap/Fetcher/'.$className.'.php')) {
-          require_once('src/lib/OpenWeatherMap/Fetcher/'.$className.'.php');
-          return true;
-      }
-      if (file_exists('src/lib/OpenWeatherMap/Util/'.$className.'.php')) {
-          require_once('src/lib/OpenWeatherMap/Util/'.$className.'.php');
-          return true;
-      }
-      return false;
+    $className = substr($className, strrpos($className, '\\') + 1);
+    if (file_exists('src/lib/OpenWeatherMap/'.$className.'.php')) {
+        require_once('src/lib/OpenWeatherMap/'.$className.'.php');
+        return true;
+    }
+    if (file_exists('src/lib/OpenWeatherMap/Fetcher/'.$className.'.php')) {
+        require_once('src/lib/OpenWeatherMap/Fetcher/'.$className.'.php');
+        return true;
+    }
+    if (file_exists('src/lib/OpenWeatherMap/Util/'.$className.'.php')) {
+        require_once('src/lib/OpenWeatherMap/Util/'.$className.'.php');
+        return true;
+    }
+    return false;
 }
 require_once("src/lib/OpenWeatherMap.php");
 
