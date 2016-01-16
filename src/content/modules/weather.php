cf2e5476537aa9272b1cfdcef53682288b5eebe3
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 00:00:54 +0900

    날씨 모양

--- src/content/modules/weather.php
+++ src/content/modules/weather.php
@@ -4,9 +4,15 @@ $weather = getWeather();
 <div class="weather">
     <link href="css/owfont-regular.css" rel="stylesheet" type="text/css">
     <i class="owf owf-<?php echo $weather->weather->id;?> owf-5x"></i>
-    <br>
-    <?php
+    <div class="weather-temp">
+        <?php
+        echo $weather->temperature;
+        ?>
+    </div>
+    <div class="weather-name">
+        <?php
         echo lang('weather', $weather->weather->id);
-        echo '기온: ', $weather->temperature;
+        ?>
+    </div>
     ?>
 </div>
