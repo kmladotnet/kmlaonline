c24881c1a72cf0aa8545b5ac36065c87c705320e
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 23:26:50 +0900

    안해

--- src/content/modules/minjok-news.php
+++ src/content/modules/minjok-news.php
@@ -8,9 +8,7 @@
 
             $type=trim($each[1]);
             $date=trim($each[2]);
-            $msg=preg_replace('@\x{FFFD}@u', '', $each[3]);
-            echo $msg;
-            echo strip_tags($msg);
+            $msg=strip_tags(preg_replace('@\x{FFFD}@u', '', $each[3]));
             ?>
             <dt style="width: 90px; margin-bottom: 8px">
                 <?php echo "[$date] $type";?>
