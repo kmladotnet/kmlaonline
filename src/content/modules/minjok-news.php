f85639505f189493eb9ee649a288fef2a2ffd3e2
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 22:20:43 +0900

    검은색

--- src/content/modules/minjok-news.php
+++ src/content/modules/minjok-news.php
@@ -14,7 +14,7 @@
                 <?php echo "[$date] $type";?>
             </dt>
             <dd style="margin-left: 100px">
-                <a href="http://www.minjok.hs.kr/members/" target="_new"><?php echo "$msg"?></a>
+                <a href="http://www.minjok.hs.kr/members/" target="_new" style="color: black"><?php echo "$msg"?></a>
             </dd>
             <?php
         }
