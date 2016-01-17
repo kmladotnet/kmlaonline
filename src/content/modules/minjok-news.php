cf6295e8e035be4be94a4cbe4cb961558ee1cd53
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 23:17:04 +0900

    아

--- src/content/modules/minjok-news.php
+++ src/content/modules/minjok-news.php
@@ -9,6 +9,8 @@
             $type=trim($each[1]);
             $date=trim($each[2]);
             $msg=trim(strip_tags(str_replace('�','',$each[3])));
+            echo $each[3];
+            echo $msg;
             ?>
             <dt style="width: 90px; margin-bottom: 8px">
                 <?php echo "[$date] $type";?>
