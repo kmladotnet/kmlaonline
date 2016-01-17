a8e654e2f9b55cf1c7dd5f592b0c9039d1250d5b
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 23:06:43 +0900

    nbsp

--- src/content/modules/minjok-news.php
+++ src/content/modules/minjok-news.php
@@ -14,7 +14,7 @@
                 <?php echo "[$date] $type";?>
             </dt>
             <dd style="margin-left: 100px">
-                <a href="http://www.minjok.hs.kr/members/" target="_new" style="color: black; white-space: nowrap;"><?php echo htmlspecialchars(str_replace('�', '', $msg)); ?></a>
+                <a href="http://www.minjok.hs.kr/members/" target="_new" style="color: black; white-space: nowrap;"><?php echo str_replace('&nbsp;', '', htmlspecialchars($msg)); ?></a>
             </dd>
             <?php
         }
