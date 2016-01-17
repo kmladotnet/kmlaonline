1a117e3a547881666572c11242e4b59a616862bc
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 22:11:14 +0900

    kmla 뉴스 마지막 3개 생략

--- src/content/modules/minjok-news.php
+++ src/content/modules/minjok-news.php
@@ -2,7 +2,10 @@
     <?php
     $f=file_get_contents("http://www.minjok.hs.kr/app/kumla_notice/kumla_all.html");
     if(preg_match_all('/<li>\[([^\]]+):([^\]]+)\](.*?)<br>/sim', $f, $m, PREG_SET_ORDER)){
-        foreach($m as $each) {
+        $length = count($m);
+        for ($i = 0; $i < $length - 3; $i++) {
+            $each = $m[$i];
+
             $type=trim($each[1]);
             $date=trim($each[2]);
             $msg=trim(strip_tags($each[3]));
