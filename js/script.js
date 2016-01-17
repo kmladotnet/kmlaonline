645d418ef2503bf1062d91b2bb52eec0c437ee8e
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 22:57:00 +0900

    애니메이션

--- js/script.js
+++ js/script.js
@@ -720,19 +720,19 @@ function toggleLayoutEditing() {
     if(!editMode) {
         $(".main-block-close").css({display: "inline-block", "border-width": "1px"});
         $(".main-block-options").css({display: "inline-block", "border-width": "1px"});
-        $(".main-block-close").velocity({width: 28, opacity: 1}, 300);
-        $(".main-block-options").velocity({width: 28, opacity: 1}, 300);
-        $(".main-block-reload").velocity({width: 28, "border-radius": 0}, 300);
+        $(".main-block-close").velocity({width: 28, opacity: 1}, 200);
+        $(".main-block-options").velocity({width: 28, opacity: 1}, 200);
+        $(".main-block-reload").velocity({width: 28, "border-radius": 0}, 200);
         $(".main-block-title").css("cursor", "move");
-        $("#main-edit-pane").velocity("slideDown", {duration: 300, easing: "easeOutCubic"});
+        $("#main-edit-pane").velocity("slideDown", {duration: 200, easing: "easeOutCubic"});
         $('.grid-stack').data('gridstack').enable();
         $("#main-edit-button").html("저장하고 편집 모드 종료");
     } else {
-        $(".main-block-close").velocity({width: 0, opacity: 0}, 300, function() {$(this).css({display: "none", "border-width": "0"})});
-        $(".main-block-options").velocity({width: 0, opacity: 0}, 300, function() {$(this).css({display: "none", "border-width": "0"})});
-        $(".main-block-reload").velocity({width: 24, "border-radius": 12}, 300);
+        $(".main-block-close").velocity({width: 0, opacity: 0}, 200, function() {$(this).css({display: "none", "border-width": "0"})});
+        $(".main-block-options").velocity({width: 0, opacity: 0}, 200, function() {$(this).css({display: "none", "border-width": "0"})});
+        $(".main-block-reload").velocity({width: 24, "border-radius": 12}, 200);
         $(".main-block-title").css("cursor", "default");
-        $("#main-edit-pane").velocity("slideUp", {duration: 300, easing: "easeOutCubic"});
+        $("#main-edit-pane").velocity("slideUp", {duration: 200, easing: "easeOutCubic"});
         $('.grid-stack').data('gridstack').disable();
         $("#main-edit-button").html("편집 모드 시작");
         updateModules();
