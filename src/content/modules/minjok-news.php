0d20af8e5b4fece90e88a0ba947a623da77c580a
Dongmin Lee <ldm2468@gmail.com>
2016-01-17 22:15:18 +0900

    dl 사용

--- src/content/modules/minjok-news.php
+++ src/content/modules/minjok-news.php
@@ -1,16 +1,16 @@
-<ul id="js-news" class="js-hidden">
+<dl class="dl-horizontal">
     <?php
     $f=file_get_contents("http://www.minjok.hs.kr/app/kumla_notice/kumla_all.html");
     if(preg_match_all('/<li>\[([^\]]+):([^\]]+)\](.*?)<br>/sim', $f, $m, PREG_SET_ORDER)){
         $length = count($m);
-        for ($i = 0; $i < $length - 3; $i++) {
+        for ($i = 0; $i < $length - 2; $i++) {
             $each = $m[$i];
 
             $type=trim($each[1]);
             $date=trim($each[2]);
             $msg=trim(strip_tags($each[3]));
-            ?><li class="news-item"><i class="fa fa-caret-right"></i><a href="http://www.minjok.hs.kr/members/" target="_new" style="color:black"><?php echo "<b>[$date] $type</b>: $msg"?></a></li><?php
+            ?><dt><?php echo "[$date] $type";?></dt><dd><a href="http://www.minjok.hs.kr/members/" target="_new" style="color:black"><?php echo "$msg"?></a></dd><?php
         }
     }
     ?>
-</ul>
+</dl>
