6d0d08bc00b7f9d7a5feb5afebfa894cfa317665
Dongmin Lee <ldm2468@gmail.com>
2016-01-18 23:25:21 +0900

    펑펑

--- src/content/modules/module.php
+++ src/content/modules/module.php
@@ -341,7 +341,7 @@ function defaultModule($module_name) {
 }
 
 function allModules($modules) {
-    echo $modules;
+    print_r($modules);
     foreach($modules as $module) {
         getModuleShell($module['name'], array_key_exists('options', $module['options'])
                   ? array_merge(defaultOptions($module['name']), $module['options']['options'])
