66102789820daaf329c5cdc965d688a118955760
Dongmin Lee <ldm2468@gmail.com>
2016-01-20 10:57:25 +0900

    순서

--- src/content/main.php
+++ src/content/main.php
@@ -22,12 +22,12 @@ function printContentPc(){
                     <i class="fa fa-exclamation-triangle"></i> 편집 모드를 종료하거나 레이아웃 저장 버튼을 누르기 전엔 레이아웃이 저장되지 않습니다.
                 </div>
                 <div style="margin-top: 6px;">
-                    예시 레이아웃 사용 전에 기존 레이아웃을 백업하는 것을 추천합니다.
                     <select id="example-layout" class="selectpicker" data-style="btn-primary" data-size="10" title="예시 레이아웃">
                         <option data-divider="true"></option>
                         <option value="colorful">알록달록</option>
                         <option value="warrior">키보드워리어</option>
                     </select>
+                    예시 레이아웃 적용 전에는 기존 레이아웃을 백업하는 것이 좋습니다.
                 </div>
                 <div style="margin-top: 6px;">
                     <select id="add-module" class="selectpicker" data-style="btn-primary" data-size="10" title="패널 추가">
