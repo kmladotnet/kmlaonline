f9178402aab9ea6a62ec77d8716d0b6bd2051c53
Dongmin Lee <ldm2468@gmail.com>
2016-01-25 01:26:43 +0900

    모양수정2

--- src/content/modules/minjok-news.php
+++ src/content/modules/minjok-news.php
@@ -1,4 +1,4 @@
-<div style="overflow: hidden; margin-bottom: 0">
+<div style="overflow: hidden; margin-bottom: 0;color: black; white-space: nowrap;">
     <?php
     $f=file_get_contents("http://www.minjok.hs.kr/app/kumla_notice/kumla_all.html");
     if(preg_match_all('/<li>\[([^\]]+):([^\]]+)\](.*?)<br>/sim', $f, $m, PREG_SET_ORDER)){
@@ -9,7 +9,7 @@
             $type=trim($each[1]);
             $date=trim($each[2]);
             $msg=str_replace('&amp;nbsp;', '', htmlspecialchars(trim(strip_tags($each[3])), ENT_IGNORE));
-            echo "[$date] $type";
+            echo "<b>[$date] $type</b>";
             ?>
             <a href="http://www.minjok.hs.kr/members/" target="_new" style="color: black; white-space: nowrap;"><?php echo $msg;?></a>
             <br>
