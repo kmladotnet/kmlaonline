a83a88790640da5752b193a394e43ea3c0fafd8b
Dongmin Lee <ldm2468@gmail.com>
2016-01-31 02:16:51 +0900

    디버깅 끝!

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-echo "<!--";
-var_dump($_SERVER);
-var_dump($_REQUEST);
-echo "-->";
 $includes=array();
 switch(isset($_GET['action'])?$_GET['action']:"main"){
 	case "main":	$fn="main"; break;
