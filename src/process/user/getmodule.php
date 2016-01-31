823e5f971a9c76babca75420e7df70788aee4629
Dongmin Lee <ldm2468@gmail.com>
2016-02-01 02:47:25 +0900

    새 모듈 불러올때 디폴트 존중

--- src/process/user/getmodule.php
+++ src/process/user/getmodule.php
@@ -1,5 +1,5 @@
 <?php
 require_once("src/content/modules/module.php");
 $post_data = json_decode($_REQUEST['json'], true);
-moduleContents($post_data['name'], $post_data['options']['options']);
+moduleContents($post_data['name'], array_merge(defaultOptions($post_data['name']), $post_data['options']['options']));
 ?>
--- src/process/user/getmodulelite.php
+++ src/process/user/getmodulelite.php
@@ -1,5 +1,5 @@
 <?php
 require_once("src/content/modules/module.php");
 $post_data = json_decode($_REQUEST['json'], true);
-moduleContentsLite($post_data['name'], $post_data['options']['options']);
+moduleContentsLite($post_data['name'],  array_merge(defaultOptions($post_data['name']), $post_data['options']['options']));
 ?>
