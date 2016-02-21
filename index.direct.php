4165a586791c16d2f7e92ef82e969fb2dec08d6a
Dongmin Lee <ldm2468@gmail.com>
2016-02-21 16:13:38 +0900

    script 버전+1

--- index.direct.php
+++ index.direct.php
@@ -68,7 +68,7 @@
     <script src="/js/lightbox.js"></script>
     <script src="/swfupload/swfupload.js"></script>
     <script src="/swfupload/swfupload.queue.js"></script>
-    <script src="/js/script.js?v=2.0"></script>
+    <script src="/js/script.js?v=3.0"></script>
     <link rel="stylesheet" href="/css/lightbox.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="/css/font.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="/sass-compiled/screen.css" type="text/css" media="screen" />
