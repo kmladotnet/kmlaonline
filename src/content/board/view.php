c8c934f3bdb9b828587b82ddea570f70157a1cc8
Dongmin Lee <ldm2468@gmail.com>
2016-03-23 00:51:00 +0900

    Return value, instead of throwing it away

--- src/content/board/view.php
+++ src/content/board/view.php
@@ -4,7 +4,7 @@ function getHue($hex) {
 }
 
 function getHash($id, $key) {
-    hash_hmac("md2", $id, $key);
+    return hash_hmac("crc32", $id, $key);
 }
 
 function printAttachList($article, $cat, $mode=0){
