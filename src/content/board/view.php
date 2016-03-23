26969838294cf54831dc2347dd23abe37ab083ba
Dongmin Lee <ldm2468@gmail.com>
2016-03-23 09:48:06 +0900

    Escape php

--- src/content/board/view.php
+++ src/content/board/view.php
@@ -197,7 +197,7 @@ function putCommentTree($parent,$root){
                         </div>
                         <div style="clear:both"></div>
                     </div>
-                <?php }
+                <?php } ?>
 			</div>
 			<div style="margin-left:20px;border-left-color: #DDD;border-left-style: dotted;border-left-width: 1px;"><?php putCommentTree($comment['n_id'],$root); ?></div>
 		<?php } ?>
