3087fbc2511a66cf3e9da012671cadaaf4cba941
Dongmin Lee <ldm2468@gmail.com>
2016-03-25 10:48:22 +0900

    Make bad post text size same as others

--- src/content/board/view.php
+++ src/content/board/view.php
@@ -184,7 +184,7 @@ function putCommentTree($parent,$root){
                     <span style="font-size:8pt;color:gray;"><?php echo date("Y-m-d H:i:s", $comment['n_writedate'])?></span>
                     <?php if($board_id!='picexhibit') { ?>
                         <div id="item_contents_<?php echo $comment['n_id'];?>"  <?php if($votes < 0) {
-                            echo 'style="color:rgb('.floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),')!important; font-size:0.9em!important;font-weight:normal!important;',($votes > -6 ? '' : 'display:none'),'"';
+                            echo 'style="color:rgb('.floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),')!important; font-size:1em!important;font-weight:normal!important;',($votes > -6 ? '' : 'display:none'),'"';
                         }
                         ?>>
                             <?php filterContent($comment['s_data']);?>
@@ -418,7 +418,7 @@ function printOneForumItem($article,$root,$suppress_comments=false) {
 			</div>
             <div class="item_contents" style="padding:10px">
                 <div id="item_contents_<?php echo $article['n_id'];?>" <?php if($votes < 0) {
-                    echo 'style="color:rgb('.floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),')!important; font-size:0.9em!important;font-weight:normal!important;',($votes > -6 ? '' : 'display:none'),'"';
+                    echo 'style="color:rgb('.floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),')!important; font-size:1em!important;font-weight:normal!important;',($votes > -6 ? '' : 'display:none'),'"';
                 }?>><?php
                     filterContent($article['s_data']);
                 ?></div>
@@ -463,7 +463,7 @@ function printOneForumItem($article,$root,$suppress_comments=false) {
             </div>
             <div class="item_contents" style="padding:10px">
                 <div id="item_contents_<?php echo $article['n_id'];?>" <?php if($votes < 0) {
-                    echo 'style="color:rgb('.floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),')!important; font-size:0.9em!important;font-weight:normal!important;',($votes > -6 ? '' : 'display:none'),'"';
+                    echo 'style="color:rgb('.floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),',',floor(255 * min(0.5, 0.2-$votes / 15)),')!important; font-size:1em!important;font-weight:normal!important;',($votes > -6 ? '' : 'display:none'),'"';
                     }?>><?php
                     filterContent($article['s_data']);
                 ?></div>
