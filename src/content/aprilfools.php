4dea732b2cce129f34d672809ee7fcf4fa8abdb0
Dongmin Lee <ldm2468@gmail.com>
2016-03-31 23:48:29 +0900

    april links are allowed

--- src/content/aprilfools.php
+++ src/content/aprilfools.php
@@ -30,7 +30,7 @@ function printContent() {
             <td class="t_left">보안프로그램을 한번에 다운받기 위한 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -38,7 +38,7 @@ function printContent() {
             <td class="t_left">로그인과 중요정보에 대한 전자서명통신 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -46,7 +46,7 @@ function printContent() {
             <td class="t_left">공인인증서 로그인과 거래내역에 대한 전자서명을 위한 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -54,7 +54,7 @@ function printContent() {
             <td class="t_left">비인가된 접근을 차단하고 해킹툴 및 바이러스를 검색하고 치료해 주는 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -62,7 +62,7 @@ function printContent() {
             <td class="t_left">키보드를 통해 입력되는 정보가 유출되거나 변조되지 않도록 보호해 주는 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -70,7 +70,7 @@ function printContent() {
             <td class="t_left">익명으로 올려진 여러가지 악성 글을 차단해주는 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -78,7 +78,7 @@ function printContent() {
             <td class="t_left">불법적인 웹컨텐츠 접근으로부터 웹페이지를 보호하는 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -86,7 +86,7 @@ function printContent() {
             <td class="t_left">악성 프로그램에 의해 웹 페이지가 변조 되는 것을 차단해주는 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -94,7 +94,7 @@ function printContent() {
             <td class="t_left">닷넷 선배님들이 남기고 가신 유산, 고스톱 게임을 실행할 수 있습니다.</td>
             <td>
                 미설치
-                <a href="/data/game/gostop.swf" class="btn btn-danger">설치하기</a>
+                <a href="/data/game/gostop.swf" class="april-link btn btn-danger">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -102,7 +102,7 @@ function printContent() {
             <td class="t_left">키보드를 통해 입력되는 정보가 유출되거나 변조되지 않도록 보호해 주는 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
         <tr>
@@ -110,18 +110,18 @@ function printContent() {
             <td class="t_left">오늘의 날짜 확인을 유도하는 프로그램입니다.</td>
             <td>
                 미설치
-                <a class="btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
+                <a class="april-link btn btn-danger" href="april-fools/KmlaActiveX.exe">설치하기</a>
             </td>
         </tr>
     </tbody>
 </table>
 
-    <a class="btn btn-primary" href="april-fools/KmlaActiveX.exe">모두 설치하기(권장)</a>
-      <a class="btn btn-default aprilfools-button" style="position: relative" href="<?php echo $april_link;?>">설치하지 않고 계속 (권장하지 않음)</a>
+    <a class="april-link btn btn-primary" href="april-fools/KmlaActiveX.exe">모두 설치하기(권장)</a>
+      <a class="april-link btn btn-default aprilfools-button" style="position: relative" href="<?php echo $april_link;?>">설치하지 않고 계속 (권장하지 않음)</a>
   </div>
 <script>
 $(".aprilfools-button").mouseover(function() {$(this).css("left", Math.random() * 1000)});
-$("a").not(".aprilfools-button").removeAttr("href");
+$("a").not(".april-link").not("".removeAttr("href");
 </script>
 <?php
 }
