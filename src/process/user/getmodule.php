e0724a460d07406b7792c48ceabeea5189ff4324
Dongmin Lee <ldm2468@gmail.com>
2016-04-16 13:48:27 +0900

    remove trailing ?>s

--- src/process/user/getmodule.php
+++ src/process/user/getmodule.php
@@ -2,4 +2,3 @@
 require_once("src/content/modules/module.php");
 $post_data = json_decode($_REQUEST['json'], true);
 moduleContents($post_data['name'], array_merge(defaultOptions($post_data['name']), $post_data['options']['options']));
-?>
