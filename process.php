00f8933cc16b2db64883b9e057975c5a93ebebab
Dongmin Lee <ldm2468@gmail.com>
2016-04-16 13:56:52 +0900

    prepare food api

--- process.php
+++ process.php
@@ -1,10 +1,6 @@
 <?php
 include "src/lib.php";
 $fn="src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
-if(file_exists($fn)){
+if(file_exists($fn)) {
 	include($fn);
-}else{
-    echo $fn;
-	//redirectTo("./"); // Change to 404
 }
-?>
