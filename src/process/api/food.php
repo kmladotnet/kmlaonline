9fb1b2319e0ff11c2ac947996467ca9bad67ea91
Dongmin Lee <ldm2468@gmail.com>
2016-04-17 12:05:14 +0900

    merge apis

--- src/process/api/food.php
+++ src/process/api/food.php
@@ -5,15 +5,31 @@ if(isset($_GET['year']) && isset($_GET['month']) && isset($_GET['day'])) {
     $d = intval($_GET['day']);
 } elseif(isset($_GET['time'])) {
     $t = intval($_GET['time']);
-    $y = date('Y', $t);
-    $m = date('n', $t);
-    $d = date('j', $t);
+    $y = intval(date('Y', $t));
+    $m = intval(date('n', $t));
+    $d = intval(date('j', $t));
+    $h = intval(date('G', $t));
+    $foodTime = 'error';
+    if($h < 8 || $h >= 22) $foodTime = 'breakfast';
+    elseif($h >= 8 && $h < 13) $foodTime = 'lunch';
+    else $foodTime = 'dinner';
+    if($h >= 22) {
+        $y = date('Y', strtotime('+1 day'));
+        $m = date('n', strtotime('+1 day'));
+        $d = date('j', strtotime('+1 day'));
+    }
 }
 
 $query = "SELECT s_mode, s_data FROM kmlaonline_schedule_table WHERE n_year=$y AND n_month=$m AND n_day=$d";
 $foodData = array();
-if($res = $mysqli->query($query)){
-    while ($row = $res->fetch_array(MYSQLI_ASSOC)) {
+$foodData['year'] = $y;
+$foodData['month'] = $m;
+$foodData['day'] = $d;
+if(isset($foodTime)) {
+    $foodData['food'] = $foodTime;
+}
+if($foodData = $mysqli->query($query)){
+    while ($row = $foodData->fetch_array(MYSQLI_ASSOC)) {
         if(substr($row['s_mode'], 0, 4) === 'food') {
             $foodTime = 'error';
             switch(substr($row['s_mode'], 5)) {
@@ -27,7 +43,7 @@ if($res = $mysqli->query($query)){
             $foodData[$foodTime] = $row['s_data'];
         }
     }
-    $res->close();
+    $foodData->close();
     if($mysqli->more_results()) $mysqli->next_result();
 }
 echo json_encode($foodData);
