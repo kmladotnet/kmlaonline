24989b80d24379a5d70254965628c278b4e8baf6
Dongmin Lee <ldm2468@gmail.com>
2016-05-13 11:56:11 +0900

    fix colors

--- index.direct.php
+++ index.direct.php
@@ -134,8 +134,8 @@
     <div class="hide-on-upper-panel menu-shadow" style="left:0;width:100%;right:0;display:block;height:40px;box-shadow: 0 0px 30px rgba(0, 0, 0, 0.2);">
         <div class="total-header-menu-extend" style="position:absolute;left:0;width:100%;right:0;display:block;height:40px;"></div>
     </div>
-    <div class="hide-on-upper-panel" style="position:absolute;background:rgb(21,70,107);left:0;width:100%;right:0;display:block;height:40px;z-index:-5;">
-        <div class="total-header-extend-1" style="position:absolute;background:rgb(21,70,107);left:0;width:100%;right:0;display:block;height:40px;"></div>
+    <div class="hide-on-upper-panel" style="position:absolute;background:#0b518a;left:0;width:100%;right:0;display:block;height:40px;z-index:-5;">
+        <div class="total-header-extend-1" style="position:absolute;background:#0b518a;left:0;width:100%;right:0;display:block;height:40px;"></div>
     </div>
     <div class="hide-on-upper-panel" style="position:absolute;left:0;width:100%;right:0;display:block;height:160px;top:40px;z-index:-5;">
         <div class="total-header-extend-1" style="position:absolute;left:0;width:100%;right:0;display:block;height:160px;"></div>
