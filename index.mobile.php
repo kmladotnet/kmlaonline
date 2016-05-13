06ac8665c0a7be993f973987f46b9999fe5f34a1
Dongmin Lee <ldm2468@gmail.com>
2016-05-13 11:52:09 +0900

    renew colors(2)

--- index.mobile.php
+++ index.mobile.php
@@ -20,12 +20,12 @@
     <link rel="icon" type="image/png" href="/icon/android-chrome-192x192.png" sizes="192x192">
     <link rel="icon" type="image/png" href="/icon/favicon-16x16.png" sizes="16x16">
     <link rel="manifest" href="/icon/manifest.json">
-    <link rel="mask-icon" href="/icon/safari-pinned-tab.svg" color="#15466b">
+    <link rel="mask-icon" href="/icon/safari-pinned-tab.svg" color="#0b518a">
     <link rel="shortcut icon" href="/icon/favicon.ico">
-    <meta name="msapplication-TileColor" content="#15466b">
+    <meta name="msapplication-TileColor" content="#0b518a">
     <meta name="msapplication-TileImage" content="/icon/mstile-144x144.png">
     <meta name="msapplication-config" content="/icon/browserconfig.xml">
-    <meta name="theme-color" content="#15466b">
+    <meta name="theme-color" content="#0b518a">
 
 	<base href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/" />
     <!-- gridstack -->
