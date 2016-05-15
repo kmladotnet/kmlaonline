2c8b2d9f75a46ab31e533f6e0c23e6198fc1b5ba
Dongmin Lee <ldm2468@gmail.com>
2016-05-15 17:07:20 +0900

    make comment trees easier to follow

--- src/content/board/view.php
+++ src/content/board/view.php
@@ -258,7 +258,7 @@ function putCommentTree($parent,$root){
                     <div style="clear:both"></div>
                 </div>
 			</div>
-			<div style="margin-left:20px;border-left-color: #DDD;border-left-style: dotted;border-left-width: 1px;"><?php putCommentTree($comment['n_id'],$root); ?></div>
+			<div style="margin-left:20px;padding-left:8px;border-left-color: #DDD;border-left-style: dotted;border-left-width: 1px;"><?php putCommentTree($comment['n_id'],$root); ?></div>
 		<?php } ?>
 	</div><?php
 }
