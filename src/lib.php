101b095bb8aab41a45a5245b239c5ddee0b87f87
Dongmin Lee <ldm2468@gmail.com>
2016-05-19 19:36:38 +0900

    fixing locale

--- src/lib.php
+++ src/lib.php
@@ -44,7 +44,7 @@ if(isset($_COOKIE['remember_user'])) { // 자동 로그인
 }
 session_write_close();
 /********************** END INITIALIZATION SESSION ************************/
-setlocale(LC_ALL, 'ko_KR.UTF-8');
+setlocale(LC_TIME, 'Korean');
 
 if(isset($_SESSION['user'])) {
 	$me = $member->getMember($_SESSION['user']);
