651f0bb3d6b316b9b0e554b24a7dc7034eb03881
Dongmin Lee <ldm2468@gmail.com>
2016-07-11 21:01:46 +0900

    make stars a bit bigger

--- src/content/modules/menu.php
+++ src/content/modules/menu.php
@@ -68,7 +68,7 @@ function printMenu($allDay = false) {
     <?php if(getTheme($me)['beta']) { ?>
         <script>
             $(function() {
-               $(".rate").rateYo({fullStar: true, starWidth: "14px"}).css("display", "inline-block");
+               $(".rate").rateYo({fullStar: true, starWidth: "16px"}).css("display", "inline-block");
             });
         </script>
     <?php }
