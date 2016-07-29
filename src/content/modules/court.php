f6f7202ae612da5d4078bba9b5f4cb758313522b
Dongmin Lee <ldm2468@gmail.com>
2016-07-30 01:29:10 +0900

    check null

--- src/content/modules/court.php
+++ src/content/modules/court.php
@@ -1,19 +1,22 @@
 <?php
 $courtPost = getLatestCourtPost();
-$guilty = goesToCourt($me['s_name'], $courtPost);
-$isDasan = isCourtDasan($courtPost);
-?>
-<div class="main-block-content">
-    <div class="court-face <?php echo $guilty ? "guilty" : "innocent"; ?>">
-        <i class="fa fa-<?php echo $guilty ? "frown" : "smile"; ?>-o" aria-hidden="true"></i>
+if($courtPost !== null) {
+    $guilty = goesToCourt($me['s_name'], $courtPost);
+    $isDasan = isCourtDasan($courtPost);
+    ?>
+    <div class="main-block-content">
+        <div class="court-face <?php echo $guilty ? "guilty" : "innocent"; ?>">
+            <i class="fa fa-<?php echo $guilty ? "frown" : "smile"; ?>-o" aria-hidden="true"></i>
+        </div>
+        <div style="text-align: center; font-size: 13pt;">
+            <?php if($guilty) { ?>
+                이번 법정은
+                <span class="court-location <?php echo $isDasan ? "dasan" : "gym"; ?>">
+                    <?php echo $isDasan ? "소강당" : "체육관"; ?>
+                </span>
+            <?php } ?>
+            <a href="/board/student_judicial/view/<?php echo $courtPost['n_id']; ?>">법정리스트</a>
+        </div>
     </div>
-    <div style="text-align: center; font-size: 13pt;">
-        <?php if($guilty) { ?>
-            이번 법정은
-            <span class="court-location <?php echo $isDasan ? "dasan" : "gym"; ?>">
-                <?php echo $isDasan ? "소강당" : "체육관"; ?>
-            </span>
-        <?php } ?>
-        <a href="/board/student_judicial/view/<?php echo $courtPost['n_id']; ?>">법정리스트</a>
-    </div>
-</div>
+<?php
+}
