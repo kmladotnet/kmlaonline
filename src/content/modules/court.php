3b7a2b27b550754582ba2ef8a41e5ee518bc51e5
Dongmin Lee <ldm2468@gmail.com>
2016-07-30 01:23:34 +0900

    don't show location if innocent

--- src/content/modules/court.php
+++ src/content/modules/court.php
@@ -8,10 +8,12 @@ $isDasan = isCourtDasan($courtPost);
         <i class="fa fa-<?php echo $guilty ? "frown" : "smile"; ?>-o" aria-hidden="true"></i>
     </div>
     <div style="text-align: center; font-size: 13pt;">
-        이번 법정은
-        <span class="court-location <?php echo $isDasan ? "dasan" : "gym"; ?>">
-            <?php echo $isDasan ? "소강당" : "체육관"; ?>
-        </span>
+        <?php if($guilty) { ?>
+            이번 법정은
+            <span class="court-location <?php echo $isDasan ? "dasan" : "gym"; ?>">
+                <?php echo $isDasan ? "소강당" : "체육관"; ?>
+            </span>
+        <?php } ?>
         <a href="/board/student_judicial/view/<?php echo $courtPost['n_id']; ?>">법정리스트</a>
     </div>
 </div>
