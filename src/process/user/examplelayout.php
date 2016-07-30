8a8a102544871d8fb3e4a5ef3c81ced6fa6b3806
Dongmin Lee <ldm2468@gmail.com>
2016-07-30 19:39:27 +0900

    new colorful default

--- src/process/user/examplelayout.php
+++ src/process/user/examplelayout.php
@@ -2,18 +2,19 @@
 $layout = '';
 switch($_POST['name']) {
     case 'colorful':
-        $layout = '[{"name":"minjok-news","options":{"x":0,"y":0,"w":5,"h":4,"options":{"color":"info"}}},{"name":"menu","options":{"x":5,"y":0,"w":5,"h":5,"options":{"color":"success","all-day":true}}},{"name":"birthday","options":{"x":10,"y":0,"w":2,"h":2,"options":{"color":"warning"}}},{"name":"weather","options":{"x":10,"y":2,"w":2,"h":3,"options":{"color":"info"}}},{"name":"important","options":{"x":0,"y":4,"w":5,"h":6,"options":{"color":"success","show-cat":false,"show-title":true,"show-name":false,"show-date":true}}},{"name":"kmlaboard","options":{"x":5,"y":5,"w":7,"h":5,"options":{"color":"warning"}}},{"name":"article-list","options":{"x":0,"y":10,"w":5,"h":6,"options":{"color":"info","cat":["77"],"show-cat":false,"show-title":true,"show-name":true,"show-date":true,"title":"큼라 카페"}}}]';
+        $layout = '[{"name":"minjok-news","options":{"x":0,"y":0,"w":6,"h":4,"options":{"color":"warning"}}},{"name":"menu","options":{"x":8,"y":0,"w":2,"h":5,"options":{"color":"success","all-day":false}}},{"name":"birthday","options":{"x":10,"y":0,"w":2,"h":2,"options":{"color":"danger"}}},{"name":"weather","options":{"x":6,"y":0,"w":2,"h":5,"options":{"color":"info"}}},{"name":"important","options":{"x":6,"y":5,"w":6,"h":4,"options":{"color":"danger","num":"10","show-cat":false,"show-title":true,"show-name":false,"show-date":true}}},{"name":"kmlaboard","options":{"x":0,"y":4,"w":6,"h":5,"options":{"color":"success"}}},{"name":"article-list","options":{"x":0,"y":9,"w":5,"h":5,"options":{"color":"info","cat":["77"],"num":"8","show-cat":false,"show-title":true,"show-name":true,"show-date":true,"title":"큼라 카페"}}},{"name":"court","options":{"x":10,"y":2,"w":2,"h":3,"options":[]}}]';
         $modules = json_decode($layout, true);
         $my_articles = json_decode( <<<JSON
         {
           "name":"article-list",
           "options":{
              "x":5,
-             "y":10,
+             "y":9,
              "w":7,
-             "h":6,
+             "h":5,
              "options":{
-                "color" : "success",
+                "color" : "warning",
+                "num": 8,
                 "title":"내 게시판"
              }
           }
