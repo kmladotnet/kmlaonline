d0de78de8073482c177baf038449b51815fbb24b
Dongmin Lee <ldm2468@gmail.com>
2016-08-19 20:21:37 +0900

    enable register

--- src/process/check/register.php
+++ src/process/check/register.php
@@ -1,2 +1,2 @@
-<script type="text/javascript">alert("가입기간이 아닙니다.");location.href="/";</script>
-<!--<?php redirectTo("./user/register"); ?>-->
+<!-- <script type="text/javascript">alert("가입기간이 아닙니다.");location.href="/";</script> -->
+<?php redirectTo("./user/register"); ?>
