46ad9bde28793f71296d965a30dad9bddd2435f3
Dongmin Lee <ldm@ldm2468.com>
2016-11-16 01:21:02 +0900

    css version increment

--- index.direct.php
+++ index.direct.php
@@ -78,7 +78,7 @@
         </script>
     <?php } ?>
     <link rel="stylesheet" href="/css/font.css" type="text/css" media="screen" />
-    <link rel="stylesheet" href="/sass-compiled/screen.css?v=5" type="text/css" media="screen" />
+    <link rel="stylesheet" href="/sass-compiled/screen.css?v=6" type="text/css" media="screen" />
 
     <?php
     if($april_fools) {
