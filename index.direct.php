4f49c4160939f6a67248c50dd3d9c17ec72ee06d
Dongmin Lee <ldm@ldm2468.com>
2017-01-08 01:22:42 +0900

    version up v3

--- index.direct.php
+++ index.direct.php
@@ -34,7 +34,7 @@
     <base href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/" />
 
     <!-- gridstack -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/gridstack.js/0.2.5/gridstack.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/gridstack.js/0.2.6/gridstack.min.css" />
 
     <!-- jquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
