7d0916d3ca71a42ccfc0a42efd0a484d7c2eb0f3
Hyeonjae Kim <hyeonjae42@gmail.com>
2017-01-30 01:41:27 +0900

    unblock register

--- src/process/check/register.php
+++ src/process/check/register.php
@@ -1,2 +1,3 @@
-<script type="text/javascript">alert("가입기간이 아닙니다.");location.href="/";</script>
-<?php if(false) redirectTo("./user/register"); ?>
+<!--Reserved for blocking new register-->
+<!--<script type="text/javascript">alert("가입기간이 아닙니다.");location.href="/";</script>-->
+<?php if(true) redirectTo("./user/register"); ?>
