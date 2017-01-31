aa5128aa77ee9e2c6c7c96e76da25c8158fed587
Hyeonjae Kim <Hyeonjae Kim>
2017-01-31 20:29:46 +0900

    Open register page(2)

--- src/process/check/register.php
+++ src/process/check/register.php
@@ -1,3 +1,3 @@
 <!--Reserved for blocking new register-->
-<script type="text/javascript">alert("가입기간이 아닙니다.");location.href="/";</script>
-<?php if(false) redirectTo("./user/register"); ?>
+<!--<script type="text/javascript">alert("가입기간이 아닙니다.");location.href="/";</script>-->
+<?php if(true) redirectTo("./user/register"); ?>
