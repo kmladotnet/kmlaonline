5741f8158eb2a8e067658c01e45c6ecb1530463e
Hyeonjae Kim <Hyeonjae Kim>
2017-02-02 01:12:55 +0900

    Reset password page test(3 - block)

--- src/content/user/resetpwd.php
+++ src/content/user/resetpwd.php
@@ -1,7 +1,8 @@
 <?php
-if(true) redirectTo("./user/resetpwd");
 $title = lang("user", "test_reset_password", "title") . " - " . $title;
 function printContent(){ ?>
+
     <script src="//cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.9.0/validator.min.js"></script>
+    <script type="text/javascript">alert("테스트 중입니다.");location.href="/";</script>
     <?php
 }
