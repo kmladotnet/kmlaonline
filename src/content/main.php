131b0e8d466d69ed29622afb92b06e63c7e9029e
Hyeonjae Kim <Hyeonjae Kim>
2017-02-04 14:38:40 +0900

    test

--- src/content/main.php
+++ src/content/main.php
@@ -341,6 +341,20 @@ JSON
         $('.grid-stack').data('gridstack').disable();
     });
     </script>
+    <script>
+        require(['/js/converse'], function (converse) {
+            (function () {
+                var $ = converse.env.jQuery;
+                $.extend( $.easing, {
+                    easeInOutExpo: function (x, t, b, c, d) {
+                        if (t == 0) return b;
+                        if (t == d) return b + c;
+                        if ((t /= d / 2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+                        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+                    },
+                });
+            })
+        });
 	<?php
 }
 ?>
