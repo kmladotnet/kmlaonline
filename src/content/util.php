c9c39a58f0f838d445a6e9f8e8b2d25cc3a35146
Hyeonjae Kim <Hyeonjae Kim>
2017-02-04 15:38:23 +0900

    test

--- src/content/util.php
+++ src/content/util.php
@@ -3,5 +3,4 @@ $sub=basename($_GET['sub']);
 if(!file_exists("src/content/util/$sub.php")) return die404();
 if(file_exists("js/content/util/$sub.js")) $includes[]="/js/content/util/$sub.js";
 if(file_exists("css/content/util/$sub.css")) $includes[]="/css/content/util/$sub.css";
-if(file_exists("js/converse/bootstrap/dist/css/$sub.css")) $includes[] = "/js/converse/bootstrap/dist/css/$sub.css";
 include "src/content/util/$sub.php";
