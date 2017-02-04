192ea4da08a4b1c2c88c7f2f1347b654d541e273
Hyeonjae Kim <Hyeonjae Kim>
2017-02-04 15:42:17 +0900

    test

--- index.direct.php
+++ index.direct.php
@@ -69,8 +69,8 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.1.1/jquery.rateyo.min.js"></script>
 
     <!-- converse.js-->
-    <!--link rel="stylesheet" type="text/css" media = "screen" href="css/converse/converse.css"/>
-    <script data-main="main" src="js/converse/requirejs/require.js"></script-->
+    <link rel="stylesheet" type="text/css" media = "screen" href="css/converse/converse.css"/>
+    <script data-main="main" src="js/converse/requirejs/require.js"></script>
 
     <!-- other -->
     <script src="/swfupload/swfupload.js"></script>
