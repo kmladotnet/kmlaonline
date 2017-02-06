f27c5e727ec2f24210f35d92ac982cd70cfbcef3
Hyeonjae Kim <Hyeonjae Kim>
2017-02-06 18:52:51 +0900

    Test a reset page

--- src/process/check/resetpwd.php
+++ src/process/check/resetpwd.php
@@ -1,2 +1,2 @@
-<!--script type="text/javascript">alert("테스팅 기간입니다. 새로운 서비스 준비중에요! ㅇㅅㅇ");location.href="/";</script-->
-<?php if(true) redirectTo("./user/resetpwd"); ?>
+<script type="text/javascript">alert("테스팅 기간입니다. 새로운 서비스 준비중에요! ㅇㅅㅇ");location.href="/";</script>
+<?php if(false) redirectTo("./user/resetpwd"); ?>
