46c257f4cd32ee787b3b7aa899857b5995277292
Hyeonjae Kim <Hyeonjae Kim>
2017-02-06 16:47:50 +0900

    Test a reset page

--- src/content/user/login.php
+++ src/content/user/login.php
@@ -178,7 +178,7 @@ function printContent(){
     <form method="post" action="./check" id="downform_login" onsubmit="return true;">
         <input type="hidden" id="downform_login_action" name="action" value="resetpwd" />
         <input type="hidden" name="returnto" value="<?php echo ((isset($_REQUEST['returnto']) && $_REQUEST['returnto']!=" ")?$_REQUEST['returnto']:"/ ")?>" />
-        <div style="float:right"><button class="btn btn-default" style="margin-right:5px;border-radius:5px;" onclick="$('#downform_login_action').val('resetpwd');$('#downform_login').submit();">회원가입</button></div>
+        <div style="float:right"><button class="btn btn-default" style="margin-right:5px;border-radius:5px;" onclick="$('#downform_login_action').val('resetpwd');$('#downform_login').submit();">테스팅 버튼</button></div>
     </form>
     </div>
     <script src="/js/content/user/login.js"></script>
