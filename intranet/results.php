19ec9134ccfb3c2b4bc948c9f0cd1a35d214b4fd
Hyeonjae Kim <Hyeonjae Kim>
2017-02-06 23:04:04 +0900

    Trial of building intranet

---
+++ intranet/results.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book-O-Roma Search Results</title>
+</head>
+<body>
+    <h1>Book-O-Rama Search Results</h1>
+    <?php
+        $searchtype = $_POST['searchtype'];
+        $searchterm = trim($_POST['serachterm']);
+
+        if(!$searchterm || !$serachterm) {
+            echo 'You have not entered search details. Please go back and try again.';
+            exit;
+        }
+
+        //Gets the current configuration setting of magic_quotes_gpc
+        if(!get_magic_quotes_gpc()) {
+            $serachtype = addslashes($searchtype);
+            $searchterm = addslashes($searchterm);
+        }
+
+        @ $db = new mysqli('localhost', 'bookorama', 'boororama123', 'books');
+
+        if(mysqli_connect_errno()) {
+            echo 'Error: Could not connect to database. Please try again later.';
+            exit;
+        }
+?>
+</body>
+</html>
