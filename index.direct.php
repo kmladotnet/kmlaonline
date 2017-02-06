71a605e0924be675032c0382ebd24fa3a71ab9bb
Dongmin Lee <ldm@ldm2468.com>
2017-02-07 01:26:43 +0900

    new icons

--- index.direct.php
+++ index.direct.php
@@ -9,26 +9,13 @@
 
     <!-- favicons -->
     <?php //수정할 때 http://realfavicongenerator.net/ 이용할 것. ?>
-    <link rel="apple-touch-icon" sizes="57x57" href="/icon/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/icon/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/icon/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/icon/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/icon/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/icon/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/icon/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/icon/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" href="/icon/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/icon/favicon-194x194.png" sizes="194x194">
-    <link rel="icon" type="image/png" href="/icon/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="/icon/android-chrome-192x192.png" sizes="192x192">
-    <link rel="icon" type="image/png" href="/icon/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/icon/manifest.json">
-    <link rel="mask-icon" href="/icon/safari-pinned-tab.svg" color="#074275">
-    <link rel="shortcut icon" href="/icon/favicon.ico">
-    <meta name="msapplication-TileColor" content="#074275">
-    <meta name="msapplication-TileImage" content="/icon/mstile-144x144.png">
-    <meta name="msapplication-config" content="/icon/browserconfig.xml">
+    <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png?v=XBrA5kwEdx">
+    <link rel="icon" type="image/png" href="/icon/favicon-32x32.png?v=XBrA5kwEdx" sizes="32x32">
+    <link rel="icon" type="image/png" href="/icon/favicon-16x16.png?v=XBrA5kwEdx" sizes="16x16">
+    <link rel="manifest" href="/icon/manifest.json?v=XBrA5kwEdx">
+    <link rel="mask-icon" href="/icon/safari-pinned-tab.svg?v=XBrA5kwEdx" color="#074275">
+    <link rel="shortcut icon" href="/icon/favicon.ico?v=XBrA5kwEdx">
+    <meta name="msapplication-config" content="/icon/browserconfig.xml?v=XBrA5kwEdx">
     <meta name="theme-color" content="#074275">
 
     <base href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/" />
