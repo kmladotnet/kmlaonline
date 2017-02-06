bb81a84f71181bd99438c04158e7895f7dbcc650
Hyeonjae Kim <Hyeonjae Kim>
2017-02-06 16:55:36 +0900

    Test a reset page

--- src/content/user/resetpwd.php
+++ src/content/user/resetpwd.php
@@ -3,7 +3,7 @@ $title = lang("user", "test_reset_password", "title") . " - " . $title;
 function printContent(){ ?>
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.9.0/validator.min.js"></script>
-    <!--script type="text/javascript">alert("테스트 중입니다.");location.href="/";</script-->
+    <script type="text/javascript">alert("테스트 중입니다.");location.href="/";</script>
     <?php
     global $max_level;
     insertOnLoadScript("putAlertOnLeave();");
