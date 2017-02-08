b82f45a23db5b8c95aead452c7d7e6dcc2135914
Hyeonjae Kim <Hyeonjae Kim>
2017-02-08 13:10:20 +0900

    trial

--- intranet/index.php
+++ intranet/index.php
@@ -6,7 +6,7 @@
 <body>
     <p>This is just a test page.</p>
     <?php
-        include 'lib.php';
+        include 'lib_real.php';
 
         if($db) echo "WOW";
         else echo "There is a problem";
---
+++ intranet/test/lib_real.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set("Asia/Seoul");
+include "intranet/presentool/PresentTool.php";
+require "lib.php";
+?>
