69b859bbfcff512e0d24e77b9f6db60777393386
Hyeonjae Kim <Hyeonjae Kim>
2017-02-09 20:32:10 +0900

    justice_system change db_config_part: fix dumb prob

--- intranet/presentool/TotalArticleList.php
+++ intranet/presentool/TotalArticleList.php
@@ -14,11 +14,11 @@ class TotalArticleList{
     function prepareFirstUse(){
         $query = array();
         array_push($query, "CREATE TABLE IF NOT EXISTS `$this->table_data` (".
-                                "article_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,".
-                                "student_id INT DEFAULT 0,".
-                                "article_kind INT NOT NULL,".
-                                "accuse_date DATE,".
-                                "Primary Key (article_id),".
+                                "article_id BIGINT NOT NULL AUTO_INCREMENT, ".
+                                "student_id INT DEFAULT 0, ".
+                                "article_kind INT NOT NULL, ".
+                                "accuse_date DATE, ".
+                                "Primary Key (article_id), ".
                                 "Foreign Key (student_id) REFERENCES test_student_data (n_id))");
 
         $this->db->autocommit(false);
