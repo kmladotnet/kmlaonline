68490775aff63079b19df24a4d1173ae5bad41e9
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 00:16:12 +0900

    justice_system change db_config_part (fix stupid typos)

--- intranet/lib_real.php
+++ intranet/lib_real.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Seoul");
 include "presentool/PresentTool.php";
 
 session_start();
-$dbInit = true;
+//$dbInit = true;
 require "db-config.php";
 session_write_close();
 ?>
