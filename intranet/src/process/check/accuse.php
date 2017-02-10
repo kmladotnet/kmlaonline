8fb7948c55301dee429cc5b1988ac92809c80f23
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 14:03:20 +0900

    justice_system initial view config

--- intranet/src/process/check/accuse.php
+++ intranet/src/process/check/accuse.php
@@ -1,2 +1,2 @@
 <!--script type="text/javascript">alert("기소 기간이 아닙니다.");location.href="/intranet";</script-->
-<?php if(true) redirectTo("./user/accuse"); ?>
+<?php if(true) redirectTo("./intranet/user/accuse"); ?>
