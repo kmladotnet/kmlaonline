9cd3e1525ff3d0cb64cfe330b1c8455f36a4dcdc
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 18:38:27 +0900

    justice_system initial view (autocomplete test)

--- intranet/src/content/user/suggest_name.php
+++ intranet/src/content/user/suggest_name.php
@@ -15,7 +15,7 @@
             $a_json_row["id"] = $n_id;
             $a_json_row["value"] = $name;
             $a_json_row["label"] = $grade . "학년" . $name . "($student_id)";
-            array_push($a_json, $a_json_row);*/
+            array_push($a_json, $a_json_row);
         }
     }
 
