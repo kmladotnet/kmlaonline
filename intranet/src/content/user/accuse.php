e8ee700932a057ffc2f3b0f6c0070a3f21cae678
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 22:55:14 +0900

    justice_system accusing page config

--- intranet/src/content/user/accuse.php
+++ intranet/src/content/user/accuse.php
@@ -2,13 +2,50 @@
 $title = "기소";
 
 function printContent(){ ?>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.9.0/validator.min.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            $( "#datepicker1" ).datepicker({
+                dateFormat: 'mm-dd'
+            });
+        });
+    </script>
 
-
-    <p>It works!!</p>
-    <form action="ajax/user/accuse">
-        <input type="text" placeholder="이름" id="name" class="ui-autocomplete-input" autocomplete="on" />
-        <input type="text" placeholder="기소자" id="accuser" class="ui-autocomplete-input" autocomplete="off" />
-        <input type="text" placeholder="항목" id="article_kind" class="ui-autocomplete-input" autocomplete="on" />
+    <form data-toggle="validator" data-delay="100" action="ajax/user/accuse" method="post" enctype="multipart/form-data" onsubmit="window.onbeforeunload=null;">
+        <input type="hidden" name="prev_url" value="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']) ?>" />
+        <div style="text-align:center; width:100%">
+            <h1><?php echo "새로운 기소 신청"; ?></h1>
+            <h2><?php echo "기소 항목" ?></h2>
+            <table style="margin:20px auto; width:800px;">
+                <tr>
+                    <th style="width:200px;">
+                        <h2>학생 이름</h2>
+                    </th>
+                    <th style="width:200px;">
+                        <h2>기소 일자</h2>
+                    </th>
+                    <th style="width:200px;">
+                        <h2>기소자</h2>
+                    </th>
+                    <th style="width:600px;">
+                        <h2>기소 항목</h2>
+                    </th>
+                </tr>
+                <tr>
+                    <th style="width:190px;">
+                        <input type="text" placeholder="이름" id="name" class="ui-autocomplete-input" autocomplete="on" />
+                    </th>
+                    <th style="width:190px;">
+                        <input type="text" placeholder="기소자" id="accuser" class="ui-autocomplete-input" autocomplete="off" />
+                    </th>
+                    <th style="width:190px;">
+                        <input type="text" placeholder="기소일" id="datepicker1" class="ui-autocomplete-input" autocomplete="off" />
+                    </th>
+                    <th style="width:580px;">
+                        <input type="text" placeholder="항목" id="article_kind" class="ui-autocomplete-input" autocomplete="on" />
+                    </th>
+                </tr>
+            </table>
     </form>
 
 <?php
