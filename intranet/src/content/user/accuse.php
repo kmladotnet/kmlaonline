30a70c118c84654b6002851b80a05f59e3634a94
Hyeonjae Kim <Hyeonjae Kim>
2017-02-11 00:25:59 +0900

    justice_system accusing page config

--- intranet/src/content/user/accuse.php
+++ intranet/src/content/user/accuse.php
@@ -2,7 +2,16 @@
 $title = "기소";
 
 function printContent(){ ?>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.9.0/validator.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(document).on("focus", '#article_table tr:last-child td:last-child', function() {
+                var table = $("#article_table");
+                table.append('<tr><p>WOW</p></tr>');
+            });
+        });
+    </script>
+
     <script type="text/javascript">
         $(function() {
             $( ".date_picker" ).each(function(){
@@ -13,6 +22,7 @@ function printContent(){ ?>
         });
     </script>
 
+    <script src="//cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.9.0/validator.min.js"></script>
 
 
     <form data-toggle="validator" data-delay="100" action="ajax/user/accuse" method="post" enctype="multipart/form-data" onsubmit="window.onbeforeunload=null;">
@@ -20,7 +30,7 @@ function printContent(){ ?>
         <div style="text-align:center; width:100%">
             <h1><?php echo "새로운 기소 신청"; ?></h1>
             <h2><?php echo "기소 항목" ?></h2>
-            <table style="margin:20px auto; width:800px;" id="article_table">
+            <table style="margin:20px auto; width:1200px;" id="article_table">
                 <tr>
                     <th style="width:200px;">
                         <h3>학생 이름</h3>
@@ -47,7 +57,7 @@ function printContent(){ ?>
                     </td>
                     </td>
                     <td style="width:580px;">
-                        <input type="text" placeholder="항목" id="article_kind_1" class="ui-autocomplete-input auto_article" autocomplete="on" />
+                        <input type="text" style="padding-left: 5px" placeholder="항목" id="article_kind_1" class="ui-autocomplete-input auto_article" autocomplete="on" />
                         <input type="hidden" id="article_id" value="0" />
                     </td>
                 </tr>
@@ -64,7 +74,7 @@ function printContent(){ ?>
                     </td>
 
                     <td style="width:580px;">
-                        <input type="text" placeholder="항목" id="article_kind_2" class="ui-autocomplete-input auto_article" autocomplete="on" />
+                        <input type="text" style="padding-left: 5px" placeholder="항목" id="article_kind_2" class="ui-autocomplete-input auto_article" autocomplete="on" />
                         <input type="hidden" id="article_id" value="0" />
                     </td>
                 </tr>
