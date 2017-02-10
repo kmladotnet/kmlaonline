a49a7b9b8fb970d440776eae4d02b7d0ee1fe6ab
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 15:27:36 +0900

    justice_system initial view (autocomplete search)

--- intranet/src/content/user/suggest_name.php
+++ intranet/src/content/user/suggest_name.php
@@ -1,5 +1,5 @@
 <?php
-    $term = trim(strip_tags($_GET[]));
+    $term = trim(strip_tags($_GET["term"]));
     $a_json = array();
     $a_json_row = array();
     if($data = $db -> query("SELECT * FROM test_student_data WHERE name LIKE '%$term%' ORDER BY n_id")) {
@@ -8,7 +8,7 @@
             $student_id = htmlentities(stripcslashes($row['student_id']));
             $name = htmlentities(stripcslashes($row['name']));
             $n_id = intval($row['n_id']);
-            $a_json_row["code"] = $n_id;
+            $a_json_row["id"] = $n_id;
             $a_json_row["value"] = $name;
             $a_json_row["label"] = $grade . "학년" . $name . "($student_id)";
             array_push($a_json, $a_json_row);
