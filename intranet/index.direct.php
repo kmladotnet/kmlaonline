d9d57d12713c3e9a022126b745281152262a044d
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 20:02:52 +0900

    justice_system initial view (how to change design?)

--- intranet/index.direct.php
+++ intranet/index.direct.php
@@ -30,6 +30,9 @@
     });
     </script>
 
+    <link rel="stylesheet" href="../css/font.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../sass-compiled/screen.css?v=6" type="text/css" media="screen" />
+
 
     <title>
         <?php echo $title ?>
