c90177aa8b7c408905bb5d1cecc9705a7cf1493e
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 14:50:43 +0900

    justice_system initial view config (http -> https)

--- intranet/src/content/user/accuse.php
+++ intranet/src/content/user/accuse.php
@@ -2,7 +2,7 @@
 $title = "기소";
 
 function printContent(){ ?>
-    <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"</script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.min.js"</script>
     <script src="intranet/src/jquery_ui/jquery-ui.js"></script>
     <p>It works!!</p>
     <form action="ajax/user/accuse">
