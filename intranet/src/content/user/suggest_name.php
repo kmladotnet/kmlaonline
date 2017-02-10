e44bc380017f35baee80d2b52c5e7d267b4ead32
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 18:29:23 +0900

    justice_system initial view (autocomplete test)

--- intranet/src/content/user/suggest_name.php
+++ intranet/src/content/user/suggest_name.php
@@ -5,7 +5,7 @@
     $a_json = array();
     $a_json_row = array();
     $query = "SELECT * FROM test_student_data WHERE name LIKE '%$term%' ORDER BY n_id";
-
+    echo $query;
     if($data = $db -> query($query)) {
         while($row = mysqli_fetch_array($data)) {
             $grade = intval($row['grade']);
