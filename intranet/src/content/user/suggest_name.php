5d03442be0366bb0f97a2856345014a1a908b5e2
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 18:39:57 +0900

    justice_system initial view (autocomplete test)

--- intranet/src/content/user/suggest_name.php
+++ intranet/src/content/user/suggest_name.php
@@ -9,12 +9,12 @@
     if($data = $db -> query($query)) {
         while($row = mysqli_fetch_array($data)) {
             $grade = intval($row['n_grade']);
-            $student_id = htmlentities(stripcslashes($row['student_id']));
-            $name = htmlentities(stripcslashes($row['name']));
+            $student_id = htmlentities(stripslashes($row['student_id']));
+            $name = htmlentities(stripslashes($row['name']));
             $n_id = intval($row['n_id']);
             $a_json_row["id"] = $n_id;
             $a_json_row["value"] = $name;
-            $a_json_row["label"] = $grade . "학년" . $name . "($student_id)";
+            $a_json_row["label"] = $grade . "학년" . $name . "(" . $student_id . ")";
             array_push($a_json, $a_json_row);
         }
     }
