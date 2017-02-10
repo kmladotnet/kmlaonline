07835a0e540d0590c93000c08e5535512b5bfaa0
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 18:53:05 +0900

    justice_system initial view (autocomplete test)

--- intranet/index.direct.php
+++ intranet/index.direct.php
@@ -10,7 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
-    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.min.js"</script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
     <script src="../jquery_ui/jquery-ui.js"></script>
     <script type="text/javascript">
     $(document).ready(function($){
