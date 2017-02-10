dde51464028bcfbee0f6b6792d5107c17e8f60ef
Hyeonjae Kim <Hyeonjae Kim>
2017-02-11 00:08:36 +0900

    justice_system accusing page config

--- intranet/src/content/user/accuse.php
+++ intranet/src/content/user/accuse.php
@@ -37,33 +37,33 @@ function printContent(){ ?>
                 </tr>
                 <tr>
                     <td style="width:190px;">
-                        <input type="text" placeholder="이름" id="name" class="ui-autocomplete-input auto_name" autocomplete="on" />
+                        <input type="text" placeholder="이름" id="name_1" class="ui-autocomplete-input auto_name" autocomplete="on" />
                         <input type="hidden" id="n_student" class="ui-input" value="0" />
                     </td>
                     <td style="width:190px;">
-                        <input type="text" placeholder="기소자" id="accuser" class="ui-autocomplete-input" autocomplete="off" />
+                        <input type="text" placeholder="기소자" id="accuser_1" class="ui-autocomplete-input" autocomplete="off" />
                     </td>
                     <td style="width:190px;">
-                        <input type="text" placeholder="기소일" id="datepicker1" class="ui-autocomplete-input date_picker" autocomplete="off" />
+                        <input type="text" placeholder="기소일" id="datepicker_1" class="ui-autocomplete-input date_picker" autocomplete="off" />
                     </td>
                     <td style="width:580px;">
-                        <input type="text" placeholder="항목" id="article_kind" class="ui-autocomplete-input auto_article" autocomplete="on" />
+                        <input type="text" placeholder="항목" id="article_kind_1" class="ui-autocomplete-input auto_article" autocomplete="on" />
                         <input type="hidden" id="article_id" value="0" />
                     </td>
                 </tr>
                 <tr>
                     <td style="width:190px;">
-                        <input type="text" placeholder="이름" id="name" class="ui-autocomplete-input auto_name" autocomplete="on" />
+                        <input type="text" placeholder="이름" id="name_1" class="ui-autocomplete-input auto_name" autocomplete="on" />
                         <input type="hidden" id="n_student" class="ui-input" value="0" />
                     </td>
                     <td style="width:190px;">
-                        <input type="text" placeholder="기소자" id="accuser" class="ui-autocomplete-input" autocomplete="off" />
+                        <input type="text" placeholder="기소자" id="accuser_2" class="ui-autocomplete-input" autocomplete="off" />
                     </td>
                     <td style="width:190px;">
-                        <input type="text" placeholder="기소일" id="datepicker2" class="ui-autocomplete-input date_picker" autocomplete="off" />
+                        <input type="text" placeholder="기소일" id="datepicker_2" class="ui-autocomplete-input date_picker" autocomplete="off" />
                     </td>
                     <td style="width:580px;">
-                        <input type="text" placeholder="항목" id="article_kind" class="ui-autocomplete-input auto_article" autocomplete="on" />
+                        <input type="text" placeholder="항목" id="article_kind_2" class="ui-autocomplete-input auto_article" autocomplete="on" />
                         <input type="hidden" id="article_id" value="0" />
                     </td>
                 </tr>
