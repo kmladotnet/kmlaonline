4af2341ca9d90bfcef4a6d238de3a81e9dd782f8
Hyeonjae Kim <Hyeonjae Kim>
2017-02-10 19:18:55 +0900

    justice_system initial view ( )

--- intranet/src/content/user/accuse.php
+++ intranet/src/content/user/accuse.php
@@ -6,7 +6,7 @@ function printContent(){ ?>
 
     <p>It works!!</p>
     <form action="ajax/user/accuse">
-        <input type="text" placeholder="이름" id="name" class="ui-autocomplete-input" autocomplete="off" />
+        <input type="text" placeholder="이름" id="name" class="ui-autocomplete-input" autocomplete="on" />
         <script type="text/javascript">
 
         </script>
