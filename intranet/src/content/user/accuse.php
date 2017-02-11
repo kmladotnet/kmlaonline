63145030f9991ee0f624ca3c10d76ce615ae114f
Hyeonjae Kim <Hyeonjae Kim>
2017-02-11 11:02:20 +0900

    judicial automation table row auto-increase test

--- intranet/src/content/user/accuse.php
+++ intranet/src/content/user/accuse.php
@@ -95,7 +95,7 @@ function printContent(){ ?>
             addNewRow();
         }
         console.log(event.which);
-        console.log($(this).closest("tr"));
+        console.log($(this).closest("tr").is(":last-child"));
     });
     </script>
 <?php
