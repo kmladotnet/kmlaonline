7e2d829e20267878f5f0ab5a449bd3d0313634c3
Hyeonjae Kim <Hyeonjae Kim>
2017-02-11 11:15:02 +0900

    judicial automation table row auto-increase test

--- intranet/src/content/user/accuse.php
+++ intranet/src/content/user/accuse.php
@@ -11,7 +11,7 @@ function printContent(){ ?>
             });
         });*/
         function addNewRow(){
-            var table = $("#article_table");
+            var table = $("#article_table_body");
             table.append("<tr>\r\n                    <td style=\"width:190px;\">\r\n                        <input type=\"text\" style=\"text-align:center\" placeholder=\"\uC774\uB984\" id=\"name_1\" class=\"ui-autocomplete-input auto_name\" autocomplete=\"on\" \/>\r\n                        <input type=\"hidden\" id=\"n_student\" class=\"ui-input\" value=\"0\" \/>\r\n                    <\/td>\r\n                    <td style=\"width:190px;\">\r\n                        <input type=\"text\" style=\"text-align:center\" placeholder=\"\uAE30\uC18C\uC77C\" id=\"datepicker_2\" class=\"ui-autocomplete-input date_picker\" autocomplete=\"off\" \/>\r\n                    <\/td>\r\n                    <td style=\"width:190px;\">\r\n                        <input type=\"text\" style=\"text-align:center\" placeholder=\"\uAE30\uC18C\uC790\" id=\"accuser_2\" class=\"ui-autocomplete-input\" autocomplete=\"off\" \/>\r\n                    <\/td>\r\n\r\n                    <td style=\"width:400px;\">\r\n                        <input type=\"text\" style=\"text-align:center; width:380px\" placeholder=\"\uD56D\uBAA9\" id=\"article_kind_2\" class=\"ui-autocomplete-input auto_article\" autocomplete=\"on\" \/>\r\n                        <input type=\"hidden\" id=\"article_id\" value=\"0\" \/>\r\n                    <\/td>\r\n                <\/tr>");
         }
     </script>
@@ -35,54 +35,56 @@ function printContent(){ ?>
             <h1><?php echo "새로운 기소 신청"; ?></h1>
             <h2><?php echo "기소 항목" ?></h2>
             <table style="margin:50px auto; width:1000px;" id="article_table">
-                <tr>
-                    <th style="width:200px;">
-                        <h3>학생 이름</h3>
-                    </th>
-                    <th style="width:200px;">
-                        <h3>기소 일자</h3>
-                    </th>
-                    <th style="width:200px;">
-                        <h3>기소자</h3>
-                    </th>
-                    <th style="width:400px;">
-                        <h3>기소 항목</h3>
-                    </th>
-                </tr>
-                <tr>
-                    <td style="width:190px;">
-                        <input type="text" style="text-align:center" placeholder="이름" id="name_1" class="ui-autocomplete-input auto_name" autocomplete="on" />
-                        <input type="hidden" id="n_student" class="ui-input" value="0" />
-                    </td>
-                    <td style="width:190px;">
-                        <input type="text" style="text-align:center" placeholder="기소일" id="datepicker_1" class="ui-autocomplete-input date_picker" autocomplete="off" />
-                    <td style="width:190px;">
-                        <input type="text" style="text-align:center" placeholder="기소자" id="accuser_1" class="ui-autocomplete-input" autocomplete="off" />
-                    </td>
-                    </td>
-                    <td style="width:400px;">
-                        <input type="text" style="text-align:center; width:380px" placeholder="항목" id="article_kind_1" class="ui-autocomplete-input auto_article" autocomplete="on" />
-                        <input type="hidden" id="article_id" value="0" />
-                    </td>
-                </tr>
-                <tr>
-                    <td style="width:190px;">
-                        <input type="text" style="text-align:center" placeholder="이름" id="name_1" class="ui-autocomplete-input auto_name" autocomplete="on" />
-                        <input type="hidden" id="n_student" class="ui-input" value="0" />
-                    </td>
-                    <td style="width:190px;">
-                        <input type="text" style="text-align:center" placeholder="기소일" id="datepicker_2" class="ui-autocomplete-input date_picker" autocomplete="off" />
-                    </td>
-                    <td style="width:190px;">
-                        <input type="text" style="text-align:center" placeholder="기소자" id="accuser_2" class="ui-autocomplete-input" autocomplete="off" />
-                    </td>
+                <tbody id="article_table_body">
+                    <tr>
+                        <th style="width:200px;">
+                            <h3>학생 이름</h3>
+                        </th>
+                        <th style="width:200px;">
+                            <h3>기소 일자</h3>
+                        </th>
+                        <th style="width:200px;">
+                            <h3>기소자</h3>
+                        </th>
+                        <th style="width:400px;">
+                            <h3>기소 항목</h3>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td style="width:190px;">
+                            <input type="text" style="text-align:center" placeholder="이름" id="name_1" class="ui-autocomplete-input auto_name" autocomplete="on" />
+                            <input type="hidden" id="n_student" class="ui-input" value="0" />
+                        </td>
+                        <td style="width:190px;">
+                            <input type="text" style="text-align:center" placeholder="기소일" id="datepicker_1" class="ui-autocomplete-input date_picker" autocomplete="off" />
+                        <td style="width:190px;">
+                            <input type="text" style="text-align:center" placeholder="기소자" id="accuser_1" class="ui-autocomplete-input" autocomplete="off" />
+                        </td>
+                        </td>
+                        <td style="width:400px;">
+                            <input type="text" style="text-align:center; width:380px" placeholder="항목" id="article_kind_1" class="ui-autocomplete-input auto_article" autocomplete="on" />
+                            <input type="hidden" id="article_id" value="0" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width:190px;">
+                            <input type="text" style="text-align:center" placeholder="이름" id="name_1" class="ui-autocomplete-input auto_name" autocomplete="on" />
+                            <input type="hidden" id="n_student" class="ui-input" value="0" />
+                        </td>
+                        <td style="width:190px;">
+                            <input type="text" style="text-align:center" placeholder="기소일" id="datepicker_2" class="ui-autocomplete-input date_picker" autocomplete="off" />
+                        </td>
+                        <td style="width:190px;">
+                            <input type="text" style="text-align:center" placeholder="기소자" id="accuser_2" class="ui-autocomplete-input" autocomplete="off" />
+                        </td>
 
-                    <td style="width:400px;">
-                        <input type="text" style="text-align:center; width:380px" placeholder="항목" id="article_kind_2" class="ui-autocomplete-input auto_article" autocomplete="on" />
-                        <input type="hidden" id="article_id" value="0" />
-                    </td>
-                </tr>
-            </table>
+                        <td style="width:400px;">
+                            <input type="text" style="text-align:center; width:380px" placeholder="항목" id="article_kind_2" class="ui-autocomplete-input auto_article" autocomplete="on" />
+                            <input type="hidden" id="article_id" value="0" />
+                        </td>
+                    </tr>
+                </table>
+            </tbody>
             <input type="submit" class="btn btn-primary" value="확인" style="margin:10px" />
         </div>
     </form>
@@ -96,7 +98,7 @@ function printContent(){ ?>
         }
         console.log(event.which);
         console.log($(this).closest("tr"));
-        console.log($("#article_table").children().last());
+        console.log($("#article_table_body").children().last());
     });
     </script>
 <?php
