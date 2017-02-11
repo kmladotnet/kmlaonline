cc6540d96896adda2962833939f673e28bd6a237
Hyeonjae Kim <Hyeonjae Kim>
2017-02-11 15:17:37 +0900

    judicial automation table row auto-increase test

--- intranet/process.php
+++ intranet/process.php
@@ -1,5 +1,5 @@
 <?php
-global $member;
+global $student;
 include "lib_real.php";
 $fn="src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
 if(file_exists($fn)) {
