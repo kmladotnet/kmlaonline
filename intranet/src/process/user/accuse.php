c74a8b6ce99f43ca248edf697548c6afd05d8ebb
Hyeonjae Kim <Hyeonjae Kim>
2017-02-11 14:13:31 +0900

    judicial automation table row auto-increase test

---
+++ intranet/src/process/user/accuse.php
@@ -0,0 +1,25 @@
+<?php
+function redirWithBody($failReason){
+    ?>
+    <form method="post" action="<?php echo htmlspecialchars($_POST['prev_url'])?>" id="poster">
+        <?php
+        foreach($_POST as $key=>$val){
+            $val=htmlspecialchars($val);
+            echo "<input type='hidden' name='$key' valaue='$val' />";
+        }
+        ?>
+        <input type="hidden" name="error_occured" value="<?php echo htmlspecialchars(json_encode($failReason)) ?>" />
+        <input type="submit" id="submitter" value="Click here if the page doesn't continue" />
+    </form>
+    <script type="text/javascript">$('#poster').submit();$('#submitter').css("visibility", "hidden");</script>
+    <?php
+}
+
+ function validName($name){
+    if(strpos($name, '(') === false) return false;
+    if(strpos($name, ')') === false) return false;
+    if($student->isValidStudent($name) === false) return false;
+ }
+
+ $failReason = array();
+ if(!validName($_POST['name_1'])) echo "<p>Your entry is wrong</p>";
