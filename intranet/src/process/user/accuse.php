dafa64fcf1b6c085f8681e113e07f0145090c30d
Hyeonjae Kim <Hyeonjae Kim>
2017-02-22 01:37:31 +0900

    input filter test (I'm dumb..)

--- intranet/src/process/user/accuse.php
+++ intranet/src/process/user/accuse.php
@@ -29,4 +29,4 @@ $failReason = array();
 if(!validName($name)) echo "<p>Your entry is wrong</p>";
 $n_student = $student->getIdByStudentId(substr($name, strpos($name, '(') + 1, 6));
 if($n_student === false) echo "<p> Invalid entry </p>";
-else var_dump($n_nump);
+else var_dump($n_student);
