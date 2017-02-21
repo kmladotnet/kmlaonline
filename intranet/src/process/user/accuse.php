8b851d253ca193451f30e25ab2e5ef5d56b5f447
Hyeonjae Kim <Hyeonjae Kim>
2017-02-22 01:36:55 +0900

    input filter test

--- intranet/src/process/user/accuse.php
+++ intranet/src/process/user/accuse.php
@@ -29,4 +29,4 @@ $failReason = array();
 if(!validName($name)) echo "<p>Your entry is wrong</p>";
 $n_student = $student->getIdByStudentId(substr($name, strpos($name, '(') + 1, 6));
 if($n_student === false) echo "<p> Invalid entry </p>";
-elseif(isset($n_student) === false) echo "<p> the value isn't set yet... </p>";
+else var_dump($n_nump);
