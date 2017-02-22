d5639225397b3926821d324b4f82db5c679502f3
Hyeonjae Kim <Hyeonjae Kim>
2017-02-22 11:01:24 +0900

    input form filter test

--- intranet/lib_real.php
+++ intranet/lib_real.php
@@ -61,3 +61,46 @@ function redirectTo($link){
 function isAjax(){
     return isset($_POST['ajax']) || (isset($_SERVER['HTTP_X_CONTENT_ONLY']) && $_SERVER['HTTP_X_CONTENT_ONLY']);
 }
+
+function ajaxDie($arr=array(), $message=false){
+    global $overriden;
+    if($message!==false)
+        $arr['__other']=$message;
+    $arr['error']=1;
+    if(isset($overriden)) $arr['__overriden']=$overriden;
+    die(json_encode($arr));
+}
+
+function ajaxOk($arr=array(), $redir_to=false, $alert_message=false, $confirm_message=false){
+    global $overriden;
+    if($redir_to!==false) $arr['redirect_to']=$redir_to;
+    if($alert_message!==false) $arr["alert_message"]=$alert_message;
+    if($confirm_message!==false) $arr["confirm_message"]=$confirm_message;
+    $arr['error']=0;
+    if(isset($overriden)) $arr['__overriden']=$overriden;
+    die(json_encode($arr));
+}
+
+function redirectWith($str,$dat=""){
+    //TODO : You must change this part so as to fit "intranet" style!!!!!! (2017. 2. 22)
+    ?>
+            <!doctype html>
+            <html>
+            <head>
+                <meta charset="utf-8" />
+                <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+                <title>Redirecting...</title>
+                <base href="/" />
+                <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+                <script type="text/javascript" src="/js/script.js" charset="utf-8"></script>
+            </head>
+
+            <body>
+                <?php
+                    call_user_func($str,$dat);
+                ?>
+            </body>
+            </html>
+            <?php
+    die();
+}
--- intranet/src/process/user/accuse.php
+++ intranet/src/process/user/accuse.php
@@ -23,13 +23,41 @@ function validName($name){
 
 $name = $_POST['name_1'];
 $article_var = $_POST['article_kind_1'];
-echo $article_var;
+
 $failReason = array();
 
 if(!validName($name)) echo "<p>Your entry is wrong</p>";
 $n_student = $student->getIdByStudentId(substr($name, strpos($name, '(') + 1, 6));
-if($n_student === false) echo "<p> Invalid entry </p>";
+if($n_student === false){
+    echo "<p> Invalid entry </p>";
+    $failReason["name_1"] = "존재하지 않는 학생입니다.";
+}
 else echo $n_student["n_id"];
 $n_article = $article_kind->getArticleIdByExplicitName($article_var);
-if($n_article === false) echo "<p> Invalid article kind </p>";
+if($n_article === false){
+    echo "<p> Invalid article kind </p>";
+    $failReason["article_kind_1"] = "존재하지 않는 항목입니다.";
+}
 else echo "<p>" . $n_article["ak_eng"] . " " . $n_article["ak_id"] . "</p>";
+if(count($failReason) > 0){
+    if(isAjax()){
+        ajaxDie($failReason);
+    }else{
+        redirectWith("redirectWithBody", $failReason);
+    }
+} else{
+    if(false !== $mid = $article->addArticle($n_student["n_id"], $n_article["ak_id"], $_POST["accuse_date"], $_POST["accuser"])){
+            if(isAjax()){
+                ajaxOK(array(), "/user/success");
+            }else {
+                redirectTo("/user/success");
+            }
+    }else{
+        if(isAjax()){
+            ajaxDie(array(), "알 수 없는 오류가 발생하였습니다.");
+        }else{
+            $failReason['__other']="알 수 없는 오류가 발생하였습니다.";
+            redirectWith("redirWithBody",$failReason);
+        }
+    }
+}
