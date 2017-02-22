1ce9fbd7f7b36f1cd77653871ca81b7a78ad200b
Hyeonjae Kim <Hyeonjae Kim>
2017-02-22 15:10:49 +0900

    add user.php

--- intranet/src/content/user.php
+++ intranet/src/content/user.php
@@ -2,6 +2,6 @@
 $sub=basename($_GET['sub']);
 if(!file_exists("src/content/user/$sub.php")) return die404();
 if(file_exists("js/content/user/$sub.js")) $includes[]="/js/content/user/$sub.js";
-if($is_mobile && file_exists("css/content/user/$sub.mobile.css")) $includes[]="/css/content/user/$sub.mobile.css";
+//if($is_mobile && file_exists("css/content/user/$sub.mobile.css")) $includes[]="/css/content/user/$sub.mobile.css";
 else if(file_exists("css/content/user/$sub.css")) $includes[]="/css/content/user/$sub.css";
 include "src/content/user/$sub.php";
