fac2b50dc6a6fca384d6d2b99df1ffedff0481a4
Hyeonjae Kim <Hyeonjae Kim>
2017-02-22 15:14:02 +0900

    add user.php

--- intranet/lib_real.php
+++ intranet/lib_real.php
@@ -104,3 +104,10 @@ function redirectWith($str,$dat=""){
             <?php
     die();
 }
+
+function die404(){
+    global $fn; $fn="404"; return;
+    header("HTTP/1.1 404 Not Found");
+    header("Status: 404 Not Found");
+    die("<h1>404 Not Found</h1>");
+}
