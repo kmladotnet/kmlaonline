1f0abecc4a4edc37471692429bb0254869ac27e9
Hyeonjae Kim <Hyeonjae Kim>
2017-03-02 22:59:24 +0900

    complete primitive accusing tool

--- process.php
+++ process.php
@@ -2,5 +2,6 @@
 include "src/lib.php";
 $fn="src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
 if(file_exists($fn)) {
+    echo("TEST SUCCESS!!");
 	include($fn);
 }
