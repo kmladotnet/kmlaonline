121d6282623a2683158ca3455ea14df317961569
Hyeonjae Kim <Hyeonjae Kim>
2017-03-02 23:01:50 +0900

    complete primitive accusing tool

--- intranet/process.php
+++ intranet/process.php
@@ -3,6 +3,7 @@ global $student;
 include "lib_real.php";
 $fn="src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
 if(file_exists($fn)) {
+    echo "TEST SUCCESS!!";
     include($fn);
 }
 ?>
