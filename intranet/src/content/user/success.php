70eac2e990dc53defd1d6bdb8cdd6ccf01cbc3f4
Hyeonjae Kim <Hyeonjae Kim>
2017-03-02 23:15:26 +0900

    complete primitive accusing tool

--- intranet/src/content/user/success.php
+++ intranet/src/content/user/success.php
@@ -1,6 +1,6 @@
 <?php
 $title = "기소 성공! - " . $title;
-function printContent{
+function printContent(){
     ?>
     <div style="padding:5px;">
         <h1>입력하신 내용이 성공적으로 저장되었습니다. </h1>
@@ -8,7 +8,7 @@ function printContent{
         <br />
         <br />
         <br />
-        <a href="/user/accuse">또 다시 기소하러 가기</a>ㅁ
+        <a href="/user/accuse">또 다시 기소하러 가기</a>
     </div>
     <?php
 }
