af9fd9b35ffd96ae3bfe3096cad04109da8503da
Hyeonjae Kim <Hyeonjae Kim>
2017-03-02 23:16:53 +0900

    complete primitive accusing tool

--- intranet/src/content/user/success.php
+++ intranet/src/content/user/success.php
@@ -8,7 +8,13 @@ function printContent(){
         <br />
         <br />
         <br />
-        <a href="/user/accuse">또 다시 기소하러 가기</a>
+        <form method="post" action="./check" id="downform_accuse" onsubmit="return true;">
+            <input type="hidden" id="downform_accuse_action" name="action" value="default" />
+            <div style="float:left">
+                <button class="btn btn-default" style="margin-right:5px;border-radius:5px;" onclick="$('#downform_accuse_action').val('accuse');$('#downform_accuse').submit();"> 또 다시 기소하러 가기
+                </button>
+            </div>
+        </form>
     </div>
     <?php
 }
