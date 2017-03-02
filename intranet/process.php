d34eb12bdf54402760cb7386050d376bee76d11c
Hyeonjae Kim <Hyeonjae Kim>
2017-03-02 23:23:48 +0900

    complete primitive accusing tool

--- intranet/process.php
+++ intranet/process.php
@@ -2,9 +2,7 @@
 global $student;
 include "lib_real.php";
 $fn="src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
-echo "<p>$fn</p>";
 if(file_exists($fn)) {
-    echo "<p>TEST SUCCESS!!</p>";
     include($fn);
 }
 ?>
--- intranet/src/content/user/success.php
+++ intranet/src/content/user/success.php
@@ -8,7 +8,7 @@ function printContent(){
         <br />
         <br />
         <br />
-        <form method="post" action="/intranet/check" id="downform_accuse" onsubmit="return true;">
+        <form method="post" action="../check" id="downform_accuse" onsubmit="return true;">
             <input type="hidden" id="downform_accuse_action" name="action" value="default" />
             <div style="float:left">
                 <button class="btn btn-default" style="margin-right:5px;border-radius:5px;" onclick="$('#downform_accuse_action').val('accuse');$('#downform_accuse').submit();"> 또 다시 기소하러 가기
