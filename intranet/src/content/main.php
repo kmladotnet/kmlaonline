c97b97bc8263cb87ab0404a18b36f88e6dfac920
Hyeonjae Kim <Hyeonjae Kim>
2017-03-06 16:31:35 +0900

    add Select2.js

--- intranet/src/content/main.php
+++ intranet/src/content/main.php
@@ -7,7 +7,7 @@ function printContent(){
         <form method="post" action="./check" id="downform_accuse" onsubmit="return true;">
             <input type="hidden" id="downform_accuse_action" name="action" value="default" />
             <div style="float:left">
-                <button class="btn btn-default" style="margin-right:5px;border-radius:5px;" onclick="$('#downform_accuse_action').val('accuse');$('#downform_accuse').submit();">기소하기
+                <button class="btn btn-default" style="margin-right:5px;border-radius:5px;" onclick="$('#downform_accuse_action').val('accuse_test');$('#downform_accuse').submit();">기소하기
                 </button>
             </div>
         </form>
