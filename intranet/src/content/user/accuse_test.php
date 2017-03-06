0d0b66308cfdaa2f2f7f96f2deadc16d326a4fb4
Hyeonjae Kim <Hyeonjae Kim>
2017-03-06 17:16:51 +0900

    implement Select2.js to accuse_test.php

--- intranet/src/content/user/accuse_test.php
+++ intranet/src/content/user/accuse_test.php
@@ -104,8 +104,7 @@ function printContent(){ ?>
                         </td-->
                         <td style="width:400px;">
                             <select class="js-example-basic-multiple" style="width:400px" multiple="multiple">
-                                <option value="AL">Alabama</option>
-                                <option value="CA">California</option>
+                                <option value="3620194" selected="selected">select2/select2</option>
                             </select>
                         </td>
                     </tr>
