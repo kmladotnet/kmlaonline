4b6dce6e3507c60521790518defdc978ddd56b30
Hyeonjae Kim <Hyeonjae Kim>
2017-03-06 17:19:13 +0900

    implement Select2.js to accuse_test.php

--- intranet/src/content/user/accuse_test.php
+++ intranet/src/content/user/accuse_test.php
@@ -56,8 +56,7 @@ function printContent(){ ?>
                     cache: true
                 },
                 escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-                minimumInputLength: 1,
-                templateResult: formatRepo, // omitted for brevity, see the source of this page
+                minimumInputLength: 1, // omitted for brevity, see the source of this page
                 templateSelection: formatRepoSelection // omitted for brevity, see the source of this page
             });
         });
