f90c865d3d01c74f7f0f728b86726dca5d33690e
Hyeonjae Kim <Hyeonjae Kim>
2017-03-11 21:45:04 +0900

    Select2 Test

--- intranet/src/content/user/accuse_test.php
+++ intranet/src/content/user/accuse_test.php
@@ -4,24 +4,6 @@ $title = "기소 테스팅";
 function printContent(){ ?>
 
     <script type="text/javascript">
-        /*$(document).ready(function(){
-            $(document).on("focus", '#article_table tr:last-child td:last-child', function() {
-                var table = $("#article_table");
-                table.append('<tr><p>WOW</p></tr>');
-            });
-        });*/
-        function addNewRow(){
-            var num = $("#article_table_body").children().length - 1;
-
-            var last_tr = $("#article_table_body").children().last();
-            var newtr = "<td style=\"width:190px;\">\r\n                        <input type=\"text\" style=\"text-align:center\" placeholder=\"\uC774\uB984\" id=\"name_" + num + "\" class=\"ui-autocomplete-input auto_name\" autocomplete=\"on\" \/>\r\n                        <input type=\"hidden\" id=\"n_student\" class=\"ui-input\" value=\"0\" \/>\r\n                    <\/td>\r\n                    <td style=\"width:190px;\">\r\n                        <input type=\"text\" style=\"text-align:center\" placeholder=\"\uAE30\uC18C\uC77C\" id=\"datepicker_" + num + "\" class=\"ui-autocomplete-input date_picker\" autocomplete=\"off\" \/>\r\n                    <\/td>\r\n                    <td style=\"width:190px;\">\r\n                        <input type=\"text\" style=\"text-align:center\" placeholder=\"\uAE30\uC18C\uC790\" id=\"accuser_" + num + "\" class=\"ui-autocomplete-input\" autocomplete=\"off\" \/>\r\n                    <\/td>\r\n\r\n                    <td style=\"width:400px;\">\r\n                        <input type=\"text\" style=\"text-align:center; width:380px\" placeholder=\"\uD56D\uBAA9\" id=\"article_kind_" + num + "\" class=\"ui-autocomplete-input auto_article\" autocomplete=\"on\" \/>\r\n                        <input type=\"hidden\" id=\"article_id\" value=\"0\" \/>\r\n                    <\/td>";
-            $('.')
-            last_tr.append(newtr);
-
-        }
-    </script>
-
-    <script type="text/javascript">
         $(function() {
             $( ".date_picker" ).each(function(){
                 $(this).datepicker({
@@ -73,9 +55,7 @@ function printContent(){ ?>
                             <input type="hidden" id="article_id" value="0" />
                         </td-->
                         <td style="width:400px;">
-                            <select class="js-example-basic-multiple" style="width:400px" multiple="multiple">
-                                <option value="AL">Alabama</option>
-                                <option value="WY">Wyoming</option>
+                            <select class="js-accuse-multiple-articles" style="width:400px" multiple="multiple">
                             </select>
                         </td>
                     </tr>
@@ -104,36 +84,22 @@ function printContent(){ ?>
 
     <!--script type="text/javascript" src="/"></script-->
     <script type="text/javascript">
-        $('.js-example-basic-multiple').select2({
+        $('.js-accuse-multiple-articles').select2({
             //langauge: "ko",
             placeholder: '기소 항목을 선택하세요.',
             ajax: {
-                url: "suggest_article_kind1",
+                url: "suggest_article_kind",
                 dataType: "json",
                 delay: 250,
-                /*data: function (term, page) {
-                    return {
-                        term: term,
-                        page_limit: 10
-                        //page: params.page
-                    };
-                },
-                results: function (data, page) {
-                    return { results: data.results };
-                },*/
                 processResults: function (data) {
                     //params.value = params.value || 1;
 
                     return {
                         results: data
-                        //pagination: {
-                        //    more: (params.page * 30) < data.total_count
-                        //}
                     };
                 },
                 cache: true
-            }//,
-            //escapeMarkup: function (markup) { return markup; } // let our custom formatter work
+            }
         });
     </script>
 
@@ -147,8 +113,6 @@ function printContent(){ ?>
         console.log($(this).closest("tr").index() != 1);
         console.log($(this).closest("tr").index() - num);
         console.log($(this).closest("tr").index() == (num - 1));
-        //console.log($(this).closest("tr").is(":last-child"));
-        //console.log($("#article_table_body").children().last());
     });
     </script>
 
