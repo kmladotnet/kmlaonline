c529c2307ca627e4c26630ed3b4a1909a9863ff4
Hyeonjae Kim <Hyeonjae Kim>
2017-03-11 20:45:56 +0900

    Select2 Test

--- intranet/src/content/user/accuse_test.php
+++ intranet/src/content/user/accuse_test.php
@@ -108,7 +108,7 @@ function printContent(){ ?>
             //langauge: "ko",
             placeholder: '기소 항목을 선택하세요.',
             ajax: {
-                url: "suggest_article_kind",
+                url: "suggest_article_kind1",
                 dataType: "json",
                 delay: 250,
                 /*data: function (term, page) {
