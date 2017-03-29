931393a43489d8db506b64068fc1ac83c470948a
Hyeonjae Kim <Hyeonjae Kim>
2017-03-29 23:43:38 +0900

    Change User View(original version)

--- src/header/menubar.php
+++ src/header/menubar.php
@@ -7,12 +7,15 @@
         <div class="widthholder">
             <div class="menu1_text">교내</div>
             <div class="menu1_sub">
-        <a href="board/student_council" class="menu2">학생회</a>
+                <a href="board/student_council" class="menu2">학생회</a>
                 <a href="board/student_legislative" class="menu2">입법</a>
                 <a href="board/student_judicial" class="menu2">사법</a>
                 <a href="board/student_executive" class="menu2">행정</a>
                 <a href="board/student_discuss" class="menu2">학급회의</a>
-                <a href="board/student_clubs" class="menu2">동아리</a>
+                <?php
+                    if($me[n_level] > 19)
+                        echo '<a href="board/student_clubs" class="menu2">동아리</a>';
+                ?>
             </div>
         </div>
     </div>
