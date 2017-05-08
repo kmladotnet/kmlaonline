f48d7ba8e1890e6907e178a29e66878b99e9b9db
Hyeonjae Kim <Hyeonjae Kim>
2017-05-09 01:09:36 +0900

    Test Message

--- intranet/process.php
+++ intranet/process.php
@@ -1,7 +1,9 @@
 <?php
 global $student;
 include "lib_real.php";
-$fn="intranet/src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
+$fn="intranet/src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php"; ?>
+<script type="text/javascript">alert("기소 기간이 아닙니다.");location.href="/intranet";</script>
+<?php
 if(file_exists($fn)) {
     include($fn);
 }
