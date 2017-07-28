e92b71d87adf4d68d845edf93ef68d1e8067fc6c
HJ Kim <hyeonjae42@gmail.com>
2017-07-28 23:35:49 +0900

    FPDF Test - test.php

--- test/fpdf/ex.php
+++ test/fpdf/ex.php
@@ -2,9 +2,9 @@
 require('korean.php');
 
 $pdf = new PDF_Korean();
-$pdf->AddUHCFont();
+$pdf->AddUHChwFont();
 $pdf->AddPage();
-$pdf->SetFont('UHC','',18);
+$pdf->SetFont('UHC-hw','',18);
 $pdf->Write(8,'PHP 3.0은 1998년 6월에 공식적으로 릴리즈되었다. 공개적인 테스트 이후약 9개월만이었다.');
 $pdf->Output();
 ?>
