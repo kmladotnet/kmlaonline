ba10d8be976e7dc4e406399c562c08573a19bd05
HJ Kim <hyeonjae42@gmail.com>
2017-07-28 23:46:23 +0900

    FPDF Test - test.php

--- test/fpdf/ex.php
+++ test/fpdf/ex.php
@@ -1,14 +1,14 @@
 <?php
-require('korean.php');
+    require('korean.php');
 
-$pdf = new PDF_Korean();
+    $pdf = new PDF_Korean();
 
-$pdf->AddUHCFont('명조');
-//$pdf->Open();
-$pdf->AddPage();
+    $pdf->AddUHCFont('명조');
+    //$pdf->Open();
+    $pdf->AddPage();
 
-$pdf->SetFont('명조','',18);
-$pdf->Write(8,'PHP 3.0은 1998년 6월에 공식적으로 릴리즈되었다. 공개적인 테스트 이후약 9개월만이었다.');
+    $pdf->SetFont('명조','',18);
+    $pdf->Write(8,'PHP 3.0은 1998년 6월에 공식적으로 릴리즈되었다. 공개적인 테스트 이후약 9개월만이었다.');
 
-$pdf->Output();
+    $pdf->Output();
 ?>
