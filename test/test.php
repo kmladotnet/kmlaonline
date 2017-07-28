932466bccc786284ae8cc2844809608af2871272
HJ Kim <hyeonjae42@gmail.com>
2017-07-28 13:37:49 +0900

    FPDF Test - test.php

--- test/test.php
+++ test/test.php
@@ -4,7 +4,7 @@ require('fpdf/fpdf.php');
 $pdf = new FPDF();
 $pdf -> AddPage();
 $pdf -> SetFont("Arial", "B", 16);
-$pdf -> Cell(40, 10, "Hello World!");
+$pdf -> Cell(40, 10, "외출 • 외박 • 결석");
 $pdf -> Output();
 
 ?>
