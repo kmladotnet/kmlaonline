a3566038f3252790e5c4ec4e6d8cae33e08e5a73
HJ Kim <hyeonjae42@gmail.com>
2017-07-28 23:30:38 +0900

    FPDF Test - test.php

--- test/test.php
+++ test/test.php
@@ -2,8 +2,9 @@
 require('fpdf/korean.php');
 
 $pdf = new PDF_Korean();
+$pdf->AddUHCFont();
 $pdf -> AddPage();
-$pdf -> SetFont("Arial", "B", 16);
+$pdf -> SetFont("UHC", "B", 16);
 $pdf -> Cell(40, 10, "외출 • 외박 • 결석");
 $pdf -> Output();
 
