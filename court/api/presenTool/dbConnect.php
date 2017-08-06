cc95148497161a36b6acaa74d2c57bb65e14b9a9
HJ Kim <hyeonjae42@gmail.com>
2017-08-07 03:16:15 +0900

    Court Test - Building Login Page(8/7 =8=)

---
+++ court/api/presenTool/dbConnect.php
@@ -0,0 +1,23 @@
+<?php
+
+class dbConnect {
+    private $conn;
+
+    function __construct(){
+
+    }
+
+    function connect(){
+        include_once '../config.php';
+
+        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to Database: ". mysqli_connect_error();
+        }
+
+        return $this->conn;
+    }
+}
+
+?>
