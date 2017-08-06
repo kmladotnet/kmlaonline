a5c1d5a78e51c4c65b6afe60cb3f51ff7b47d67f
HJ Kim <hyeonjae42@gmail.com>
2017-08-07 03:08:09 +0900

    Court Test - Building Login Page(8/7 =7=)

---
+++ court/api/presenTool/index.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'dbHandler.php';
+require_once 'passwordHash.php';
+require '.././libs/Slim/Slim.php';
+
+\Slim\Slim::registerAutoLoader();
+
+$app = new \Slim\Slim();
+
+$user_id = NULL;
+
+require_once 'authentication.php';
+
+/**
+*
+*/
+function verifyRequiredParams($required_fields, $request_params) {
+    $error = false;
+    $error_fields = "";
+    foreach($required_fields as $field){
+        if(!isset($request_params->$field) || strlen(trim($request_params->$field)) <=0) {
+            $error = true;
+            $error_field .= $field . ', ';
+        }
+    }
+
+    if($error) {
+        $response = array();
+        $app = \Slim\Slim::getInstance();
+        $response["status"] = "error";
+        $response["message"] = 'Required fields' . substr($error_fields, 0, -2) . ' is missing or empty';
+        echoResponse(200, $response);
+        $app->stop();
+    }
+}
+
+function echoResponse($status_code, $response){
+    $app = \Slim\Slim::getInstance();
+    $app->status($status_code);
+    $app->contentType('application/json');
+    echo json_encode($response);
+}
+
+$app->run();
+?>
