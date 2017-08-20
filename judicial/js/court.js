1496d285ab1a1d37994bc8025f40a8e8549fa253
HJ Kim <hyeonjae42@gmail.com>
2017-08-20 21:49:52 +0900

    court javascript test

--- judicial/js/court.js
+++ judicial/js/court.js
@@ -4,6 +4,8 @@ app.controller("courtCtrl", ['$scope', '$http', '$templateCache', function($scop
     var articleList = [];
 
     $scope.articleList = articleList;
+    $scope.method = 'GET';
+    $scope.url = '/test/users.json';
 
     $scope.addNewArticle = function(_grade, _name, _accuse_date, _accuser, _article, _point){
         $scope.articleList.push({grade: _grade, name: _name, accuse_date: _accuse_date, accuser: _accuser, article: _article, point: _point});
@@ -22,8 +24,9 @@ app.controller("courtCtrl", ['$scope', '$http', '$templateCache', function($scop
 
 
         $http({
-            method: 'GET',
-            url: '/test/users.json'
+            method: $scope.method,
+            url: $scope.url,
+            cache: $templateCache
         }).then(function(response){
             $scope.status = response.status;
             $scope.data = response.data;
@@ -31,5 +34,10 @@ app.controller("courtCtrl", ['$scope', '$http', '$templateCache', function($scop
             $scope.data = response.data || 'Request failed';
             $scope.status = response.status;
         });
+    };
+
+    $scope.updateModel = function(method, url) {
+        $scope.method = method;
+        $scope.url = url;
     };
 }]);
