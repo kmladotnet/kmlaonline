fcd6d357281bc29ea0a22b20fd5ae882118e84e4
HJ Kim <hyeonjae42@gmail.com>
2017-08-20 21:59:39 +0900

    court javascript test

--- judicial/js/court.js
+++ judicial/js/court.js
@@ -1,43 +1,43 @@
 var app = angular.module("kmla_court", []);
 
-app.controller("courtCtrl", ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache){
-    var articleList = [];
+app.controller("courtCtrl", function($scope, $http){
+        var articleList = [];
 
-    $scope.articleList = articleList;
-    $scope.method = 'GET';
-    $scope.url = '/test/users.json';
+        $scope.articleList = articleList;
 
-    $scope.addNewArticle = function(_grade, _name, _accuse_date, _accuser, _article, _point){
-        $scope.articleList.push({grade: _grade, name: _name, accuse_date: _accuse_date, accuser: _accuser, article: _article, point: _point});
-        $scope.accused_grade = "";
-        $scope.accused_name = "";
-        $scope.accuser = "";
-        $scope.accused_article = "";
-        $scope.accused_point = "";
+        $scope.method = 'GET';
+        $scope.url = '/test/users.json';
 
-        console.log($scope.accused_date);
-    };
+        $scope.addNewArticle = function(_grade, _name, _accuse_date, _accuser, _article, _point){
+            $scope.articleList.push({grade: _grade, name: _name, accuse_date: _accuse_date, accuser: _accuser, article: _article, point: _point});
+            $scope.accused_grade = "";
+            $scope.accused_name = "";
+            $scope.accuser = "";
+            $scope.accused_article = "";
+            $scope.accused_point = "";
 
-    $scope.fetch = function() {
-        $scope.code = null;
-        $scope.response = null;
+            console.log($scope.accused_date);
+        };
+
+        $scope.fetch = function() {
+            $scope.code = null;
+            $scope.response = null;
 
 
-        $http({
-            method: $scope.method,
-            url: $scope.url,
-            cache: $templateCache
-        }).then(function(response){
-            $scope.status = response.status;
-            $scope.data = response.data;
-        }, function(response){
-            $scope.data = response.data || 'Request failed';
-            $scope.status = response.status;
-        });
-    };
+            $http({
+                method: "GET",
+                url: "test/user.json"
+            }).then(function mySuccess(response){
+                $scope.status = response.statusText;
+                $scope.data = response.data;
+            }, function myError(response){
+                $scope.data = response.data || 'Request failed';
+                $scope.status = response.statusText;
+            });
+        };
 
-    $scope.updateModel = function(method, url) {
-        $scope.method = method;
-        $scope.url = url;
-    };
-}]);
+        $scope.updateModel = function(method, url) {
+            $scope.method = method;
+            $scope.url = url;
+        };
+});
