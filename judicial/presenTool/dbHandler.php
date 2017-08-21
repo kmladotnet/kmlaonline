1f6077489155a1f272208dc1a7a3fa85615cc9ca
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 18:59:28 +0900

    Database test for court (typo revision)

--- judicial/presenTool/dbHandler.php
+++ judicial/presenTool/dbHandler.php
@@ -1,5 +1,4 @@
 <?php
     require_once('config.php');
-    echo "<b> " . DB_USERNAME . " </b>";
-    $db = initializePresenTools('DB_HOST', 'DB_USERNAME', 'DB_PASSWORD', 'DB_NAME');
+    $db = initializePresenTools(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 ?>
