92bb8225c10fc04d71439d888b6c2d87014684dc
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 18:37:55 +0900

    Database test for court

--- judicial/process/accuse.php
+++ judicial/process/accuse.php
@@ -1,6 +1,6 @@
 <?php
     include("../lib.php");
     $data = json_decode(file_get_contents("php://input"));
-    echo $data->grade;
-    echo $data->name;
+
+    addCourtArticle(317, "2017-08-21", 75, 3);
 ?>
