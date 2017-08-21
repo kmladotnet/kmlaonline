eddfe473936bcb0c2ccd39fab3febc3b450e4f8b
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 21:25:34 +0900

    Database test for court (accuse test from website)

--- judicial/lib.php
+++ judicial/lib.php
@@ -5,6 +5,7 @@ include "presenTool/dbHandler.php";
 
 function createNewArticle($grade, $accused_name, $accuser_name, $article_kind_name, $accuse_date="2017-08-21"){
     global $member, $accuser, $article_kind, $article;
+
     if(($accused_id = $member->gradeName2CourtId($grade, $accused_name))
         && ($accuser_id = $accuser->accuserName2Id($accuser_name)) && ($article_kind_id = $article_kind->articleDesc2Id($article_kind_name))){
         echo $member->gradeName2CourtId($grade, $accused_name) . "<br />";
@@ -12,6 +13,9 @@ function createNewArticle($grade, $accused_name, $accuser_name, $article_kind_na
         if($article->addCourtArticle((int) $accused_id, $accuse_date, (int) $accuser_id, (int) $article_kind_id)) echo "OKAY";
         else echo "NOT OKAY";
     }
-    else echo "ERROR OCCURED";
+    else{
+        echo " $accused_id $accuser_id $article_kind_id $accuse_date";
+        echo "ERROR OCCURED";
+    }
 }
 ?>
