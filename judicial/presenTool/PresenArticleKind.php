535adf1440abbc6c134f0c62d543d1d827a9d3be
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 19:55:33 +0900

    Database test for court (typo revision - PresenArticleKind.php)

--- judicial/presenTool/PresenArticleKind.php
+++ judicial/presenTool/PresenArticleKind.php
@@ -3,6 +3,10 @@ class PresenArticleKind {
     private $db;
     private $table_data;
 
+    private function escape($str) {
+        return $this->db->real_escape_string($str);
+    }
+
     function __construct($db){
         $this->db = $db;
         $this->table_data = 'article_list';
