0fa036531cc505ceb4f2b427c465ce007a240871
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 21:22:59 +0900

    Database test for court (accuse test from website)

--- judicial/process/accuse.php
+++ judicial/process/accuse.php
@@ -1,12 +1,12 @@
 <?php
     include("../lib.php");
     $data = json_decode(file_get_contents("php://input"));
-    /*$data_grade = $data->grade;
+    $data_grade = $data->grade;
     $data_name = $data->name;
     //$data_date = $data->accuse_date;
     $data_accuser = $data->accuser;
     $data_article = $data->article;
-    //$data_point = $data->point; */
+    //$data_point = $data->point;
 
     createNewArticle($data_grade, $data_name, $data_accuser, $data_article);
 
