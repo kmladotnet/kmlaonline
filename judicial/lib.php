d3de5aec2138289b8406b508a44918dec8e77563
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 20:08:22 +0900

    Database test for court (typo - PresenArticleKind.php)

--- judicial/lib.php
+++ judicial/lib.php
@@ -4,7 +4,7 @@ include "presenTool/PresenTools.php";
 include "presenTool/dbHandler.php";
 
 function createNewArticle($grade, $student_name, $accuse_date="2017-08-21", $accuser=2, $article_kind=2){
-    if($accused_id = gradeName2CourtId($grade, $student_name)) echo $accused_id;
+    if($accused_id = $member->gradeName2CourtId($grade, $student_name)) echo $accused_id;
     //$article->addCourtArticle(317, "2017-08-21", 75, 3);
 }
 ?>
