083e0d7103485a95338e510baf4577c87034508b
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 00:48:26 +0900

    Article List Page (almost done)

--- judicial/content/main.php
+++ judicial/content/main.php
@@ -139,7 +139,9 @@
                                 <tbody>
                                     <tr>
                                         <td>
-                                            <tags-input type="text" class="form-control" ng-model="accused_name2" name="name2"></tags-input>
+                                            <div class="form-group">
+                                                <tags-input type="text" class="form-control" ng-model="accused_name2" name="name2"></tags-input>
+                                            </div>
                                         </td>
                                         <td><input type="date" class="form-control" ng-model="accused_date2"></td>
                                         <td><input type="text" class="form-control" ng-model="accuser2"></td>
