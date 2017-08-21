ef8a635c8a2ea164ad18d0a853deb1d529b0cde5
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 20:22:03 +0900

    Database test for court (typo - PresenArticleKind.php)

--- judicial/lib.php
+++ judicial/lib.php
@@ -3,10 +3,10 @@ date_default_timezone_set("Asia/Seoul");
 include "presenTool/PresenTools.php";
 include "presenTool/dbHandler.php";
 
-function createNewArticle($grade, $student_name, $accuser, $article_kind, $accuse_date="2017-08-21"){
+function createNewArticle($grade, $accused_name, $accuser_name, $article_kind_name, $accuse_date="2017-08-21"){
     global $member, $accuser, $article_kind, $article;
-    if($accused_id = $member->gradeName2CourtId($grade, $student_name)
-        && $accuser_id = $accuser->accuserName2Id($accuser) && $article_kind_id = $article_kind->articleDesc2Id($article_kind))
+    if($accused_id = $member->gradeName2CourtId($grade, $accused_name)
+        && $accuser_id = $accuser->accuserName2Id($accuser_name) && $article_kind_id = $article_kind->articleDesc2Id($article_kind_name))
         $article->addCourtArticle($accused_id, $accuse_date, $accuser_id, $article_kind_id);
 }
 ?>
