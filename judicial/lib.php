d0b3bbed840b0b36de615c34937c0f978446e596
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 20:20:53 +0900

    Database test for court (typo - PresenArticleKind.php)

--- judicial/lib.php
+++ judicial/lib.php
@@ -7,6 +7,6 @@ function createNewArticle($grade, $student_name, $accuser, $article_kind, $accus
     global $member, $accuser, $article_kind, $article;
     if($accused_id = $member->gradeName2CourtId($grade, $student_name)
         && $accuser_id = $accuser->accuserName2Id($accuser) && $article_kind_id = $article_kind->articleDesc2Id($article_kind))
-        $article->addCourtArticle($accused_id, $accuse_date, $accuser, $article_kind);
+        $article->addCourtArticle($accused_id, $accuse_date, $accuser_id, $article_kind_id);
 }
 ?>
