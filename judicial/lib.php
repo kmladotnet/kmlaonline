2a5c416451fc4b0170768a05c9060d6e85e96ebb
HJ Kim <hyeonjae42@gmail.com>
2017-08-21 20:59:08 +0900

    Database test for court (accuse test from website)

--- judicial/lib.php
+++ judicial/lib.php
@@ -7,7 +7,7 @@ function createNewArticle($grade, $accused_name, $accuser_name, $article_kind_na
     global $member, $accuser, $article_kind, $article;
     if($accused_id = $member->gradeName2CourtId($grade, $accused_name)
         && $accuser_id = $accuser->accuserName2Id($accuser_name) && $article_kind_id = $article_kind->articleDesc2Id($article_kind_name)){
-        echo $accused_id . " " . "$accuser_id" . " $article_kind_id $accuse_date";
+        echo " $accused_id $accuser_id $article_kind_id $accuse_date";
         if($article->addCourtArticle($accused_id, $accuse_date, $accuser_id, $article_kind_id)) echo "OKAY";
         else echo "NOT OKAY";
     }
