1d7aaba5c0d45b77178d4a399fd3f7212b182719
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 23:58:30 +0900

    submitListofArticle

--- judicial/content/main.php
+++ judicial/content/main.php
@@ -172,7 +172,7 @@
                                             <ui-select multiple ng-model="accusedSelect.selectedPeople" theme="bootstrap" ng-disabled="disabled" sortable="true" close-on-select="false">
                                                 <ui-select-match placeholder="기소할 학생을 선택해주세요...">{{$item.grade}}-{{$item.name}}</ui-select-match>
                                                 <ui-select-choices repeat="accused in (accusedArray | filter: $select.search) track by accused.student_id">
-                                                    <div ng-bind-html="accused.name | highlight: $select.search"></div>
+                                                    <div ng-bind-html="accused.name"></div>
                                                     <small>
                                                         {{accused.grade}}학년 {{accused.name}}
                                                     </small>
