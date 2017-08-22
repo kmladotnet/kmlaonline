6a02536e531b8cb9d7c7062cd22d056d1fd340d3
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 23:31:28 +0900

    submitListofArticle

--- judicial/content/main.php
+++ judicial/content/main.php
@@ -103,7 +103,7 @@
                 </ul>
             </div>
             <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                <div>
+                <!--div>
                     <h1 class="page-header">새로운 항목 기소</h1>
                     <div class="table-responsive">
                         <form name="newArticle">
@@ -151,7 +151,7 @@
                     <button type="button" class="btn btn-info pull-right" ng-click="addNewArticle(accused_grade, accused_name, accused_date, accuser, accused_article, accused_point)">기소</button>
                     <br/>
 
-                </div>
+                </div-->
                 <div>
                     <h1 class="page-header">새로운 항목 기소(2)</h1>
                     <div class="table">
