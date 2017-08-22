05bdbd8194ece9cee3cc895f26605d8e7a39155b
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 16:55:11 +0900

    TEST(Select.js)

--- judicial/js/court.js
+++ judicial/js/court.js
@@ -1,36 +1,5 @@
 var app = angular.module("kmla_court", ['ngTagsInput', 'ngSanitize', 'ui.select']);
 
-app.filter('propsFilter', function() {
-  return function(items, props) {
-    var out = [];
-
-    if (angular.isArray(items)) {
-      var keys = Object.keys(props);
-
-      items.forEach(function(item) {
-        var itemMatches = false;
-
-        for (var i = 0; i < keys.length; i++) {
-          var prop = keys[i];
-          var text = props[prop].toLowerCase();
-          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-            itemMatches = true;
-            break;
-          }
-        }
-
-        if (itemMatches) {
-          out.push(item);
-        }
-      });
-    } else {
-      // Let the output be the input untouched
-      out = items;
-    }
-
-    return out;
-  };
-});
 
 app.controller("courtCtrl", function($scope, $http, $timeout, $interval){
         var articleList = [];
