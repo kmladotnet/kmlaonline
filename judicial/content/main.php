69a6e675ef027e22e45c6b3a1621bf97f2abba6a
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 19:25:21 +0900

    getAccuserList

--- judicial/content/main.php
+++ judicial/content/main.php
@@ -185,8 +185,8 @@
                                                 <ui-select-match>
                                                     <span ng-bind="$select.selected.name"></span>
                                                 </ui-select-match>
-                                                <ui-select-choices repeat="item in (itemArray | filter: $select.search) track by item.id">
-                                                    <span ng-bind="item.name"></span>
+                                                <ui-select-choices repeat="accuser in (accuserArray | filter: $select.search) track by accuser.a_id">
+                                                    <span ng-bind="accuser.name"></span>
                                                 </ui-select-choices>
                                             </ui-select>
                                         </td>
