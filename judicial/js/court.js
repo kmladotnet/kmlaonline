c99e755c590aef907d6f69984514a80602329c4e
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 09:35:50 +0900

    TEST

--- judicial/js/court.js
+++ judicial/js/court.js
@@ -11,8 +11,7 @@ app.controller("courtCtrl", function($scope, $http){
         $scope.accused_name2 = [];
 
         $scope.loadTags = function(query){
-            console.log(query);
-            return $http.get('process/suggest.php');
+            return $http.get('process/suggest.php?q=' + query);
         };
 
         $scope.submitNewArticle = function(_grade, _name, _accuse_date, _accuser, _article, _point){
