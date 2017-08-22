c10cb5b210bfa21d701ff13870d124ed80684c3a
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 09:11:17 +0900

    TEST

--- judicial/process/suggest.php
+++ judicial/process/suggest.php
@@ -1,21 +1,4 @@
 <?php
-    /*$final = array();
-    $test = array("text" => "김현재");
-    array_push($final, $test);
-    $test = array("text" => "조성민");
-    array_push($final, $test);
-    $test = array("text" => "심무경");
-    array_push($final, $test);
-    $test = array("text" => "김명순");
-    array_push($final, $test);
-    $test = array("text" => "조성현");
-    array_push($final, $test);
-    $test = array("text" => "강건");
-    array_push($final, $test);
-    $test = array("text" => "김ㅅ김");
-    array_push($final, $test);
-    echo json_encode($final);*/
-
     include("../lib.php");
     $q = $_GET['q'];
     suggestMemberByQuery($q);
