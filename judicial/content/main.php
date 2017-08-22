b615b868df640212d63771b96dba1e98adc5167c
HJ Kim <hyeonjae42@gmail.com>
2017-08-22 20:55:47 +0900

    main.php student name tag

--- judicial/content/main.php
+++ judicial/content/main.php
@@ -210,7 +210,7 @@
                                                 </ui-select-choices>
                                             </ui-select>
                                         </td>
-                                        <td><input type="text" class="form-control" ng-model="$select.selected.ak_eng" ng-disabled="point_disabled"></td>
+                                        <td><input type="text" class="form-control" ng-model="3" ng-disabled="point_disabled"></td>
                                     </tr>
                                 </tbody>
                             </table>
