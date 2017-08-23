02144cb30d2241430dda8c6d041682291f4b0de3
HJ Kim <hyeonjae42@gmail.com>
2017-08-23 11:12:59 +0900

    Court Test - articleSort

--- judicial/process/articleSort.php
+++ judicial/process/articleSort.php
@@ -2,14 +2,29 @@
     include('../lib.php');
 
     $result = getAllProcessingArticles();
-    uasort($result, 'article_cmp');
+    /*uasort($result, 'article_cmp');
     print_r($result);
 
     function article_cmp($ar1, $ar2){
 
         if($ar1['status'] === $ar2['status']){
             if(count($ar1['article_array']) === count($ar2['article_array'])) {
-                return 0;
+                $sum1 = 0;
+                $sum2 = 0;
+                $points_1 = array();
+                $points_2 = array();
+                for($i = 0; $i < count($ar1['article_array']); $i++){
+                    $sum1 += $ar1['article_array'][$i]['point'];
+                    $points_1.push($ar1['article_array'][$i]['article_kind']);
+                    $sum2 += $ar2['article_array'][$i]['point'];
+                    $points_2.push($ar1['article_array'][$i]['article_kind']);
+                }
+                if($sum1 === $sum2){
+                    $points_1
+                } else {
+                    if($sum1 < $sum2) return -1;
+                    else return 1;
+                }
             } else {
                 if(count($ar1['article_array']) < count($ar2['article_array'])) return 1;
                 else return -1;
@@ -18,6 +33,9 @@
             if($ar1['status'] < $ar2['status']) return -1;
             else return 1;
         }
-    }
-
+    }*/
+    $test1 = [30, 12, 13];
+    $test2 = [30, 13, 12];
+    echo $test1 == $test2;
+    echo $test1 === $test2;
 ?>
