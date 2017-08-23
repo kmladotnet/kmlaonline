bf2d2b081d712be858bd118afc4f476d17885642
HJ Kim <hyeonjae42@gmail.com>
2017-08-24 01:42:48 +0900

    final update for sorted list

--- judicial/content/list.php
+++ judicial/content/list.php
@@ -57,23 +57,11 @@
         <div class="row">
             <div class="col-sm-3 col-md-2 sidebar">
                 <ul class="nav nav-sidebar">
-                    <li><a href="main">기소하기</a></li>
+                    <li><a href="#">기소하기</a></li>
                     <li class="active"><a href="">이번 주 법정 <span class="sr-only">(current)</span></a></li>
                     <li><a href="#">통계</a></li>
-                    <li><a href="#">Export</a></li>
                 </ul>
-                <ul class="nav nav-sidebar">
-                    <li><a href="">Nav item</a></li>
-                    <li><a href="">Nav item again</a></li>
-                    <li><a href="">One more nav</a></li>
-                    <li><a href="">Another nav item</a></li>
-                    <li><a href="">More navigation</a></li>
-                </ul>
-                <ul class="nav nav-sidebar">
-                    <li><a href="">Nav item again</a></li>
-                    <li><a href="">One more nav</a></li>
-                    <li><a href="">Another nav item</a></li>
-                </ul>
+
             </div>
             <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                 <div>
