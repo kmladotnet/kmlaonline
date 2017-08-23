79402aee1752bfb0f74a256faf0f010dc3bfd815
HJ Kim <hyeonjae42@gmail.com>
2017-08-23 11:35:19 +0900

    Court Test - articleSort

--- judicial/process/articleSort.php
+++ judicial/process/articleSort.php
@@ -15,9 +15,9 @@
                 $points_2 = array();
                 for($i = 0; $i < count($ar1['article_array']); $i++){
                     $sum1 += $ar1['article_array'][$i]['point'];
-                    $points_1.push($ar1['article_array'][$i]['article_kind']);
+                    array_push($points_1, $ar1['article_array'][$i]['article_kind']);
                     $sum2 += $ar2['article_array'][$i]['point'];
-                    $points_2.push($ar1['article_array'][$i]['article_kind']);
+                    array_push($points_2, $ar2['article_array'][$i]['article_kind']);
                 }
                 if($sum1 === $sum2){
                     if(count(array_diff(array_merge($points_1, $points2), array_intersect($points_1, $points_2))) === 0) {
@@ -32,7 +32,7 @@
                             else return 1;
                         }
                     } else {
-                        if(array_sum($points_1) < array_sum($points_1)) return -1;
+                        if(array_sum($points_1) < array_sum($points_2)) return -1;
                         return 1;
                     }
                 } else {
