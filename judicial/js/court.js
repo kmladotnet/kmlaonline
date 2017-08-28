fa9d0a71239d51889270c7e795f6e5b223b006ab
HJ Kim <hyeonjae42@gmail.com>
2017-08-28 15:00:32 +0900

    excel

--- judicial/js/court.js
+++ judicial/js/court.js
@@ -34,21 +34,6 @@ app.filter('propsFilter', function() {
   };
 });
 
-app.factory('Excel', function($window){
-        var uri='data:application/vnd.ms-excel;base64,',
-            template='<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
-            base64=function(s){return $window.btoa(unescape(encodeURIComponent(s)));},
-            format=function(s,c){return s.replace(/{(\w+)}/g,function(m,p){return c[p];})};
-        return {
-            tableToExcel:function(tableId,worksheetName){
-                var table=$(tableId),
-                    ctx={worksheet:worksheetName,table:table.html()},
-                    href=uri+base64(format(template,ctx));
-                return href;
-        }
-    };
-});
-
 app.controller("courtCtrl", function($scope, $http){
         $scope.disabled = undefined;
         $scope.point_disabled = true;
@@ -249,7 +234,20 @@ app.controller("courtCtrl", function($scope, $http){
         };
 });
 
-app.controller("listCtrl", function($scope, $http, Excel, $timeout){
+app.factory('Excel', function($window){
+        var uri='data:application/vnd.ms-excel;base64,',
+            template='<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+            base64=function(s){return $window.btoa(unescape(encodeURIComponent(s)));},
+            format=function(s,c){return s.replace(/{(\w+)}/g,function(m,p){return c[p];})};
+        return {
+            tableToExcel:function(tableId,worksheetName){
+                var table=$(tableId),
+                    ctx={worksheet:worksheetName,table:table.html()},
+                    href=uri+base64(format(template,ctx));
+                return href;
+        }
+    };
+}).controller("listCtrl", function($scope, $http, Excel, $timeout){
     var articleList = [];
     $scope.articleList = articleList;
 
@@ -350,11 +348,11 @@ app.controller("listCtrl", function($scope, $http, Excel, $timeout){
     };
 
     $scope.exportToExcel = function(tableId){
-        console.log("function start!!!");
+        //console.log("function start!!!");
         var exportHref = Excel.tableToExcel(tableId, 'Court List');
-        console.log(exportHref);
+        //console.log(exportHref);
 
-        location.href = exportHref;
+        $timeout(function(){location.href = exportHref;}, 100);
 
-    };
+    }
 });
