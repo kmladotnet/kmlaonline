4b597bb371b65e2e867d8ec94e03670d4f7836a1
HJ Kim <hyeonjae42@gmail.com>
2017-08-29 10:45:22 +0900

    outdoor

--- src/content/util/outdoor.php
+++ src/content/util/outdoor.php
@@ -8,21 +8,21 @@ function printContent(){
     <h1>외출 외박 신청서 작성</h1>
     <div class="table">
         <form name="newOutdoor">
-            <table class="table table-striped">
+            <table class="table table-striped table-bordered">
                 <tr>
                     <div class="btn-group">
-                      <button type="button" class="btn btn-danger">Action</button>
-                      <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="caret"></span>
-                        <span class="sr-only">Toggle Dropdown</span>
-                      </button>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                      </ul>
+                        <button type="button" class="btn">Action</button>
+                        <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="caret"></span>
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
                     </div>
                 </tr>
             </table>
