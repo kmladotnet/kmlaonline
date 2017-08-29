ee5766e5381bc0df66dd45fb3eee5f318aad3157
HJ Kim <hyeonjae42@gmail.com>
2017-08-29 10:57:52 +0900

    outdoor

--- src/content/util/outdoor.php
+++ src/content/util/outdoor.php
@@ -11,18 +11,8 @@ function printContent(){
             <table class="table table-striped table-bordered">
                 <tr>
                     <div class="btn-group">
-                        <button type="button" class="btn">Action</button>
-                        <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="caret"></span>
-                            <span class="sr-only">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                        </ul>
+                        <input type="number" name="grade">
+                        <label>학년</label>
                     </div>
                 </tr>
             </table>
