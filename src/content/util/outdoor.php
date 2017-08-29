1a6da6dfdd9cd272e65f2dea196a70625efff709
HJ Kim <hyeonjae42@gmail.com>
2017-08-29 10:43:07 +0900

    outdoor

--- src/content/util/outdoor.php
+++ src/content/util/outdoor.php
@@ -1,4 +1,32 @@
 <?php
 redirectLoginIfRequired();
 $title = "외출 외박 신청서 작성 - " . $title;
+
+function printContent(){
+    global $member, $me;
+    ?>
+    <h1>외출 외박 신청서 작성</h1>
+    <div class="table">
+        <form name="newOutdoor">
+            <table class="table table-striped">
+                <tr>
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-danger">Action</button>
+                      <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                      </button>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                      </ul>
+                    </div>
+                </tr>
+            </table>
+        </form>
+    </div>
+}
 ?>
