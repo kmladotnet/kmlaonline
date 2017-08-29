4b307b1377ece716cc600149df03d137a8795c64
HJ Kim <hyeonjae42@gmail.com>
2017-08-29 11:02:09 +0900

    outdoor

--- src/content/util/outdoor.php
+++ src/content/util/outdoor.php
@@ -6,14 +6,19 @@ function printContent(){
     global $member, $me;
     ?>
     <h1>외출 외박 신청서 작성</h1>
-    <div class="table">
+    <div class="table" style="margin-top:20px">
         <form name="newOutdoor">
             <table class="table table-striped table-bordered">
                 <tr>
-                    <div class="btn-group">
-                        <input type="number" name="grade">
-                        <label>학년</label>
-                    </div>
+                    <td>
+                        <p>인적 정보</p>
+                    </td>
+                    <td>
+                        <div class="btn-group">
+                            <input type="number" name="grade" class="form-control">
+                            <label>학년</label>
+                        </div>
+                    </td>
                 </tr>
             </table>
         </form>
