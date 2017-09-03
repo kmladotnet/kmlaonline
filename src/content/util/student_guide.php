6af978134884eb06738281d5a9d5a7798fc658d1
HJ Kim <hyeonjae42@gmail.com>
2017-09-03 22:10:45 +0900

    student_guidance test

--- src/content/util/student_guide.php
+++ src/content/util/student_guide.php
@@ -22,7 +22,7 @@ function printContent(){
                 <tbody>
                     <tr>
                         <td>
-                            <ui-select multiple ng-model="accusedSelect.selectedPeople" theme="bootstrap" ng-disabled="disabled" sortable="true" close-on-select="false">
+                            <!--ui-select multiple ng-model="accusedSelect.selectedPeople" theme="bootstrap" ng-disabled="disabled" sortable="true" close-on-select="false">
                                 <ui-select-match placeholder="기소할 학생을 선택해주세요...">{{$item.grade}}-{{$item.name}}</ui-select-match>
                                 <ui-select-choices repeat="accused in (accusedArray | filter: $select.search) track by accused.student_id">
                                     <div ng-bind-html="accused.name | highlight: $select.search"></div>
@@ -30,17 +30,19 @@ function printContent(){
                                         {{accused.grade}}학년 {{accused.name}}
                                     </small>
                                 </ui-select-choices>
-                            </ui-select>
+                            </ui-select-->
+                            <input type="text" class="form-control" value="<?php echo htmlspecialchars($me['s_name']) ?>" ng-disabled="point_disabled">
                         </td>
                         <td>
-                            <div class="input-group">
+                            <!--div class="input-group">
                                 <input type="text" class="form-control" uib-datepicker-popup="yyyy-MM-dd" ng-model="accused_date2" is-open="popup" datepicker-options="dateOptions" ng-required="true" close-text="Close">
                                 <span class="input-group-btn">
                                     <button type="button" class="btn btn-default" ng-click="openCalender()">
                                         <i class="glyphicon glyphicon-calendar"></i>
                                     </button>
                                 </span>
-                            </div>
+                            </div-->
+                            <input type="text" class="form-control" value="<?php echo htmlspecialchars($me['s_name']) ?>" ng-disabled="point_disabled">
                         </td>
                         <td><input type="text" class="form-control" value="<?php echo htmlspecialchars($me['s_name']) ?>" ng-disabled="point_disabled"></td>
                         <td><input type="text" class="form-control" value="Late for School" ng-disabled="point_disabled"></td>
