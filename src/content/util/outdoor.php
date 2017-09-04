1f5a41c436b479527f19d5a57f03c47c4da4c735
HJ Kim <hyeonjae42@gmail.com>
2017-09-04 21:59:13 +0900

    outdoor angular test

--- src/content/util/outdoor.php
+++ src/content/util/outdoor.php
@@ -7,7 +7,7 @@ function printContent(){
     $me=array_merge($me, $member->getAdditionalData($me['n_id']));
     ?>
     <h1>외출 외박 신청서 작성</h1>
-        <form ng-app="outdoor" ng-controller="outdoorCtrl">
+        <form ng-app="outdoor" ng-controller="outdoorCtrl" ng-init="info.grade=<?php echo htmlspecialchars($me['n_grade']) ?>">
             <table class="table table-striped">
                 <tbody>
                     <tr>
