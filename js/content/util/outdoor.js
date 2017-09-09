5c9e00f5645d7a6d9128e031adbdbf7b8d2195ab
HJ Kim <hyeonjae42@gmail.com>
2017-09-10 00:12:28 +0900

    outdoor data test

--- js/content/util/outdoor.js
+++ js/content/util/outdoor.js
@@ -7,8 +7,11 @@ app.controller("outdoorCtrl", function($http, $scope){
         $http({
             method: 'GET',
             url: '/proc/util/outdoor-basic'
-        }).success(function mySuccess(response){
+        }).then(function mySuccess(response){
             $scope.info = response;
+            $scope.status = response.statusText;
+        }, function myError(response){
+            $scope.status = response.statusText;
         })
     }
 });
