2a1751a05db923d8e65990083a7ded9facc84f31
HJ Kim <hyeonjae42@gmail.com>
2017-09-09 23:52:50 +0900

    outdoor

--- src/process/util/outdoor-basic.php
+++ src/process/util/outdoor-basic.php
@@ -3,7 +3,7 @@
         global $member, $me;
         $temp = $member->getAdditionalData($me['n_id']);
         echo $member->getMember($me['n_id'])['s_name'];
-        echo $temp['grade'];
+        echo $temp['n_grade'];
 
     }
 ?>
