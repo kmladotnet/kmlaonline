0b1a018e56f8b682d2ed55231d02f805f0437f7d
HJ Kim <hyeonjae42@gmail.com>
2017-09-09 23:52:14 +0900

    outdoor

--- src/process/util/outdoor-basic.php
+++ src/process/util/outdoor-basic.php
@@ -1,8 +1,9 @@
 <?php
     if(isset($_SESSION['user'])){
-        global $member;
-        echo $member->getMember(1576)['s_name'];
-        echo getMyBasicInfo();
+        global $member, $me;
+        $temp = $member->getAdditionalData($me['n_id']);
+        echo $member->getMember($me['n_id'])['s_name'];
+        echo $temp['grade'];
 
     }
 ?>
