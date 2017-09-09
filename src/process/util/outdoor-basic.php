acce2db1bf740e530806b5cc1e8296702a83847b
HJ Kim <hyeonjae42@gmail.com>
2017-09-09 23:57:12 +0900

    outdoor

--- src/process/util/outdoor-basic.php
+++ src/process/util/outdoor-basic.php
@@ -1,9 +1,16 @@
 <?php
     if(isset($_SESSION['user'])){
         global $member, $me;
+        $result = array();
+
         $temp = $member->getAdditionalData($me['n_id']);
-        echo $member->getMember($me['n_id'])['s_name'];
-        echo $temp['n_grade'];
+        $result['name'] = $me['s_name'];
+        $result['grade'] = $temp['n_grade'];
+        $result['class'] = $temp['s_class'];
+        $result['room'] = $temp['s_room'];
+        $result['phone_number'] = $temp['s_phone'];
+
+        echo json_encode($result);
 
     }
 ?>
