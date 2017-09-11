7f822c3d143bbd0826255f1c218b8c7b1d6793bf
HJ Kim <hyeonjae42@gmail.com>
2017-09-11 18:46:54 +0900

    test outdoor

--- js/content/util/outdoor.js
+++ js/content/util/outdoor.js
@@ -23,4 +23,12 @@ app.controller("outdoorCtrl", function($http, $scope){
             $scope.status = response.statusText;
         });
     };
+
+    $scope.printOut = function(divName){
+        var printContents = document.getElementById(divName).innerHTML;
+        var popupWin = window.open('', '_blank', 'width=300, height=300');
+        popupWin.document.open();
+        popupWin.document.write('<html><head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
+        popupWin.document.close();
+    };
 });
