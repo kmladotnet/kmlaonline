1678aa106299cec829824e34af6de93ab44aeff2
HJ Kim <hyeonjae42@gmail.com>
2017-09-12 16:31:24 +0900

    change outdoor.js (start_date)

--- js/content/util/outdoor.js
+++ js/content/util/outdoor.js
@@ -4,15 +4,13 @@ app.controller("outdoorCtrl", function($http, $scope){
     $scope.info = {};
     $scope.submitted = false;
 
-    $scope.monthArray = [{ name: "1월", value: 1 }, { name: "2월", value: 2 },
-                        { name: "3월", value: 3 }, { name: "4월", value: 4 },
-                        { name: "5월", value: 5 }, { name: "6월", value: 6 },
-                        { name: "7월", value: 7 }, { name: "8월", value: 8 },
-                        { name: "9월", value: 9 }, { name: "10월", value: 10 },
-                        { name: "11월", value: 11 }, { name: "12월", value: 12 }];
-
+    $scope.monthArray = [];
     $scope.dateArray = [];
     $scope.timeArray = [];
+
+    for(var k = 1; k < 13; k++){
+        $scope.monthArray.push({ name: i + "월", value: i});
+    }
 
     for(var i = 0; i < 31; i++){
         $scope.dateArray.push({ name: (i + 1) + "일", value: (i + 1)});
