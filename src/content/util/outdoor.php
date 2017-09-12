929e84e3d1cc33734026de9ef0dd784d9529c5d3
HJ Kim <hyeonjae42@gmail.com>
2017-09-13 00:41:27 +0900

    Test

--- src/content/util/outdoor.php
+++ src/content/util/outdoor.php
@@ -12,7 +12,7 @@ function printContent(){
                     <input type="checkbox" ng-model="submitted" ng-disabled="validity()">
                     수정 모드
                 </label>
-                <p>작성을 완료하고 수정 모드를 해제하세요.(필수 항목 * 을 체크해야 인쇄할 수 있습니다.)</p>
+                <p>작성을 완료하고 수정 모드를 해제하세요. (필수 항목 * 을 모두 체크해야 인쇄할 수 있습니다.)</p>
             </div>
             <div style="text-align: center;">
                 <button class="btn btn-info" ng-if="!submitted" ng-click="printOut('print_area')">인쇄</button>
