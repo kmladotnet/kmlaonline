1bf6929d7301027e32fbd503929c92d42ec1f301
HJ Kim <hyeonjae42@gmail.com>
2017-09-14 09:02:59 +0900

    add page for barbeque - (change in index.php)

--- teacher/content/main.php
+++ teacher/content/main.php
@@ -13,7 +13,7 @@
     <link href="/teacher/css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <!--link href="dashboard.css" rel="stylesheet"-->
+    <link href="/teacher/css/dashboard.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
