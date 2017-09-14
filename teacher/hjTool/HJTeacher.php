ef0f0a362d70143d4f98a2823f71c9929f3c4448
HJ Kim <hyeonjae42@gmail.com>
2017-09-14 15:39:09 +0900

    test

--- teacher/hjTool/HJTeacher.php
+++ teacher/hjTool/HJTeacher.php
@@ -46,5 +46,6 @@ class HJTeacher{
             return false;
         }
     }
+
 }
 ?>
--- teacher/lib.php
+++ teacher/lib.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set("Asia/Seoul");
 include "hjTool/HJTools.php";
-require(__DIR__."/hjTool/dbHandler.php");
+require(__DIR__ . "/hjTool/dbHandler.php");
 
 ?>
