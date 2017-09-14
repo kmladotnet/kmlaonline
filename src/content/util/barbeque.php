66c5603e13075606ddea0c17a398be8035b9e1bb
HJ Kim <hyeonjae42@gmail.com>
2017-09-15 01:22:32 +0900

    barbeque page

--- src/content/util/barbeque.php
+++ src/content/util/barbeque.php
@@ -4,27 +4,37 @@ $title = "바베큐 신청 - " . $title;
 
 function printContent(){
     ?>
-    <h1>바베큐 신청</h1>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bbq-navbar-collapse" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+    <div ng-app="bbqApp" ng-controller="bbqCtrl">
+        <h1>바베큐 신청</h1>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bbq-navbar-collapse" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="bbq-navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a class="navbar-brand" href="">HOME</a></li>
+                        <li><a href="">신청하기</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="">내 바베큐</a></li>
+                    </ul>
+                </div>
             </div>
-            <div class="collapse navbar-collapse" id="bbq-navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a class="navbar-brand" href="">HOME</a></li>
-                    <li><a href="">신청하기</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="">내 바베큐</a></li>
-                </ul>
+        </nav>
+
+        <div class="container-fluid">
+            <div class="col-xs-5">
+            </div>
+            <div class="col-xs-7">
+                <ng-include src=""></ng-include>
             </div>
         </div>
-    </nav>
+    </div>
 <?php }
 ?>
