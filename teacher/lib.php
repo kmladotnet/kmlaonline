320fb1877be464a7b1a93f038f8c4ad449d3f237
HJ Kim <hyeonjae42@gmail.com>
2017-09-14 20:15:58 +0900

    add barbeque db

--- teacher/lib.php
+++ teacher/lib.php
@@ -7,5 +7,6 @@ require(__DIR__ . "/hjTool/dbHandler.php");
 //echo $teacher->authTeacher("hyeonjae", "guswo2") + "\n";
 //echo $teacher->authTeacher("hyeonjae", "guswo1") + "\n";
 //echo $teacher->authTeacher("hyeonjae2", "guswo1") + "\n";
+echo "<p>test</p>";
 echo $barbeque->addBarbeque("2017-09-14", "17:30", "18:40", 1, "test bbq", "1234|1923|2212", 1576);
 ?>
