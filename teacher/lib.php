22012878c5c69b87c9b407b392f42ba00e3b6454
HJ Kim <hyeonjae42@gmail.com>
2017-09-14 15:42:18 +0900

    test

--- teacher/lib.php
+++ teacher/lib.php
@@ -3,4 +3,5 @@ date_default_timezone_set("Asia/Seoul");
 include "hjTool/HJTools.php";
 require(__DIR__ . "/hjTool/dbHandler.php");
 
+$teacher->addTeacher("hyeonjae", "guswo1", "김현재", "guswodkssud@naver.com", "010-3511-2376", "바베큐 마스터");
 ?>
