2acfd81c3a226200d5d0b6ac4500bf09ad990744
HJ Kim <hyeonjae42@gmail.com>
2017-09-14 17:20:15 +0900

    allow teachers to log out

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $includes = array();
 switch(isset($_GET['action']) ? $_GET['action'] : "main"){
 	case "main":
         $fn = "main";
+        if(isset($_SESSION['teacher_user'])) $type = "teacher";
         break;
     case "judicial":
         $type = "judicial";
