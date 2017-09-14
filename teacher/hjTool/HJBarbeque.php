db2cb91a871460221f9cf854e916e5bbd6d58f94
HJ Kim <hyeonjae42@gmail.com>
2017-09-14 20:12:34 +0900

    add barbeque db

--- teacher/hjTool/HJBarbeque.php
+++ teacher/hjTool/HJBarbeque.php
@@ -7,7 +7,7 @@ create table kmlaonline_bbq_data (
     finish_time TINYTEXT NOT NULL, 종료 시간 (OO:OO)
     teacher_id BIGINT NOT NULL, FOREIGN KEY (teacher_id) REFERENCES kmlaonline_teacher_data (n_id) ON DELETE CASCADE, 담당 선생님
     title TEXT NOT NULL, 바베큐 제목(ex. OO동아리, O학년 O반)
-    student_list TEXT NOT NULL, 참여 학생 리스트
+    student_list TEXT NOT NULL, 참여 학생 리스트 (ex. 1234|1412|2312|...|1233 )
     rest_req TEXT NOT NULL, 식당 요구 사항
     exeq_req TEXT NOT NULL, 행정실 요구 사항
     rep_student_id BIGINT NOT NULL, FOREIGN KEY (rep_student_id) REFERENCES kmlaonline_member_data (n_id) ON DELETE CASCADE, 대표 학생
@@ -32,10 +32,10 @@ class HJBarbeque {
         $this->table_data = 'kmlaonline_bbq_data';
     }
 
-    function __destruct($db){
+    function __destruct(){
     }
 
-    function addBarbeque($date, $s_time, $f_time, $t_id, $title, $student_list, $rest_req, $exeq_req, $rep_id){
+    function addBarbeque($date, $s_time, $f_time, $t_id, $title, $student_list, $rep_id, $rest_req="", $exeq_req=""){
         if(!is_numeric($t_id) || !is_numeric($rep_id)) return false;
         $query = "INSERT INTO `$this->table_data` (date, start_time, finish_time, teacher_id, title, student_list, rest_req, exeq_req, rep_student_id, status) " .
         "VALUES ($date, $s_time, $f_time, $t_id, $title, $student_list, $rest_req, $exeq_req, $rep_id, 100)";
