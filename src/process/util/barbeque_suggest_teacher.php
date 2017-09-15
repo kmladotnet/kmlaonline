43c96ffc438a7cea07ec4db6d0dbe6cac0d9452f
HJ Kim <hyeonjae42@gmail.com>
2017-09-15 19:30:23 +0900

    add getAllRawTeachers()

--- src/process/util/barbeque_suggest_teacher.php
+++ src/process/util/barbeque_suggest_teacher.php
@@ -1,15 +1,7 @@
 <?php
     if(isset($_SESSION['user'])){
         global $teacher;
-        $result = array();
-
-        $temp = $teacher->getAllRawTeachers();
-        $result['name'] = $me['s_name'];
-        $result['grade'] = $temp['n_grade'];
-        $result['class'] = $temp['s_class'];
-        $result['room'] = $temp['s_room'];
-        $result['phone_number'] = $me['s_phone'];
-
+        $result = $teacher->getAllRawTeachers();
         echo json_encode($result);
     }
 ?>
