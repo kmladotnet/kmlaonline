a5f201ce6df012ea37b4f923da0e41cac6d97b0e
HJ Kim <hyeonjae42@gmail.com>
2017-09-25 14:44:52 +0900

    add bbq page

--- teacher/js/main.js
+++ teacher/js/main.js
@@ -1,6 +1,6 @@
 var app = angular.module("teacherApp", []);
 
-app.controller("teacherCtrl", function($scope){
+app.controller("teacherCtrl", function($scope, $rootScope){
     $scope.page = "/teacher/template/main.html";
 
     $scope.changePage = function(page){
@@ -10,6 +10,9 @@ app.controller("teacherCtrl", function($scope){
                 break;
             case 'bbq':
                 $scope.page = '/teacher/template/bbq.html';
+                $rootScope.$broadcast("bbqSelected", {
+                    page: 'bbq'
+                });
                 break;
         }
     }
@@ -17,4 +20,38 @@ app.controller("teacherCtrl", function($scope){
 
 app.controller("bbqCtrl", function($scope){
     $scope.bbqRequestedList = [{title: '12학년 11반', student_list: '김현재|이영재|최우석', date: '2017-09-25', status: 300}];
+
+    $scope.$on("bbqSelected", function(event, args){
+        $scope.config_calender();
+    });
+
+    $scope.config_calender = function(){
+
+        $scope.hourArray = [];
+        $scope.minArray = [];
+
+        for(var j = 9; j < 21; j++){
+            $scope.hourArray.push({ name: (j < 12 ? "오전 " : "오후 ") + (j == 12 ? 12 : j % 12) + "시", value: j});
+        }
+
+        for(var i = 0; i < 6; i++){
+            $scope.minArray.push({ name: i + "0분", value: i * 10});
+        }
+
+        var now = new Date();
+
+        var now_year = now.getFullYear();
+        var now_month = now.getMonth(); // getMonth() returns month(0 - 11)
+
+        $scope.numberOfDays = new Date(now_year, now_month + 1, 0).getDate();
+        $scope.firstDay = new Date(now_year, now_month, 1).getDay();
+        //$scope.numberOfWeeks = 1 + ($scope.numberOfDays - (7 - $scope.firstDay) - 1) / 7 + 1;
+        $scope.calender = [];
+
+        for(var i = -$scope.firstDay, j = 0; i < $scope.numberOfDays; i++, j++){
+            if(typeof $scope.calender[Math.floor(j / 7)] === 'undefined') $scope.calender[Math.floor(j / 7)] = [];
+            if(i >= 0) $scope.calender[Math.floor(j / 7)][j % 7] = i + 1;
+            else $scope.calender[Math.floor(j / 7)][j % 7] = "";
+        }
+    };
 });
