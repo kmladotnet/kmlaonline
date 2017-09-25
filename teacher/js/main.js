997540bc7660eb5e091adaa1ac3026ed2af19aa4
HJ Kim <hyeonjae42@gmail.com>
2017-09-25 13:55:25 +0900

    teacher site - change dir

--- teacher/js/main.js
+++ teacher/js/main.js
@@ -6,15 +6,15 @@ app.controller("teacherCtrl", function($scope){
     $scope.changePage = function(page){
         switch(page){
             case 'main':
-                $scope.page = "../template/main.html";
+                $scope.page = "teacher/template/main.html";
                 break;
             case 'bbq':
-                $scope.page = '../template/bbq.html';
+                $scope.page = 'teacher/template/bbq.html';
                 break;
         }
     }
 });
 
 app.controller("bbqCtrl", function($scope){
-    $scope.bbqRequestedList = [{title: '12학년 11반', student_list: '김현재|이영재|최우석', date: '2017-09-25', status: 300}]
+    $scope.bbqRequestedList = [{title: '12학년 11반', student_list: '김현재|이영재|최우석', date: '2017-09-25', status: 300}];
 });
