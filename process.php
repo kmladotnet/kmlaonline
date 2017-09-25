4c1a359fd033ef4dc5e17c2c347f7842766b0992
HJ Kim <hyeonjae42@gmail.com>
2017-09-25 15:46:49 +0900

    change process.php for teacher page

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 include "src/lib.php";
 include "teacher/lib.php";
-if(isset($_REQUEST['teacher'])){
+if(isset($_REQUEST['teacher']) && $_REQUEST['teacher'] == 'yes'){
     $fn = "teacher/process/" . basename($_REQUEST['action']) . ".php";
 } else {
     $fn="src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
