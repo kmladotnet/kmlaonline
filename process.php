75fd3fe0d62dcaad5d652d4fa9fc8d4d4c81218d
HJ Kim <hyeonjae42@gmail.com>
2017-09-25 16:19:42 +0900

    change nginx.conf for teacher page

--- process.php
+++ process.php
@@ -3,6 +3,7 @@ include "src/lib.php";
 include "teacher/lib.php";
 if(isset($_REQUEST['teacher']) && $_REQUEST['teacher'] == 'yes'){
     $fn = "teacher/process/" . basename($_REQUEST['actiontype']) . ".php";
+    echo $fn;
 } else {
     $fn="src/process/".basename($_REQUEST['actiontype'])."/".basename($_REQUEST['action']).".php";
 }
