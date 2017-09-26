fe1c3dff2ae0eb8b2ebc73b24983882aa0d9ed8f
HJ Kim <hyeonjae42@gmail.com>
2017-09-26 12:05:19 +0900

    html angular check

--- src/process/util/barbeque_submit_new.php
+++ src/process/util/barbeque_submit_new.php
@@ -14,7 +14,8 @@
         $finish_min = $result->finish_min->value;
         $teacher = $result->selectedTeacher->n_id;
         $participant = $result->selectedStudent;
-
+        $exeq_req = $result->exeq_req;
+        $rest_req = $result->rest_req;
         // 2. notification
 
         // 3. save data
@@ -27,7 +28,7 @@
 
         //TODO 연도 나중에 바꿔줄 것 (javascript로 받으세요)
         //function addBarbeque($date, $s_time, $f_time, $t_id, $title, $student_list, $rep_id, $rest_req="", $exeq_req="", $status)
-        $barbeque->addBarbeque("2017-09-" . $date, $start_hr . ":" . $start_min, $finish_hr . ":" . $finish_min, $teacher, "임시 테스트", $participant_f, $me['n_id'], "", "", 100);
+        $barbeque->addBarbeque("2017-09-" . $date, $start_hr . ":" . $start_min, $finish_hr . ":" . $finish_min, $teacher, "임시 테스트", $participant_f, $me['n_id'], $rest_req, $exeq_req, 100);
         echo print_r($result);
         http_response_code(200);
     } else {
