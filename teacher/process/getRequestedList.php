98a823559bc47760e648f1c189923e235e5cd339
HJ Kim <hyeonjae42@gmail.com>
2017-09-26 09:45:16 +0900

    set  in teacher page

--- teacher/process/getRequestedList.php
+++ teacher/process/getRequestedList.php
@@ -1,9 +1,9 @@
 <?php
     if(isset($_SESSION['teacher_user'])){
         if(isset($_GET['status']) && $_GET['status'] === '100'){
-            echo getMyRequestedList($me['n_id'], 100);
+            echo json_encode(getMyRequestedList($me['n_id'], 100));
         } else {
-            echo getMyRequestedList($me['n_id'], 200);
+            echo json_encode(getMyRequestedList($me['n_id'], 200));
         }
     } else {
         http_response_code(403);
