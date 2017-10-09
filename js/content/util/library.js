edac8929e22095358449211596c15ab3df5b9595
HJ Kim <hyeonjae42@gmail.com>
2017-10-10 08:57:37 +0900

    book search change

--- js/content/util/library.js
+++ js/content/util/library.js
@@ -19,6 +19,13 @@ app.controller("libCtrl", function($scope, $http){
         });
     }
 
+    $scope.bookDesc = function(book){
+        console.log("<p>" + book.title + "</p>"
+                + "<p>" + book.author + " 지음 | " + book.publisher + " | " + book.pubdate + "</p>");
+        return "<p>" + book.title + "</p>"
+                + "<p>" + book.author + " 지음 | " + book.publisher + " | " + book.pubdate + "</p>";
+    }
+
     $scope.changePage = function(page){
 
     };
