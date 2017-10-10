a812087f2799d3ebd5d92728e3bb7fa240f40061
HJ Kim <hyeonjae42@gmail.com>
2017-10-10 10:16:18 +0900

    book search - image size

--- index.direct.php
+++ index.direct.php
@@ -177,10 +177,11 @@
         <div class="hide-on-upper-panel" style="position:absolute;background:#074275;left:0;width:100%;right:0;display:block;height:40px;z-index:-5;">
             <div class="total-header-extend-1" style="position:absolute;background:#074275;left:0;width:100%;right:0;display:block;height:40px;"></div>
         </div>
+        <?php if($_SERVER["REQUEST_URI"] !== '/util/library') { ?>
         <div class="hide-on-upper-panel" style="position:absolute;left:0;width:100%;right:0;display:block;height:160px;top:40px;z-index:-5;">
             <div class="total-header-extend-1" style="position:absolute;left:0;width:100%;right:0;display:block;height:160px; <?php if($april_fools_2) echo 'height:311px; background: url(/images/bamboozle-bg.png) repeat-x center bottom;'; ?>"></div>
             <div class="total-header-extend-2" style="position:absolute;left:0;width:100%;right:0;display:block;height:160px;"></div>
-        </div>
+        </div> <?php } ?>
     <?php } ?>
     <div id="divSavingIndicatorFiller"></div>
     <div style="" id="divSavingIndicator">
@@ -210,7 +211,7 @@
         </div>
         <div style="clear:both"></div>
         <div id="below-header-menu">
-            <?php include "src/header/below-header.php"; ?>
+            <?php if($_SERVER["REQUEST_URI"] !== '/util/library') include "src/header/below-header.php"; ?>
         </div>
     </div>
     <div id="total-footer" class="hide-on-upper-panel">
