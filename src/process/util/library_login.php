e6bb39571eecdf697c7d0f35a1a7f7133bac2e1f
HJ Kim <hyeonjae42@gmail.com>
2017-10-12 12:07:18 +0900

    debug - library login(cookie)

--- src/process/util/library_login.php
+++ src/process/util/library_login.php
@@ -9,6 +9,9 @@
 
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_URL, $url);
+        $test = realpath('/tmp/library/' . $n_student_id . '.txt');
+        echo $test;
+        file_put_contents($test, "data");
         curl_setopt($ch, CURLOPT_COOKIEJAR, '/tmp/library/' . $n_student_id . '.txt');
         $headers = array(
             "Access-Control-Allow-Origin: *",
