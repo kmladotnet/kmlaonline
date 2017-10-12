e68731e53f22ffefeb52ca9323c7849cbb5d6467
HJ Kim <hyeonjae42@gmail.com>
2017-10-12 22:54:38 +0900

    upload debug

--- src/process/board/upload.php
+++ src/process/board/upload.php
@@ -3,7 +3,7 @@ redirectLoginIfRequired();
 if(count($_FILES)==0)
 	die(json_encode(array("error"=>"파일 크기가 너무 큽니다.")));
 $file_path="data/temp/";
-@mkdir($file_path,0777,true);
+@$mkdir = mkdir($file_path,0777,true);
 $file_real_name=sanitizeFileName(@end(explode('/', $_FILES['Filedata']['name'])));
 $fn=uniqid("up_",true);
 $rfile=$file_path.$fn;
@@ -11,7 +11,7 @@ $rfile=$file_path.$fn;
 $debug = print_r($_FILES, true);
 //debug
 if(@move_uploaded_file($_FILES['Filedata']['tmp_name'], $rfile)===false){
-	die(json_encode(array("error"=>"알 수 없는 오류가 발생하였습니다.", "debug_rfile"=>$rfile, "debug_"=>$_FILES['Filedata']['tmp_name'], "debug_file"=>$debug)));
+	die(json_encode(array("error"=>"알 수 없는 오류가 발생하였습니다.", "debug_rfile"=>$rfile, "debug_tmp_name"=>$_FILES['Filedata']['tmp_name'], "debug_file"=>$debug, "mkdir"=>$mkdir)));
 }else
 	echo json_encode(array(
 		"error"=>0,
