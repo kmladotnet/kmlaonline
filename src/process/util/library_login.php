03c0a76e6ac425e6608186629c344249b5a4bbc8
HJ Kim <hyeonjae42@gmail.com>
2017-10-14 19:37:25 +0900

    login error handling

--- src/process/util/library_login.php
+++ src/process/util/library_login.php
@@ -93,10 +93,12 @@
             $final_array['bookList'] = $tmp_arr;
 
             echo json_encode($final_array);
+        } else if(strpos($output_, 'USMN610')){
+            echo array("error"=>"LOGIN_ERROR", "error_desc"=>"비밀번호가 올바르지 않습니다.");
+            http_response_code(400);
         } else {
-            echo "<!-->" . $output_ . "<-->";
-            if(strpos($output_, 'USMN610')) echo "success";
-            else echo "failed";
+            echo array("error"=>"UNKNOWN_ERROR", "error_desc"=>"예기치 않은 문제가 발생하였습니다. 관리자에게 문의 바랍니다.");
+            http_response_code(400);
         }
         //var_dump($login_box);
         //echo $encoded_output;
