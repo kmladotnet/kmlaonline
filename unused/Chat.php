8b2746c1c13cc132ed7ea6c3d0804a7387e9f098
HJ Kim <hyeonjae42@gmail.com>
2017-10-15 18:01:08 +0900

    change chat.php

--- unused/Chat.php
+++ unused/Chat.php
@@ -7,15 +7,38 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct(){
+        $this->clients = new \SplObjectStorage;
+    }
+
     public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection $d sending message "$s" to $d other connection$s' . "\n", $from->resouceId, $msg, $numRecv === 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
     }
 
     public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resouceId} has disconnected\n";
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occured: {$e->getMessage()}\n";
+
+        $conn->close();
     }
 }
