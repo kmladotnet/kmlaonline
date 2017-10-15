0467dc9323331b5bd780f8263cafdb297c9994f1
HJ Kim <hyeonjae42@gmail.com>
2017-10-15 18:07:51 +0900

    change chat.php

--- unused/Chat.php
+++ unused/Chat.php
@@ -21,7 +21,7 @@ class Chat implements MessageComponentInterface {
 
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection $d sending message "$s" to $d other connection$s' . "\n", $from->resourceId, $msg, $numRecv === 1 ? '' : 's');
+        echo sprintf('Connection %d sending message "%s" to %d other connection$s' . "\n", $from->resourceId, $msg, $numRecv === 1 ? '' : 's');
 
         foreach ($this->clients as $client) {
             if ($from !== $client) {
