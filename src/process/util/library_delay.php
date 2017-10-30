c5917a81679aa36a7fae3ede864db366f53e6e59
HJ Kim <hyeonjae42@gmail.com>
2017-10-31 00:12:11 +0900

    library delay returnig date project(test)

--- src/process/util/library_delay.php
+++ src/process/util/library_delay.php
@@ -22,13 +22,32 @@
                 $output = curl_exec($ch);
                 $output = mb_convert_encoding($output, "UTF-8", "EUC-KR");
 
-                $dom = new DOMDocument('1.0', 'UTF-8');
-                @$dom->loadHTML($output);
-
+                $error_code = 0;
+                $success = false;
                 if(strpos($output, '연장횟수를')){
+                    //메시지 내용 - 연장횟수를 초과하여 연장할 수 없습니다.
+                    $error_code = 1;
                     echo "연장 횟수 초과";
+                } else if(strpos($output, '로그인 후')){
+                    //메시지 내용 - 로그인 후 이용하시기 바랍니다.
+                    $error_code = 2;
+                    echo "로그인 되어 있지 않음";
+                } else if(strpos($output, '대출연장이')){
+                    //메시지 내용 - 대출연장이 처리되었습니다.
+                    $success = true;
+                    echo "성공";
                 } else {
-                    echo "연장 횟수 초과 말고 다른 케이스";
+                    //로그인 되어 있는 상태에서 엉뚱한 요청을 보낸 경우
+                    $error_code = 3;
+                    echo "알 수 없는 오류가 발생";
+                }
+
+                if($success) {
+                    echo json_encode(array("status"=>"success"));
+                    http_response_code(200);
+                } else {
+                    echo json_encode(array("status"=>"fail", "error_code"=>$error_code));
+                    http_response_code(423);
                 }
             } else {
                 echo json_encode(array("error"=>"LOGIN_ERROR", "error_desc"=>"도서관 로그인 실패"));
