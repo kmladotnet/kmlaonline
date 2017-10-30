2edbc0d27a27e4e5c0beeefc051f096f4cf3d31f
HJ Kim <hyeonjae42@gmail.com>
2017-10-31 01:12:56 +0900

    notification setting change

--- index.direct.php
+++ index.direct.php
@@ -99,7 +99,7 @@
     <!-- other -->
     <script src="/swfupload/swfupload.js"></script>
     <script src="/swfupload/swfupload.queue.js"></script>
-    <script src="/js/script.js?v=7.3"></script>
+    <script src="/js/script.js?v=7.4"></script>
     <?php if(getTheme($me)['pinmenu']) { ?>
         <script>
             keepMenuShown = true;
