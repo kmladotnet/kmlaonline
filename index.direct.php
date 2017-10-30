9a227770f426d81018716f7315ebb534bb39f68d
HJ Kim <hyeonjae42@gmail.com>
2017-10-30 19:17:13 +0900

    change logo color

--- index.direct.php
+++ index.direct.php
@@ -8,7 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Facebook preview 관련 -->
+
+    <meta property="og:type"   content="website" />
     <meta property="og:title" content="KMLA Online" />
+    <meta property="og:url" content="http:// echo <?php $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>" />
     <meta property="og:description" content="민족사관고등학교 학생 커뮤니티 KMLA Online과 함께 하세요." />
     <meta property="og:image" content="https://kmlaonline.net/images/logo-inverse-autumn.png" />
 
