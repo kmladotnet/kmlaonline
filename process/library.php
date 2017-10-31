b7c73ac5bea8cc3d81231188a355ec20f6c5db40
HJ Kim <hyeonjae42@gmail.com>
2017-10-31 14:56:16 +0900

    library script(run in regular intervals)

--- process/library.php
+++ process/library.php
@@ -1,8 +1,9 @@
 <?php
+    include "src/lib.php";
     if(isset($_SESSION['user'])){
         // 일정한 빈도와 시간에 따라서 실행되도록 하는 스크립트
         // 목적 - 데이터베이스에 등록되어 있는 계정들에 자동 로그인 후 정보 업데이트
-        include "src/lib.php";
+
 
         $user_info = getAllLibraryUserInfo();
         print_r($user_info);
