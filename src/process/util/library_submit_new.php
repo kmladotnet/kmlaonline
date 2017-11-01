d29f6bf74da0a12cb1947aa59c06c45a5a157cd4
HJ Kim <hyeonjae42@gmail.com>
2017-11-01 14:28:44 +0900

    add db setting for new book request

--- src/process/util/library_submit_new.php
+++ src/process/util/library_submit_new.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_SESSION['user'])){
-    $result = json_decode(file_get_contents("php://input"));
+    $result = json_decode(file_get_contents("php://input"), true);
     print_r($result);
 
     $failed = false;
