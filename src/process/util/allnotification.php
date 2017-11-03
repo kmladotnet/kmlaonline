85a35cbb51936d1542cdfeced0716b98ee98334a
HJ Kim <hyeonjae42@gmail.com>
2017-11-04 03:56:32 +0900

    (off) notification to all

--- src/process/util/allnotification.php
+++ src/process/util/allnotification.php
@@ -1,5 +1,5 @@
 <?php
-
+    /*
     foreach($member->listMembers(0,0,19) as $usr){
         $member->addNotice(769, $usr['n_id'], "admin:info:profile", "큼온이 프로필 사진을 바꿨습니다. 여러분도 프로필 사진을 설정에서 업데이트 해보세요!", "/user/settings");
     }
@@ -11,5 +11,5 @@
     }
     foreach($member->listMembers(0,0,22) as $usr){
         $member->addNotice(769, $usr['n_id'], "admin:info:profile", "큼온이 프로필 사진을 바꿨습니다. 여러분도 프로필 사진을 설정에서 업데이트 해보세요!", "/user/settings");
-    }
+    } */
     //$member->addNotice(769, $me['n_id'], "admin:info:profile", "큼온이 프로필 사진을 바꿨습니다. 여러분도 프로필 사진을 설정에서 업데이트 해보세요!", "/user/settings");
