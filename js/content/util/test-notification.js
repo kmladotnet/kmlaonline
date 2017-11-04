737ff269366da59e1092638b842f16b4933004c9
HJ Kim <hyeonjae42@gmail.com>
2017-11-04 15:25:19 +0900

    test - notification

---
+++ js/content/util/test-notification.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var app = angular.module("testApp", ['ui.bootstrap', 'ngSanitize']);
+
+app.controller("testCtrl", function($scope, $http){
+
+    $scope.init = function(){
+        $scope.changePage('home');
+        $scope.selected = {};
+        $scope.isBookSelected = false;
+    };
+
+    $scope.bookFetch = function(query) {
+        $http({
+            method: "GET",
+            url: "/proc/util/library_api?query=" + query,
+        }).then(function mySuccess(response){
+            $scope.status = response.statusText;
+            $scope.testResult = response.data;
+            $scope.bookList = response.data.items;
+            console.log($scope.bookList);
+        }, function myError(response){
+            $scope.testResult = response.data || 'Request failed';
+            $scope.status = response.statusText;
+        });
+    };
+
+    $scope.submit = function(){
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+
+        $http({
+            method: 'POST',
+            url: '/proc/util/test_notification',
+            data: $scope.selected,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }).then(function mySuccess(response){
+            alert("성공적으로 제출!");
+            console.log("submit success");
+        }, function myError(response){
+            $scope.status = "Request failed";
+            console.log("submit failed");
+        });
+
+        return false;
+    };
+});
---
+++ src/process/util/test_notification.php
@@ -0,0 +1,38 @@
+<?php
+    if(isset($_SESSION['user'])){
+        $result = json_decode(file_get_contents("php://input"));
+        print_r($result);
+
+        $failed = false;
+        /*
+        // 1. check validity of data
+        $date = $result->date;
+        $start_hr  = $result->start_hour->value;
+        $start_min = $result->start_min->value;
+        $finish_hr = $result->finish_hour->value;
+        $finish_min = $result->finish_min->value;
+        $teacher = $result->selectedTeacher->n_id;
+        $participant = $result->selectedStudent;
+        $exeq_req = $result->exeq_req;
+        $rest_req = $result->rest_req;
+        // 2. notification
+
+        // 3. save data
+        $tmp = array();
+
+        foreach($participant as $std){
+            array_push($tmp, $std->n_id);
+        }
+        $participant_f = implode('|', $tmp);
+
+        //TODO 연도 나중에 바꿔줄 것 (javascript로 받으세요)
+        //function addBarbeque($date, $s_time, $f_time, $t_id, $title, $student_list, $rep_id, $rest_req="", $exeq_req="", $status)
+        $barbeque->addBarbeque("2017-09-" . $date, $start_hr . ":" . $start_min, $finish_hr . ":" . $finish_min, $teacher, "임시 테스트", $participant_f, $me['n_id'], $rest_req, $exeq_req, 100);
+        echo print_r($result); */
+        http_response_code(200);
+    } else {
+        http_response_code(404);
+    }
+
+
+?>
