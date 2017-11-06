c6cfa9b53f224a1efc568016c4078c755736cc8f
HJ Kim <hyeonjae42@gmail.com>
2017-11-06 16:30:37 +0900

    Test

--- src/content/util/chat.php
+++ src/content/util/chat.php
@@ -4,7 +4,7 @@ function printContent() {
     global $me;
 ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.3/handlebars.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
 
     <link rel="stylesheet" href="css/content/util/chat.css">
     <div id="wrapper">
