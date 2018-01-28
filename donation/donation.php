c1f7c4b20a3a75e9b310df992b3af2711f463aad
PacoKwon <haechank@gmail.com>
2018-01-28 12:44:53 +0900

    donation tweaks

---
+++ donation/donation.php
@@ -0,0 +1,20 @@
+<?php
+$mysqli = new mysqli('localhost', 'kmlaonline', 'n7h4eYWJ7vW59tT8', 'kmlaonline');
+
+$mysqli -> query("CREATE TABLE IF NOT EXISTS donation_test (".
+              "  n_num int(11),".
+              "  n_category int(11),".
+              "  s_title text,".
+              "  n_who bigint(20) NOT NULL,".
+              "  s_status text,".
+              "  s_type text,".
+              "  s_owner text".
+              ") ENGINE=InnoDB");
+
+$myfile = fopen('donation_output.txt', 'r');
+while(!feof($myfile)) {
+    $mysqli -> query(fgets($myfile));
+    // echo fgets($myfile) . "<br />";
+}
+fclose($myfile);
+?>
---
+++ donation/excel.py
@@ -0,0 +1,29 @@
+from openpyxl import load_workbook
+
+f = open('donation_output.txt', 'w')
+
+add_item = ("INSERT INTO donation_test "
+            "(n_num, n_category, s_title, s_status, s_type, s_owner) "
+            "VALUES ({}, {}, '{}', '{}', '{}', '{}')\n")
+
+print(add_item.format("a", "b", "c", "d", "e", "f"))
+
+wb = load_workbook('donations.xlsx')
+sheets = wb.get_sheet_names()
+for sheet in sheets:
+    ws = wb[sheet]
+    i = 1
+    while ws.cell(row=i, column=1).value is not None:
+        n_num = i
+        n_category = sheets.index(sheet) + 1
+        s_title = ws.cell(row=i, column=1).value
+        s_status = ws.cell(row=i, column=2).value if \
+            ws.cell(row=i, column=2).value is not None else ""
+        s_type = ws.cell(row=i, column=3).value if \
+            ws.cell(row=i, column=3).value is not None else ""
+        s_owner = ws.cell(row=i, column=4).value if \
+            ws.cell(row=i, column=4).value is not None else ""
+        print(add_item.format(n_num, n_category, s_title, s_status, s_type, s_owner))
+        f.write(add_item.format(n_num, n_category, s_title, s_status, s_type, s_owner))
+        i += 1
+f.close()
