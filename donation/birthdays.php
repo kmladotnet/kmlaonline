ed830d8d607550edda2c8a0ce0a00f5f2045675e
PacoKwon <haechank@gmail.com>
2018-02-01 09:36:42 +0900

    birthday

--- donation/birthdays.php
+++ donation/birthdays.php
@@ -4,7 +4,7 @@ include 'test-config.php';
 $query = "SELECT * FROM kmlaonline_member_data WHERE n_level = 23";
 if($res = $mysqli->query($query)) {
     while($row = $res->fetch_array(MYSQLI_ASSOC)) {
-        echo $row['n_birth_date_month'] . ", " . $row['n_birth_date_day'] . "<br />";
+        echo $row['s_name'] . ", " . $row['n_birth_date_month'] . ", " . $row['n_birth_date_day'] . "<br />";
     }
 }
 ?>
