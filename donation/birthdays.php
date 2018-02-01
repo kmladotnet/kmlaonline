5cd01eb425fa161e1df146a2a8ea0d4b5464c31c
PacoKwon <haechank@gmail.com>
2018-02-01 09:34:30 +0900

    birthday

--- donation/birthdays.php
+++ donation/birthdays.php
@@ -1,8 +1,7 @@
 <?php
-include '../src/lib.php';
 include 'test-config.php';
 
-$query = "SELECT * FROM kmlaonline_member_data WHERE n_level = $max_level";
+$query = "SELECT * FROM kmlaonline_member_data WHERE n_level = 23";
 if($res = $mysqli->query($query)) {
     while($row = $res->fetch_array(MYSQLI_ASSOC)) {
         echo $row['n_birth_date_month'] . ", " . $row['n_birth_date_day'];
