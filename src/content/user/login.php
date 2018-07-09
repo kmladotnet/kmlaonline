232878227eafa405af88df67a76929b1263f4c11
haechank@gmail.com <haechank@gmail.com>
2018-07-09 15:05:49 +0900

    food file error handling

--- src/content/user/login.php
+++ src/content/user/login.php
@@ -8,7 +8,7 @@ function printFood($jsonData, $month, $day, $whichMeal) {
             echo "$value <br />";
         }
     } else {
-        echo "<span style='color:#3e3e3e'>(입력되지 않음) <br> Kmlaonline 관리자에게 연락하세요!</span>";
+        echo "<span style='color:#a2a2a2'>(입력되지 않음) <br> kmlaonline 관리자에게 연락해주세요!</span>";
     }
 }
 
