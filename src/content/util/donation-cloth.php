c2fc53e2f25feb808a4893a4de08725a25f28de0
jueon an <aje51488815@gmail.com>
2019-02-19 01:06:20 +0900

    cloth donation open

--- src/content/util/donation-cloth.php
+++ src/content/util/donation-cloth.php
@@ -92,7 +92,7 @@ function printContent(){
 							<!-- if n_who is 0, 신청자가 없을 때 -->
 							<td>신청자가 없습니다</td>
 							<td>
-			          		<?php $date1 = new DateTime("now"); $date2 = new DateTime("2018-02-05");
+			          		<?php $date1 = new DateTime("now"); $date2 = new DateTime("2019-02-20");
 							if($date1 >= $date2) { ?>
 								<form method="post" action="/proc/util/donation" onsubmit="return saveAjax(this,'신청중...');">
 									<input type="hidden" name="from" value="cloth">
