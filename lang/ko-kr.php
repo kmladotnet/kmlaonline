cc9643aca2df6b90811c9bbc23777aaec8e426d7
jueon an <aje51488815@gmail.com>
2019-04-23 23:28:46 +0900

    fasdf

--- lang/ko-kr.php
+++ lang/ko-kr.php
@@ -184,8 +184,7 @@ $_LANGUAGE=array(
 				"remove"=>"숨기기"
 			)
 		),
-		"footer"=>'<div style="display: table;/* width: 100%; */margin: auto;width: calc(100% - 320px);"><div style="display: table-cell;text-align: right;"><img src="/images/kmlaonline-p-resize.png" style="/* display: block; *//* margin: auto; */height: 24px;display: inline-block;/* float: right; */">
-		닷넷 2007 - '.date("Y").'</div></div>'
+		"footer"=>'<div style="display: table;/* width: 100%; */margin: auto;width: calc(100% - 320px);"><div style="display: table-cell;text-align: right;"><img src="/images/kmlaonline-p-resize.png" style="/* display: block; *//* margin: auto; */height: 24px;display: inline-block;/* float: right; */"></div>'
 	),
     "weather"=>array(
         200 => '뇌우',
