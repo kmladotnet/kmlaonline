2946da414ae5b46cc69871fdecc8af210774ba9f
jueon an <aje51488815@gmail.com>
2019-09-19 15:23:38 +0900

    check variable numparcel

--- src/content/modules/parcel.php
+++ src/content/modules/parcel.php
@@ -1,11 +1,25 @@
 <?php
-    echo "테스트";
-    echo date("U", strtotime("-1 day"));
-    
+    //echo date("U", strtotime("-1 day"));
+    /*
         $parcel = getTodayParcel();
         if($parcel)
-            echo '오늘택배올라옴';
+        echo '오늘택배올라옴';
         else
-            echo '아님';
-    
-?>
+        echo '아님';
+        */
+    $parcel = getTodayParcel();
+    if($parcel !== null)
+    {
+        $numParcel = parcelNum($me['s_name'], $parcel);
+        echo $numparcel;
+    }
+    else
+    { 
+        ?>
+    <div style="line-height: 1em;" class="parcel-icon">
+        ?
+    </div>
+    <div style="text-align: center; font-size: 13pt;">
+        <a href="/board/everyday_parcel">택배리스트</a>
+    </div>
+<?php }
