1d92e0c674ba6d129c81b0f986410b67c76dfd51
jueon an <aje51488815@gmail.com>
2019-09-19 15:04:05 +0900

    delete test file, test function getTodayParcel

--- src/content/modules/parcel.php
+++ src/content/modules/parcel.php
@@ -1,6 +1,11 @@
 <?php
     echo "테스트";
     echo date("U", strtotime("-1 day"));
-
-
+    
+        $parcel = getTodayParcel();
+        if($parcel)
+            echo '오늘택배올라옴';
+        else
+            echo '아님';
+    
 ?>
