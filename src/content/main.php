825d2a23975d63dab44efd75592912169d9a503d
Sunghyun Cho <mail@chosunghyun.com>
2019-11-23 17:20:12 +0900

    공강 신청, 노래방 신청 버튼을 메인으로 꺼내기

--- src/content/main.php
+++ src/content/main.php
@@ -257,6 +257,8 @@ function printEverydayLinks(){
     ?>
     <div class="everyday-links">
         <button type="button" id="main-theme-button" class="btn btn-default" onclick="location = 'https://bit.ly/2n5ArDt'">학생회 상벌점 확인</button>
+        <button type="button" id="main-theme-button" class="btn btn-default" onclick="location = '/util/lectureroom'">공강 신청</button>
+        <button type="button" id="main-theme-button" class="btn btn-default" onclick="location = '/util/karaoke'">노래방 신청</button>
         <select id="everyday-other" class="selectpicker" data-style="btn-default" title="바로가기" data-width="180px" onchange="location = this.options[this.selectedIndex].value;">
             <?php
                 foreach(array("everyday_parcel" => "택배", "everyday_guidance" => "선도", "leftover" => "잔반") as $k => $v) {
@@ -277,8 +279,6 @@ function printEverydayLinks(){
             <option value="/board/department_environment">환경부</option>
             <option value="/board/student_mpt">MPT</option>
             <option value="/board/student_ambassador">대외홍보단</option>
-            <option value="/util/lectureroom">공강 신청</option>
-            <option value="/util/karaoke">노래방 신청</option>
             <?php
 			if(date(n)==2 ||date(n)==3)
                echo  "<option value='/util/donation-cloth'>기부물품 신청</option>";
