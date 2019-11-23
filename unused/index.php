b4a323ada13e8b6d0e7171be5e2ed0aab6b2902c
Sunghyun Cho <mail@chosunghyun.com>
2019-11-23 17:08:28 +0900

    [Optimize] Do not use network font.

--- unused/index.php
+++ unused/index.php
@@ -4,7 +4,7 @@
 	<style>
 		body {
 			width: 600px;
-			font-family: calibri;
+			font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
 		}
 
 		.error {
