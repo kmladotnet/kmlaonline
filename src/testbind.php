081d9c4be0135bda1f71d1cd64719f94afe1a9fe
Jungwoong Kim <dk040106@naver.com>
2020-03-08 21:30:30 +0900

    maintenance_200306

--- src/testbind.php
+++ src/testbind.php
@@ -6,6 +6,7 @@ include ("$ROOT_DIR"."XmppPrebind.php");
 $username = "present42";
 $password = "guswo1";
 
+
 $xmppPrebind = new XmppPrebind('kmlaonline.net', 'http://kmlaonline.net:5280/admin/', 'test_resource' , false, true);
 $xmppPrebind->connect($username, $password);
 $xmppPrebind->auth();
