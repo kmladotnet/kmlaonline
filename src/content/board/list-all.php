fb06daa9474c8643d5124f9363e4741c350f629a
Jungwoong Kim <dk040106@naver.com>
2022-03-01 19:44:17 +0900

    format

--- src/content/board/list-all.php
+++ src/content/board/list-all.php
@@ -1,8 +1,8 @@
 <?php
 function printArticleListTypeBoard($board_data, $additional_query_string)
 {
-    global $board, $member;
-    ?>
+	global $board, $member;
+?>
 	<table style="width:100%">
 		<thead>
 			<tr style="height:32px;">
@@ -16,18 +16,18 @@ function printArticleListTypeBoard($board_data, $additional_query_string)
 		</thead>
 		<tbody>
 			<?php foreach ($board_data as $item) {
-        $board_cat = $board->getCategory($item['n_cat']);
-        $board_id = $board_cat['s_id'];
-        $memb = $member->getMember($item['n_writer']);
-        $flag = $item['n_flag'];
-        $b_public_article = $flag & 0x1;
-        $b_no_comment = $flag & 0x2;
-        $b_anonymous = $flag & 0x4;
-        $b_bold_title = $flag & 0x8;
-        $b_public_article = $b_public_article && checkCategoryAccess($board_cat['n_id'], "flag public");
-        $b_no_comment = $b_no_comment && checkCategoryAccess($board_cat['n_id'], "flag no comment");
-        $b_bold_title = $b_bold_title && checkCategoryAccess($board_cat['n_id'], "flag bold title");
-        ?>
+				$board_cat = $board->getCategory($item['n_cat']);
+				$board_id = $board_cat['s_id'];
+				$memb = $member->getMember($item['n_writer']);
+				$flag = $item['n_flag'];
+				$b_public_article = $flag & 0x1;
+				$b_no_comment = $flag & 0x2;
+				$b_anonymous = $flag & 0x4;
+				$b_bold_title = $flag & 0x8;
+				$b_public_article = $b_public_article && checkCategoryAccess($board_cat['n_id'], "flag public");
+				$b_no_comment = $b_no_comment && checkCategoryAccess($board_cat['n_id'], "flag no comment");
+				$b_bold_title = $b_bold_title && checkCategoryAccess($board_cat['n_id'], "flag bold title");
+			?>
 				<tr class="board_list_item" style="<?php echo (isset($_GET['bitm']) && $_GET['bitm'] == $item['n_id']) ? "font-weight:bold;" : "" ?>">
 					<td class="no-mobile" onclick="return changeLinkTo('<?php echo htmlspecialchars(addslashes("/board/$board_id/view/{$item['n_id']}$additional_query_string")) ?>');" style="font-size:8pt;text-align:center;">
 						<?php echo htmlspecialchars($item['n_id']); ?>
@@ -38,42 +38,42 @@ function printArticleListTypeBoard($board_data, $additional_query_string)
 					<td onclick="return changeLinkTo('<?php echo htmlspecialchars(addslashes("/board/$board_id/view/{$item['n_id']}$additional_query_string")) ?>');" style="padding-left:5px;">
 						<a onclick="return false;" href="<?php echo htmlspecialchars("/board/$board_id/view/" . $item['n_id']) ?>" style="color:black;<?php echo $b_bold_title ? "font-weight:bold;" : ""; ?>">
 							<?php
-echo formatTitle($item['s_title']);
-        if (($item['n_comments'] != 0 && doesAdminBypassEverythingAndIsAdmin(!$b_no_comment))) {
-            echo " <span class='comment-num'>{$item['n_comments']}</span>";
-        }
+							echo formatTitle($item['s_title']);
+							if (($item['n_comments'] != 0 && doesAdminBypassEverythingAndIsAdmin(!$b_no_comment))) {
+								echo " <span class='comment-num'>{$item['n_comments']}</span>";
+							}
 
-        ?>
+							?>
 						</a>
 					</td>
-					<?php if ($b_anonymous) {?>
+					<?php if ($b_anonymous) { ?>
 						<td style="text-align:center;" onclick="return changeLinkTo('<?php echo htmlspecialchars(addslashes("/board/$board_id/view/{$item['n_id']}$additional_query_string")) ?>');">
 							익명
 						</td>
-					<?php } else {?>
+					<?php } else { ?>
 						<td style="text-align:center;" onclick="return changeLinkTo('<?php echo htmlspecialchars(addslashes("/user/view/" . $memb['n_id'] . "/" . $memb['s_id'])) ?>');">
 							<a href="<?php echo htmlspecialchars("/user/view/" . $memb['n_id'] . "/" . $memb['s_id']) ?>" style="color:black">
-<?php putUserCard($memb);?></a>
+								<?php putUserCard($memb); ?></a>
 						</td>
-					<?php }?>
+					<?php } ?>
 					<td style="text-align:center;" onclick="return changeLinkTo('<?php echo htmlspecialchars(addslashes("/board/$board_id/view/{$item['n_id']}$additional_query_string")) ?>');">
-<?php
-if (time() - $item['n_writedate'] < 86400) {
-            echo htmlspecialchars(date("H:i:s", $item['n_writedate']));
-        } else {
-            echo htmlspecialchars(date("y-m-d", $item['n_writedate']));
-        }
+						<?php
+						if (time() - $item['n_writedate'] < 86400) {
+							echo htmlspecialchars(date("H:i:s", $item['n_writedate']));
+						} else {
+							echo htmlspecialchars(date("y-m-d", $item['n_writedate']));
+						}
 
-        ?></td>
+						?></td>
 					<td class="no-mobile" style="text-align:center;" onclick="return changeLinkTo('<?php echo htmlspecialchars(addslashes("/board/$board_id/view/{$item['n_id']}$additional_query_string")) ?>');">
 						<?php echo htmlspecialchars($item['n_total_views']); ?>
 					</td>
 				</tr>
-			<?php }?>
+			<?php } ?>
 		</tbody>
 	</table>
-	<?php
-insertOnLoadScript('
+<?php
+	insertOnLoadScript('
 		$(".board_list_item").mouseenter(function(){
 			$(this).css("backgroundColor", "#EEE");
 		}).mouseleave(function(){
@@ -82,105 +82,107 @@ insertOnLoadScript('
 	');
 }
 function printArticleListTypeForum($board_data, $additional_query_string)
-{}
+{
+}
 function printArticleListTypeGallery($board_data, $additional_query_string)
-{}
+{
+}
 function printArticleList()
 {
-    //http://soree-kmla.com/board/all_announce?search_title=true&search_data=true&search_tag=true&search_writer=true&search=asdf
-    global $board_id, $board_act, $board, $board_cat, $member, $is_mobile, $me;
-    $accessible_categories = getCategoriesWithFixes();
-    $articleperpage = 20;
-    $orderby_name = "n_id";
-    $orderby_desc = true;
-    $incl_text = 0;
-    $search = (isset($_GET['search']) && $_GET['search'] != "") ? $_GET['search'] : false;
-    $search_mode_and = isset($_GET['search_mode']) && $_GET['search_mode'] == "and";
-    $search_submode_and = false;
-    $search_title = isset($_GET['search_title']);
-    $search_data = isset($_GET['search_data']);
-    $search_tag = isset($_GET['search_tag']);
-    $search_writer = isset($_GET['search_writer']);
-    $pagenumber = isset($_GET['page']) ? $_GET['page'] : 0;
-    $article_count = $board->getArticleCount($accessible_categories, false, 0, $search, $search_mode_and, $search_submode_and, $search_title, $search_data, $search_tag, $search_writer);
-    if (!is_numeric($pagenumber)) {
-        $pagenumber = 0;
-    } else if ($pagenumber != 0) {
-        $pagenumber--;
-    }
+	//http://soree-kmla.com/board/all_announce?search_title=true&search_data=true&search_tag=true&search_writer=true&search=asdf
+	global $board_id, $board_act, $board, $board_cat, $member, $is_mobile, $me;
+	$accessible_categories = getCategoriesWithFixes();
+	$articleperpage = 20;
+	$orderby_name = "n_id";
+	$orderby_desc = true;
+	$incl_text = 0;
+	$search = (isset($_GET['search']) && $_GET['search'] != "") ? $_GET['search'] : false;
+	$search_mode_and = isset($_GET['search_mode']) && $_GET['search_mode'] == "and";
+	$search_submode_and = false;
+	$search_title = isset($_GET['search_title']);
+	$search_data = isset($_GET['search_data']);
+	$search_tag = isset($_GET['search_tag']);
+	$search_writer = isset($_GET['search_writer']);
+	$pagenumber = isset($_GET['page']) ? $_GET['page'] : 0;
+	$article_count = $board->getArticleCount($accessible_categories, false, 0, $search, $search_mode_and, $search_submode_and, $search_title, $search_data, $search_tag, $search_writer);
+	if (!is_numeric($pagenumber)) {
+		$pagenumber = 0;
+	} else if ($pagenumber != 0) {
+		$pagenumber--;
+	}
 
-    if ($pagenumber < 0) {
-        $pagenumber = 0;
-    }
+	if ($pagenumber < 0) {
+		$pagenumber = 0;
+	}
 
-    $page_count = intval(($article_count + $articleperpage - 1) / $articleperpage);
-    if ($board_id === "student_council_election") {
-        $orderby_name = "s_title";
-    }
+	$page_count = intval(($article_count + $articleperpage - 1) / $articleperpage);
+	if ($board_id === "student_council_election") {
+		$orderby_name = "s_title";
+	}
 
-    $board_data = $board->getArticleList($accessible_categories, false, 0, $pagenumber, $articleperpage, $orderby_name, $orderby_desc, $incl_text, $search, $search_mode_and, $search_submode_and, $search_title, $search_data, $search_tag, $search_writer);
-    if ($search !== false) {
-        foreach ($board_data as $key => $val) {
-            if ($val['n_flag'] & 0x4) {
-                unset($board_data[$key]);
-            }
+	$board_data = $board->getArticleList($accessible_categories, false, 0, $pagenumber, $articleperpage, $orderby_name, $orderby_desc, $incl_text, $search, $search_mode_and, $search_submode_and, $search_title, $search_data, $search_tag, $search_writer);
+	if ($search !== false) {
+		foreach ($board_data as $key => $val) {
+			if ($val['n_flag'] & 0x4) {
+				unset($board_data[$key]);
+			}
+		}
+	}
+	$pagenumber++;
+	$k = $_GET;
+	unset($k['action'], $k['bid'], $k['bact'], $k['page'], $k['bitm']);
+	$additional_query_string = array();
+	foreach ($k as $a => $b) {
+		$additional_query_string[] = rawurlencode($a) . "=" . rawurlencode($b);
+	}
 
-        }
-    }
-    $pagenumber++;
-    $k = $_GET;unset($k['action'], $k['bid'], $k['bact'], $k['page'], $k['bitm']);
-    $additional_query_string = array();
-    foreach ($k as $a => $b) {
-        $additional_query_string[] = rawurlencode($a) . "=" . rawurlencode($b);
-    }
+	$additional_query_string = implode("&", $additional_query_string);
+	if ($additional_query_string) {
+		$additional_query_string = "?$additional_query_string";
+	}
 
-    $additional_query_string = implode("&", $additional_query_string);
-    if ($additional_query_string) {
-        $additional_query_string = "?$additional_query_string";
-    }
-
-    ?>
+?>
 	<h1 style="padding:9px;text-align:left;">모두 보기</h1>
 	<?php
-printArticleListTypeBoard($board_data, $additional_query_string);
-    ?>
+	printArticleListTypeBoard($board_data, $additional_query_string);
+	?>
 	<div style="clear:both"></div>
-	<?php if (count($board_data) == 0) {?>
+	<?php if (count($board_data) == 0) { ?>
 		<div style="padding-top:60px;padding-bottom:60px;width:100%;text-align:center;font-size:18pt;">
 			글이 없습니다.
 		</div>
-	<?php }?>
+	<?php } ?>
 	<div style="text-align:center">
-		<?php $disp = array(1 => true);?>
+		<?php $disp = array(1 => true); ?>
 		<a href="<?php echo "/board/$board_id/page/1$additional_query_string"; ?>" <?php if ($pagenumber == 1) {
-        echo "style='color:black'";
-    }
-    ?>>[1]</a>
+																																									echo "style='color:black'";
+																																								}
+																																								?>>[1]</a>
 		<?php if (2 < $pagenumber - 10) {
-        echo "...";
-    }
-    ?>
-		<?php for ($i = max(2, $pagenumber - 10); $i <= min($page_count - 1, $pagenumber + 10); $i++) {?>
+			echo "...";
+		}
+		?>
+		<?php for ($i = max(2, $pagenumber - 10); $i <= min($page_count - 1, $pagenumber + 10); $i++) { ?>
 			<a href="<?php echo "/board/$board_id/page/$i$additional_query_string"; ?>" <?php if ($pagenumber == $i) {
-        echo "style='color:black'";
-    }
-        ?>>[<?php echo $i;
-        $disp[$i] = true; ?>]</a>
-		<?php }?>
+																																										echo "style='color:black'";
+																																									}
+																																									?>>[<?php echo $i;
+						$disp[$i] = true; ?>]</a>
+		<?php } ?>
 		<?php if ($i < $page_count && $i != max(2, $pagenumber - 10)) {
-        echo "...";
-    }
-    ?>
-		<?php if (!isset($disp[$page_count]) && $page_count > 1) {?><a href="<?php echo "/board/$board_id/page/$page_count$additional_query_string"; ?>" <?php if ($pagenumber == $page_count) {
-        echo "style='color:black'";
-    }
-        ?>>[<?php echo $page_count ?>]</a>
-<?php }?>
+			echo "...";
+		}
+		?>
+		<?php if (!isset($disp[$page_count]) && $page_count > 1) { ?><a href="<?php echo "/board/$board_id/page/$page_count$additional_query_string"; ?>" <?php if ($pagenumber == $page_count) {
+																																																																												echo "style='color:black'";
+																																																																											}
+																																																																											?>>[<?php echo $page_count ?>]</a>
+		<?php } ?>
 	</div>
 	<div style="margin:0 auto;width:120px;text-align:center;">총 <?php echo $article_count ?>개</div>
-	<?php if ($is_mobile) {?>
+	<?php if ($is_mobile) { ?>
 		<div style="clear:both;"></div>
-	<?php }?>
+	<?php } ?>
 	<div style="float:right;position:relative;">
 		<form method="get" action="/board/<?php echo htmlspecialchars($board_id) ?>">
 			<div id="div_search_method">
@@ -196,15 +198,15 @@ printArticleListTypeBoard($board_data, $additional_query_string);
 			<a id="search_from_toggler" onclick="smoothToggleVisibility('#div_search_method', 1); return smoothToggleVisibility('#div_search_from');">제목에서</a>
 			<a id="search_method_toggler" onclick="smoothToggleVisibility('#div_search_from', 1); return smoothToggleVisibility('#div_search_method');">모든 조건 만족 시</a>
 			<?php if ($is_mobile) {
-        echo "<br />";
-    }
-    ?>
+				echo "<br />";
+			}
+			?>
 			<input type="text" class="form-control" name="search" value="<?php echo $search ? htmlspecialchars($search) : "" ?>" style="margin-left:3px;width:120px;vertical-align:middle; display: inline-block;" />
 			<input type="submit" class="btn btn-default" id="search_button" value="검색" style="width:80px;height:32px;vertical-align:middle;" />
 		</form>
 	</div>
 	<?php
-insertOnLoadScript('board_checkSearchFrom();board_checkSearchMethod();');
-    ?>
-	<?php
+	insertOnLoadScript('board_checkSearchFrom();board_checkSearchMethod();');
+	?>
+<?php
 }
--- src/content/util/schedule.php
+++ src/content/util/schedule.php
@@ -1,89 +1,88 @@
 <?php
-$title="일정표 - $title";
-function printContent(){
+$title = "일정표 - $title";
+function printContent()
+{
 	global $member, $mysqli, $me, $is_android, $foodJSON;
 	$curYear = isset($_GET['year']) ? $_GET['year'] : date("Y");
 	$curMonth = isset($_GET['month']) ? $_GET['month'] : date("n");
-	$mode="normal";
-	if(isset($_GET['mode'])){
-		switch($_GET['mode']){
-            case "food:0":
-            case "food:1":
-            case "food:2":
-            case "normal":
-			    $mode=$_GET['mode'];
+	$mode = "normal";
+	if (isset($_GET['mode'])) {
+		switch ($_GET['mode']) {
+			case "food:0":
+			case "food:1":
+			case "food:2":
+			case "normal":
+				$mode = $_GET['mode'];
 		}
 	}
-	if($curMonth < 1 || $curMonth > 12) $curMonth = date("n");
-	if($curYear < 1997) $curYear = 1997;
+	if ($curMonth < 1 || $curMonth > 12) $curMonth = date("n");
+	if ($curYear < 1997) $curYear = 1997;
 	$firstWeekDayOfMonth = date("N", strtotime("$curYear-$curMonth-01")) % 7;
 	$daysOfMonth = date("t", strtotime("$curYear-$curMonth-01"));
-	$currentDay =- $firstWeekDayOfMonth;
+	$currentDay = -$firstWeekDayOfMonth;
 	$calender = array();
-	for($i =- $firstWeekDayOfMonth, $j = 0; $i < $daysOfMonth; $i++, $j++){
-		if(!isset($calender[$j / 7])) $calender[$j / 7] = array();
-		if($i >= 0) $calender[$j / 7][$j % 7] = $i + 1;
+	for ($i = -$firstWeekDayOfMonth, $j = 0; $i < $daysOfMonth; $i++, $j++) {
+		if (!isset($calender[$j / 7])) $calender[$j / 7] = array();
+		if ($i >= 0) $calender[$j / 7][$j % 7] = $i + 1;
 	}
 	$scheduleData = array();
-	$query="SELECT n_day, s_data FROM kmlaonline_schedule_table WHERE n_year=$curYear AND n_month=$curMonth AND s_mode='$mode'";
-	if($res = $mysqli->query($query)){
-		while ($row = $res->fetch_array(MYSQLI_ASSOC)){
+	$query = "SELECT n_day, s_data FROM kmlaonline_schedule_table WHERE n_year=$curYear AND n_month=$curMonth AND s_mode='$mode'";
+	if ($res = $mysqli->query($query)) {
+		while ($row = $res->fetch_array(MYSQLI_ASSOC)) {
 			$scheduleData[$row['n_day']] = $row['s_data'];
 		}
 		$res->close();
-		if($mysqli->more_results())$mysqli->next_result();
+		if ($mysqli->more_results()) $mysqli->next_result();
 	}
-	?>
-    <script src="/js/content/util/schedule.js"></script>
+?>
+	<script src="/js/content/util/schedule.js"></script>
 	<div class="schedule-content">
-        <?php if($is_android && substr($mode, 0, 5) == "food:" ) { ?>
-            <div class="alert alert-info" role="alert">
-                <h3>
-                    <a href="https://play.google.com/store/apps/details?id=com.ldm2468.kmlafood">
-                        <i class="fa fa-download" aria-hidden="true"></i> Google Play 스토어에서 큼온 식단 위젯을 받아보세요!
-                    </a>
-                </h3>
-            </div>
-        <?php } ?>
+		<?php if ($is_android && substr($mode, 0, 5) == "food:") { ?>
+			<div class="alert alert-info" role="alert">
+				<h3>
+					<a href="https://play.google.com/store/apps/details?id=com.ldm2468.kmlafood">
+						<i class="fa fa-download" aria-hidden="true"></i> Google Play 스토어에서 큼온 식단 위젯을 받아보세요!
+					</a>
+				</h3>
+			</div>
+		<?php } ?>
 
-        <h2 style="text-align:center;"><img src="/images/food.png" style="width:64px;vertical-align:bottom;" />
-            <div style="display: inline-block; vertical-align: text-bottom;">
-                <select id="select-year" class="selectpicker" data-style="btn-default" data-width="80px" onchange="location = this.options[this.selectedIndex].value;">
-                    <?php
-                    for($i=$curYear - 5; $i <= date("Y"); $i++) {
-                        if($i < 1997) continue;
-                        ?>
-                        <option value="/util/schedule?mode=<?php echo $mode?>&amp;year=<?php echo $i?>&amp;month=<?php echo $curMonth?>"
-                                <?php if($i == $curYear) echo 'selected';?>>
-                            <?php echo $i?>
-                        </option>
-                    <?php } ?>
-                </select>
-            </div>
-            년
-            <div style="display: inline-block; vertical-align: text-bottom;">
-                <select id="select-month" class="selectpicker" data-style="btn-default" data-width="60px" onchange="location = this.options[this.selectedIndex].value;">
-                    <?php
-                    for($i = 1; $i <= 12; $i++){
-                        ?>
-                        <option value="/util/schedule?mode=<?php echo $mode?>&amp;year=<?php echo $curYear?>&amp;month=<?php echo $i?>"
-                                <?php if($i == $curMonth) echo 'selected';?>>
-                            <?php echo $i?>
-                        </option>
-                    <?php } ?>
-                </select>
-            </div>
-            월
-            <?php echo $mode == 'food:0' ? '아침식단' : ($mode == "food:1" ? '점심식단' : ($mode == "food:2" ? '저녁식단' : '일정')); ?>
-        </h2>
+		<h2 style="text-align:center;"><img src="/images/food.png" style="width:64px;vertical-align:bottom;" />
+			<div style="display: inline-block; vertical-align: text-bottom;">
+				<select id="select-year" class="selectpicker" data-style="btn-default" data-width="80px" onchange="location = this.options[this.selectedIndex].value;">
+					<?php
+					for ($i = $curYear - 5; $i <= date("Y"); $i++) {
+						if ($i < 1997) continue;
+					?>
+						<option value="/util/schedule?mode=<?php echo $mode ?>&amp;year=<?php echo $i ?>&amp;month=<?php echo $curMonth ?>" <?php if ($i == $curYear) echo 'selected'; ?>>
+							<?php echo $i ?>
+						</option>
+					<?php } ?>
+				</select>
+			</div>
+			년
+			<div style="display: inline-block; vertical-align: text-bottom;">
+				<select id="select-month" class="selectpicker" data-style="btn-default" data-width="60px" onchange="location = this.options[this.selectedIndex].value;">
+					<?php
+					for ($i = 1; $i <= 12; $i++) {
+					?>
+						<option value="/util/schedule?mode=<?php echo $mode ?>&amp;year=<?php echo $curYear ?>&amp;month=<?php echo $i ?>" <?php if ($i == $curMonth) echo 'selected'; ?>>
+							<?php echo $i ?>
+						</option>
+					<?php } ?>
+				</select>
+			</div>
+			월
+			<?php echo $mode == 'food:0' ? '아침식단' : ($mode == "food:1" ? '점심식단' : ($mode == "food:2" ? '저녁식단' : '일정')); ?>
+		</h2>
 		<div style="float:right">
-            <a href="https://stu.kwe.go.kr/edusys.jsp?page=sts_m42310"> 나이스에서 식단 보기 </a> |
-			<a style='font-weight:bold;<?php if($mode=="food:0") echo "color:black;"; ?>' href="/util/schedule?mode=food:0&amp;year=<?php echo $curYear?>&amp;month=<?php echo $curMonth?>">아침식단</a> | 
-			<a style='font-weight:bold;<?php if($mode=="food:1") echo "color:black;"; ?>' href="/util/schedule?mode=food:1&amp;year=<?php echo $curYear?>&amp;month=<?php echo $curMonth?>">점심식단</a> | 
-			<a style='font-weight:bold;<?php if($mode=="food:2") echo "color:black;"; ?>' href="/util/schedule?mode=food:2&amp;year=<?php echo $curYear?>&amp;month=<?php echo $curMonth?>">저녁식단</a>
+			<a href="https://stu.kwe.go.kr/edusys.jsp?page=sts_m42310"> 나이스에서 식단 보기 </a> |
+			<a style='font-weight:bold;<?php if ($mode == "food:0") echo "color:black;"; ?>' href="/util/schedule?mode=food:0&amp;year=<?php echo $curYear ?>&amp;month=<?php echo $curMonth ?>">아침식단</a> |
+			<a style='font-weight:bold;<?php if ($mode == "food:1") echo "color:black;"; ?>' href="/util/schedule?mode=food:1&amp;year=<?php echo $curYear ?>&amp;month=<?php echo $curMonth ?>">점심식단</a> |
+			<a style='font-weight:bold;<?php if ($mode == "food:2") echo "color:black;"; ?>' href="/util/schedule?mode=food:2&amp;year=<?php echo $curYear ?>&amp;month=<?php echo $curMonth ?>">저녁식단</a>
 		</div>
 		<div style="float:left">
-			<a style='font-weight:bold;<?php if($mode=="normal") echo "color:black;"; ?>' href="/util/schedule?mode=normal&amp;year=<?php echo $curYear?>&amp;month=<?php echo $curMonth?>">일정 보기</a>
+			<a style='font-weight:bold;<?php if ($mode == "normal") echo "color:black;"; ?>' href="/util/schedule?mode=normal&amp;year=<?php echo $curYear ?>&amp;month=<?php echo $curMonth ?>">일정 보기</a>
 		</div>
 		<br />
 		<table id="calender" class="table">
@@ -100,80 +99,80 @@ function printContent(){
 			</thead>
 			<tbody>
 				<?php
-				for($i = 0; $i < count($calender); $i++){
+				for ($i = 0; $i < count($calender); $i++) {
 					echo "<tr>";
-					for($j = 0; $j < 7; $j++){
-						$bg="padding: 3px; ";
-						if(isset($calender[$i][$j])){
+					for ($j = 0; $j < 7; $j++) {
+						$bg = "padding: 3px; ";
+						if (isset($calender[$i][$j])) {
 							$datename = $calender[$i][$j];
-							if($curYear == date("Y") && $curMonth == date("n") && $datename == date("j"))
+							if ($curYear == date("Y") && $curMonth == date("n") && $datename == date("j"))
 								$bg .= "background: #c5e8f4;";
 						}
-						if($j == 6)
+						if ($j == 6)
 							echo "<td style='border-top:1px solid #ccc;'>";
 						else
 							echo "<td style='border-right:1px solid #ccc;border-top:1px solid #ccc;'>";
-						if(!isset($calender[$i][$j])){
+						if (!isset($calender[$i][$j])) {
 							echo "</td>";
 							continue;
 						}
 						echo "<div style='$bg'>";
 						echo "<span class='datename'>{$datename}</span>";
-						
-						if(substr($mode, 0, 5) != "food:" || isUserPermitted($me['n_id'], "edit_food_table")){ // (1) food가 아니거나(일정이거나) (2) edit_food_table 권한이 있는가?
+
+						if (substr($mode, 0, 5) != "food:" || isUserPermitted($me['n_id'], "edit_food_table")) { // (1) food가 아니거나(일정이거나) (2) edit_food_table 권한이 있는가?
 							echo "<a class='edit' onclick='return util_schedule_goEdit(this);'>편집</a>";
 						}
-						
+
 						echo "<div style='clear:both'></div>";
 						echo "</div>";
-						if($mode == "normal"){
+						if ($mode == "normal") {
 							$minWave = date("Y") - 1997;
 							$births = array();
-							foreach($member->listMembersBirth($curMonth, $datename) as $val){
-								if($val['n_level'] >= $minWave){
-									$births[] = "<a href='/user/view/{$val['n_id']}/{$val['s_id']}'>".putUserCard($val,0,false)."</a>";
+							foreach ($member->listMembersBirth($curMonth, $datename) as $val) {
+								if ($val['n_level'] >= $minWave) {
+									$births[] = "<a href='/user/view/{$val['n_id']}/{$val['s_id']}'>" . putUserCard($val, 0, false) . "</a>";
 								}
 							}
-							if(count($births)){
+							if (count($births)) {
 								echo "<div style='padding:3px;'>";
-								echo "<b>생일: </b>".implode(", ",$births) . "<br />";
+								echo "<b>생일: </b>" . implode(", ", $births) . "<br />";
 								echo "</div>";
 								echo "<div class='divider'></div>";
 							}
 						}
-                        $curData = isset($scheduleData[$datename]) ? $scheduleData[$datename] : "";
-                        $mealArray = array("food:0"=>"breakfast", "food:1"=>"lunch", "food:2"=>"dinner");
-						?>
+						$curData = isset($scheduleData[$datename]) ? $scheduleData[$datename] : "";
+						$mealArray = array("food:0" => "breakfast", "food:1" => "lunch", "food:2" => "dinner");
+				?>
 						<form method='post' action='/ajax/util/schedule' onsubmit='return saveAjax(this,"저장 중...",null);' style='display:none'>
 							<input type='hidden' name='util_action' value='editDate' />
-							<input type="hidden" name="s_mode" value="<?php echo $mode?>" />
-							<input type="hidden" name="n_year" value="<?php echo $curYear?>" />
-							<input type="hidden" name="n_month" value="<?php echo $curMonth?>" />
-							<input type="hidden" name="n_day" value="<?php echo $datename?>" />
-							<textarea name='s_data' class="form-control" style='resize:vertical;' >
+							<input type="hidden" name="s_mode" value="<?php echo $mode ?>" />
+							<input type="hidden" name="n_year" value="<?php echo $curYear ?>" />
+							<input type="hidden" name="n_month" value="<?php echo $curMonth ?>" />
+							<input type="hidden" name="n_day" value="<?php echo $datename ?>" />
+							<textarea name='s_data' class="form-control" style='resize:vertical;'>
 <?php echo htmlspecialchars($curData); ?> </textarea>
 							<div style='text-align:right'>
 								<input type='button' style="margin: 3px" class="btn btn-default" value='취소' onclick='return util_schedule_cancelEdit(this);' />
 								<input type='submit' style="margin: 3px" class="btn btn-default" value='저장' />
 							</div>
-                        </form>
-                        <div style='width:100%;padding:3px;margin:0;border:0;text-align:center;'>
-<?php
-                            if ($mode == "normal") {
-                                if (strlen($curData) > 0) echo nl2br($curData);
-                                else echo "<span style='color:#DDD'>(지정되지 않음)</span>";
-                            } else {
-                                printFood($foodJSON, $curMonth, $calender[$i][$j], $mealArray[$mode]);
-                            }
-                            // if(strlen($curData) > 0)
-                            //     echo nl2br($curData);
-                            // else if($mode == "normal")
-                            //     echo "<span style='color:#DDD'>(지정되지 않음)</span>";
-                            // else
-                            //     echo "<span style='color:#DDD'>(입력되지 않음)</span>";
-                        ?></div>
+						</form>
+						<div style='width:100%;padding:3px;margin:0;border:0;text-align:center;'>
+							<?php
+							if ($mode == "normal") {
+								if (strlen($curData) > 0) echo nl2br($curData);
+								else echo "<span style='color:#DDD'>(지정되지 않음)</span>";
+							} else {
+								printFood($foodJSON, $curMonth, $calender[$i][$j], $mealArray[$mode]);
+							}
+							// if(strlen($curData) > 0)
+							//     echo nl2br($curData);
+							// else if($mode == "normal")
+							//     echo "<span style='color:#DDD'>(지정되지 않음)</span>";
+							// else
+							//     echo "<span style='color:#DDD'>(입력되지 않음)</span>";
+							?></div>
 						</td>
-						<?php
+				<?php
 					}
 					echo "</tr>";
 				}
@@ -181,6 +180,6 @@ function printContent(){
 			</tbody>
 		</table>
 	</div>
-	<?php
+<?php
 }
 ?>
